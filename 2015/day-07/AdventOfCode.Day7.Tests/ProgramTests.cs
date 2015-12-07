e612a7bcf00943c2a1884a0101ca1084f99b23ec
martincostello <martin@martincostello.com>
2015-12-07 15:02:44 +0000

    Day 7 - redux
    Rewrite day 7 as it was fundamentally flawed for solving the problem in
    the input file due to it not starting with known input values. Now
    refactored and simplified, it solves part 1 and has been extended to
    solve part 2 as well.

---
+++ 2015/day-07/AdventOfCode.Day7.Tests/ProgramTests.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProgramTests.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   ProgramTests.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MartinCostello.AdventOfCode.Day7
+{
+    using System.Collections.Generic;
+    using Xunit;
+
+    /// <summary>
+    /// A class that contains tests for the <see cref="Program"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class ProgramTests
+    {
+        [Fact]
+        public static void InstructionsAreInterpretedCorrectly()
+        {
+            // Arrange
+            var instructions = new string[]
+            {
+                "j -> k",
+                "123 -> x",
+                "456 -> y",
+                "x AND y -> d",
+                "x OR y -> e",
+                "x LSHIFT 2 -> f",
+                "y RSHIFT 2 -> g",
+                "NOT x -> h",
+                "NOT y -> i",
+                "i -> j",
+            };
+
+            // Act
+            IDictionary<string, ushort> actual = Program.GetWireValues(instructions);
+
+            // Assert
+            Assert.Equal(72, actual["d"]);
+            Assert.Equal(507, actual["e"]);
+            Assert.Equal(492, actual["f"]);
+            Assert.Equal(114, actual["g"]);
+            Assert.Equal(65412, actual["h"]);
+            Assert.Equal(65079, actual["i"]);
+            Assert.Equal(65079, actual["j"]);
+            Assert.Equal(65079, actual["k"]);
+            Assert.Equal(123, actual["x"]);
+            Assert.Equal(456, actual["y"]);
+        }
+    }
+}
--- 2015/day-07/AdventOfCode.Day7/Program.cs
+++ 2015/day-07/AdventOfCode.Day7/Program.cs
@@ -14,7 +14,9 @@ namespace MartinCostello.AdventOfCode.Day7
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.IO;
+    using System.Linq;
 
     /// <summary>
     /// A console application that solves <c>http://adventofcode.com/day/7</c>. This class cannot be inherited.
@@ -40,11 +42,131 @@ namespace MartinCostello.AdventOfCode.Day7
                 return -1;
             }
 
-            IDictionary<string, ILogicGate> circuit = CircuitBuilder.Build(File.ReadLines(args[0]));
+            IList<string> instructions = File.ReadAllLines(args[0]);
 
-            Console.WriteLine("The signal for wire a is {0}.", circuit["a"].Signal);
+            // Get the wire values for the initial instructions
+            IDictionary<string, ushort> values = GetWireValues(instructions);
+
+            ushort a = values["a"];
+
+            Console.WriteLine("The signal for wire a is {0:N0}.", a);
+
+            // Replace the input value for b with the value for a, then re-calculate
+            int indexForB = instructions.IndexOf("44430 -> b");
+            instructions[indexForB] = string.Format(CultureInfo.InvariantCulture, "{0} -> b", a);
+
+            values = GetWireValues(instructions);
+
+            a = values["a"];
+
+            Console.WriteLine("The new signal for wire a is {0:N0}.", a);
 
             return 0;
+        }
+
+        /// <summary>
+        /// Gets the wire values for the specified instructions.
+        /// </summary>
+        /// <param name="instructions">The instructions to get the wire values for.</param>
+        /// <returns>An <see cref="IDictionary{TKey, TValue}"/> containing the values for wires keyed by their Ids.</returns>
+        internal static IDictionary<string, ushort> GetWireValues(IEnumerable<string> instructions)
+        {
+            // Create a map of wire Ids to the instructions to get their value
+            var instructionMap = instructions
+                .Select((p) => p.Split(new string[] { " -> " }, StringSplitOptions.None))
+                .ToDictionary((p) => p.Last(), (p) => p.First().Split(' '));
+
+            Dictionary<string, ushort> result = new Dictionary<string, ushort>();
+
+            // Loop through the instructions until we have reduced each instruction to a value
+            while (result.Count != instructionMap.Count)
+            {
+                foreach (var pair in instructionMap)
+                {
+                    string wireId = pair.Key;
+
+                    if (result.ContainsKey(wireId))
+                    {
+                        // We already have the value for this wire
+                        continue;
+                    }
+
+                    string[] words = pair.Value;
+                    ushort? solvedValue = null;
+
+                    string firstOperand = words.FirstOrDefault();
+                    string secondOperand;
+
+                    if (words.Length == 1)
+                    {
+                        // "123 -> x" or " -> "lx -> a"
+                        ushort value;
+
+                        // Is the instruction a value or a previously solved value?
+                        if (ushort.TryParse(firstOperand, out value) || result.TryGetValue(firstOperand, out value))
+                        {
+                            result[wireId] = value;
+                        }
+                    }
+                    else if (words.Length == 2 && firstOperand == "NOT")
+                    {
+                        // "NOT e -> f" or "NOT 1 -> g"
+                        secondOperand = words.ElementAtOrDefault(1);
+
+                        ushort value;
+
+                        // Is the second operand a value or a previously solved value?
+                        if (ushort.TryParse(secondOperand, out value) ||
+                            result.TryGetValue(secondOperand, out value))
+                        {
+                            result[wireId] = (ushort)~value;
+                        }
+                    }
+                    else if (words.Length == 3)
+                    {
+                        secondOperand = words.ElementAtOrDefault(2);
+
+                        ushort firstValue;
+                        ushort secondValue;
+
+                        // Are both operands a value or a previously solved value?
+                        if ((ushort.TryParse(firstOperand, out firstValue) || result.TryGetValue(firstOperand, out firstValue)) &&
+                            (ushort.TryParse(secondOperand, out secondValue) || result.TryGetValue(secondOperand, out secondValue)))
+                        {
+                            string operation = words.ElementAtOrDefault(1);
+
+                            if (operation == "AND")
+                            {
+                                // "x AND y -> z"
+                                solvedValue = (ushort)(firstValue & secondValue);
+                            }
+                            else if (operation == "OR")
+                            {
+                                // "i OR j => k"
+                                solvedValue = (ushort)(firstValue | secondValue);
+                            }
+                            else if (operation == "LSHIFT")
+                            {
+                                // "p LSHIFT 2"
+                                solvedValue = (ushort)(firstValue << secondValue);
+                            }
+                            else if (operation == "RSHIFT")
+                            {
+                                // "q RSHIFT 3"
+                                solvedValue = (ushort)(firstValue >> secondValue);
+                            }
+                        }
+                    }
+
+                    // The value for this wire Id has been solved
+                    if (solvedValue.HasValue)
+                    {
+                        result[wireId] = solvedValue.Value;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
