fd045bc05191843c658aafb89bae3d70fd067b80
martincostello <martin@martincostello.com>
2015-12-08 14:30:04 +0000

    Add project structure for day 8

---
+++ 2015/day-08/AdventOfCode.Day8.Tests/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ProgramTests.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   ProgramTests.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MartinCostello.AdventOfCode.Day8
+{
+    using System;
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Program"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class ProgramTests
+    {
+    }
+}
---
+++ 2015/day-08/AdventOfCode.Day8/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode.Day8")]
+[assembly: AssemblyDescription("Solution to day 8 of http://adventofcode.com/day/8.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Martin Costello")]
+[assembly: AssemblyProduct("AdventOfCode.Day8")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e02af43-1282-4425-a081-6690630950d0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("AdventOfCode.Day8.Tests")]
