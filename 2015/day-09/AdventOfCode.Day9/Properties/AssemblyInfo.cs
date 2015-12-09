74f5c5ffbc9f08c92f6ab07cf18af03faef415bd
martincostello <martin@martincostello.com>
2015-12-09 20:37:26 +0000

    Day 9 - add solution files
    Add project files ready to start implementation.

---
+++ 2015/day-09/AdventOfCode.Day9/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode.Day9")]
+[assembly: AssemblyDescription("Solution to day 9 of http://adventofcode.com/day/9.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Martin Costello")]
+[assembly: AssemblyProduct("AdventOfCode.Day9")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("722c7de7-453b-4d21-a2e1-e5e9634d344b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("AdventOfCode.Day9.Tests")]
---
+++ 2015/day-09/Build.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set _MSBuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% set _MSBuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
+if not exist %_MSBuild% echo Error: Could not find MSBuild.exe. && exit /b 1
+
+"%~dp0\.nuget\nuget.exe" restore "%~dp0\AdventOfCode.Day9.sln" && echo.
+%_MSBuild% "%~dp0\AdventOfCode.Day9.sln" /v:minimal /maxcpucount /nodeReuse:false "/p:OutDir=%~dp0\BuildOutput" %* && echo.
+"%~dp0\packages\xunit.runner.console.2.1.0\tools\xunit.console" "%~dp0\BuildOutput\AdventOfCode.Day9.Tests.dll" && echo.
+"%~dp0\BuildOutput\AdventOfCode.Day9.exe" "%~dp0\input.txt" && echo.
