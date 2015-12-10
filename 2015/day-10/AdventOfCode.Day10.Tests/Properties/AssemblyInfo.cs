94849a8ab2f388033ef92850fef4d012dbc789e7
martincostello <martin@martincostello.com>
2015-12-10 13:00:13 +0000

    Day 10 - part 1

---
+++ 2015/day-10/AdventOfCode.Day10.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode.Day10.Tests")]
+[assembly: AssemblyDescription("Tests for the solution to day 10 of http://adventofcode.com/day/10.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Martin Costello")]
+[assembly: AssemblyProduct("AdventOfCode.Day10.Tests")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1455fb99-1320-4a45-8ee3-c386626c2808")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
