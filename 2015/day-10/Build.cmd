9c5a0a9fcdcaf88c13f26c29f37ff0d715042bc5
martincostello <martin@martincostello.com>
2015-12-10 13:01:36 +0000

    Day 10 - part 2

--- 2015/day-10/Build.cmd
+++ 2015/day-10/Build.cmd
@@ -8,3 +8,4 @@ if not exist %_MSBuild% echo Error: Could not find MSBuild.exe. && exit /b 1
 %_MSBuild% "%~dp0\AdventOfCode.Day10.sln" /v:minimal /maxcpucount /nodeReuse:false "/p:OutDir=%~dp0\BuildOutput" %* && echo.
 "%~dp0\packages\xunit.runner.console.2.1.0\tools\xunit.console" "%~dp0\BuildOutput\AdventOfCode.Day10.Tests.dll" && echo.
 "%~dp0\BuildOutput\AdventOfCode.Day10.exe" 1321131112 40 && echo.
+"%~dp0\BuildOutput\AdventOfCode.Day10.exe" 1321131112 50 && echo.
