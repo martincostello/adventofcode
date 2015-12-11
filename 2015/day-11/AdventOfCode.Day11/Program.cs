9e3635a1c6e9d1e0d101a3eb91031cf48bae1dc4
martincostello <martin@martincostello.com>
2015-12-11 18:14:54 +0000

    Add solution files for day 11

---
+++ 2015/day-11/AdventOfCode.Day11/Program.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   Program.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MartinCostello.AdventOfCode.Day11
+{
+    using System;
+
+    /// <summary>
+    /// A console application that solves <c>http://adventofcode.com/day/11</c>. This class cannot be inherited.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry-point to the application.
+        /// </summary>
+        /// <param name="args">The arguments to the application.</param>
+        /// <returns>The exit code from the application.</returns>
+        internal static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("No input value specified.");
+                return -1;
+            }
+
+            string current = args[0];
+            string next = GenerateNextPassword(current);
+
+            Console.WriteLine("Santa's new password should be '{0}'.", next);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Generates the next password that should be used based on a current password value.
+        /// </summary>
+        /// <param name="current">The current password.</param>
+        /// <returns>The next password.</returns>
+        internal static string GenerateNextPassword(string current)
+        {
+            return current;
+        }
+    }
+}
---
+++ 2015/day-11/AdventOfCode.Day11/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="https://github.com/martincostello/adventofcode">
+//   Martin Costello (c) 2015
+// </copyright>
+// <license>
+//   See license.txt in the project root for license information.
+// </license>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventOfCode.Day11")]
+[assembly: AssemblyDescription("Solution to day 11 of http://adventofcode.com/day/11.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Martin Costello")]
+[assembly: AssemblyProduct("AdventOfCode.Day11")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62a50250-9ae8-44f0-afbb-b6304c553ecf")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("AdventOfCode.Day11.Tests")]
