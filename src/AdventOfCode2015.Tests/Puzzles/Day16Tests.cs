7ccf51efddb11d8ec6989ce7202f67bf2fdd8574
martincostello <martin@martincostello.com>
2015-12-16 09:24:18 +0000

    Add basic structure for implementing day 16 part 1
    Add input file, test class and forensic analysis.

---
+++ src/AdventOfCode2015.Tests/Puzzles/Day16Tests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode2015.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day16"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Day16Tests
+    {
+        [Fact(Skip = "Not implemented yet.")]
+        public static void Day16_Solve_Returns_Correct_Solution()
+        {
+            // Arrange
+            string[] args = new[] { @".\Input\Day16\input.txt" };
+            var target = new Day16();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.NotEqual(0, target.AuntSueNumber);
+        }
+    }
+}
--- src/AdventOfCode2015/Puzzles/Day16.cs
+++ src/AdventOfCode2015/Puzzles/Day16.cs
@@ -3,10 +3,79 @@
 
 namespace MartinCostello.AdventOfCode2015.Puzzles
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
     /// <summary>
     /// A class representing the puzzle for <c>http://adventofcode.com/day/16</c>. This class cannot be inherited.
     /// </summary>
-    internal sealed class Day16 : NotImplementedPuzzle
+    internal sealed class Day16 : IPuzzle
     {
+        /// <summary>
+        /// The result of the forensic analysis of the gift from Aunt Sue X
+        /// as provided by the My First Crime Scene Analysis Machine (MFCSAM).
+        /// </summary>
+        private static readonly IDictionary<string, int> ForensicAnalysis = new Dictionary<string, int>()
+        {
+            { "children", 3 },
+            { "cats", 7 },
+            { "samoyeds", 2 },
+            { "pomeranians", 3 },
+            { "akitas", 0 },
+            { "vizslas", 0 },
+            { "goldfish", 5 },
+            { "trees", 3 },
+            { "cars", 2 },
+            { "perfumes", 1 },
+        };
+
+        /// <summary>
+        /// Gets the number of the Aunt Sue that sent the gift.
+        /// </summary>
+        internal int AuntSueNumber { get; private set; }
+
+        /// <inheritdoc />
+        public int Solve(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("No input file path specified.");
+                return -1;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.Error.WriteLine("The input file path specified cannot be found.");
+                return -1;
+            }
+
+            string[] clues = File.ReadAllLines(args[0]);
+
+            AuntSueNumber = WhichAuntSueSentTheGift(clues);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the number of the Aunt Sue that sent the gift from the specified clues.
+        /// </summary>
+        /// <param name="clues">The clues.</param>
+        /// <returns>
+        /// The number of the Aunt Sue which sent the gift based on forensic analysis of <paramref name="clues"/>.
+        /// </returns>
+        internal static int WhichAuntSueSentTheGift(ICollection<string> clues)
+        {
+            foreach (var item in ForensicAnalysis)
+            {
+                // TODO Analyze the results
+                if (clues != null && item.Value > 0)
+                {
+                }
+            }
+
+            // TODO Implement
+            return 0;
+        }
     }
 }
