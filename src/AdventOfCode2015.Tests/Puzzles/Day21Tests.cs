267ffea39a891a82f8ab2bdddbec968bc7fb0d40
martincostello <martin@martincostello.com>
2015-12-20 11:18:07 +0000

    Add empty test classes for days 18-25

---
+++ src/AdventOfCode2015.Tests/Puzzles/Day21Tests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode2015.Puzzles
+{
+    using System.Linq;
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day21"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Day21Tests
+    {
+    }
+}
