ea39f53f0b0d91268b5c4ca75ba2aa981ddc6666
martincostello <martin@martincostello.com>
2015-12-21 14:25:31 +0000

    Day 20 - part 1
    Brute force solution to find which is the first house to get at least 34
    million presents.
    A more efficient solution is required as this takes approximately 1,000
    seconds to compute.

--- src/AdventOfCode2015.Tests/Puzzles/Day20Tests.cs
+++ src/AdventOfCode2015.Tests/Puzzles/Day20Tests.cs
@@ -3,7 +3,6 @@
 
 namespace MartinCostello.AdventOfCode2015.Puzzles
 {
-    using System.Linq;
     using Xunit;
 
     /// <summary>
@@ -11,5 +10,57 @@ namespace MartinCostello.AdventOfCode2015.Puzzles
     /// </summary>
     public static class Day20Tests
     {
+        [Theory]
+        [InlineData(1, 10)]
+        [InlineData(2, 30)]
+        [InlineData(3, 40)]
+        [InlineData(4, 70)]
+        [InlineData(5, 60)]
+        [InlineData(6, 120)]
+        [InlineData(7, 80)]
+        [InlineData(8, 150)]
+        [InlineData(9, 130)]
+        public static void Day20_GetPresentsDelivered(int house, int expected)
+        {
+            // Act
+            int actual = Day20.GetPresentsDelivered(house);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(10, 1)]
+        [InlineData(30, 2)]
+        [InlineData(40, 3)]
+        [InlineData(70, 4)]
+        [InlineData(60, 4)]
+        [InlineData(120, 6)]
+        [InlineData(80, 6)]
+        [InlineData(150, 8)]
+        [InlineData(130, 8)]
+        public static void Day20_GetLowestHouseNumber(int target, int expected)
+        {
+            // Act
+            int actual = Day20.GetLowestHouseNumber(target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact(Skip = "Too slow.")]
+        public static void Day20_Solve_Returns_Correct_Solution()
+        {
+            // Arrange
+            string[] args = new[] { "34000000" };
+            var target = new Day20();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.Equal(786240, target.LowestHouseNumber);
+        }
     }
 }
--- src/AdventOfCode2015/Puzzles/Day20.cs
+++ src/AdventOfCode2015/Puzzles/Day20.cs
@@ -3,10 +3,77 @@
 
 namespace MartinCostello.AdventOfCode2015.Puzzles
 {
+    using System;
+    using System.Globalization;
+
     /// <summary>
     /// A class representing the puzzle for <c>http://adventofcode.com/day/20</c>. This class cannot be inherited.
     /// </summary>
-    internal sealed class Day20 : NotImplementedPuzzle
+    internal sealed class Day20 : IPuzzle
     {
+        /// <summary>
+        /// Gets the lowest house number that gets at least the specified number of presents.
+        /// </summary>
+        internal int LowestHouseNumber { get; private set; }
+
+        public int Solve(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("No target value specified.");
+                return -1;
+            }
+
+            int target = int.Parse(args[0], CultureInfo.InvariantCulture);
+
+            LowestHouseNumber = GetLowestHouseNumber(target);
+
+            Console.WriteLine(
+                "The first house to receive at least {0:N0} presents is house number {1:N0}.",
+                target,
+                LowestHouseNumber);
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the lowest house number that gets the specified number of presents.
+        /// </summary>
+        /// <param name="target">The target number.</param>
+        /// <returns>
+        /// The lowest house number that receives at least the specified number of presents.
+        /// </returns>
+        internal static int GetLowestHouseNumber(int target)
+        {
+            for (int i = 1; ; i++)
+            {
+                if (GetPresentsDelivered(i) >= target)
+                {
+                    return i;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of presents delivered to the specified house.
+        /// </summary>
+        /// <param name="house">The house number.</param>
+        /// <returns>
+        /// The number of presents delivered to the specified house.
+        /// </returns>
+        internal static int GetPresentsDelivered(int house)
+        {
+            int count = 0;
+
+            for (int elf = 1; elf < house + 1; elf++)
+            {
+                if (house % elf == 0)
+                {
+                    count += elf * 10;
+                }
+            }
+
+            return count;
+        }
     }
 }
