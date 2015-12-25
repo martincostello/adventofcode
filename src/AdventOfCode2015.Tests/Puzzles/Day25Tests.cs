2b9f620ed0fe26c52d5064697ed596a7dc25d887
martincostello <martin@martincostello.com>
2015-12-25 11:26:32 +0000

    Day 25 - part 1
    Find the code for the weather machine.

--- src/AdventOfCode2015.Tests/Puzzles/Day25Tests.cs
+++ src/AdventOfCode2015.Tests/Puzzles/Day25Tests.cs
@@ -3,7 +3,6 @@
 
 namespace MartinCostello.AdventOfCode2015.Puzzles
 {
-    using System.Linq;
     using Xunit;
 
     /// <summary>
@@ -11,5 +10,80 @@ namespace MartinCostello.AdventOfCode2015.Puzzles
     /// </summary>
     public static class Day25Tests
     {
+        [Theory]
+        [InlineData(20151125u, 31916031u)]
+        [InlineData(31916031u, 18749137u)]
+        [InlineData(18749137u, 16080970u)]
+        [InlineData(16080970u, 21629792u)]
+        [InlineData(21629792u, 17289845u)]
+        [InlineData(17289845u, 24592653u)]
+        [InlineData(24592653u, 8057251u)]
+        [InlineData(8057251u, 16929656u)]
+        [InlineData(16929656u, 30943339u)]
+        [InlineData(30943339u, 77061u)]
+        [InlineData(77061u, 32451966u)]
+        [InlineData(32451966u, 1601130u)]
+        [InlineData(1601130u, 7726640u)]
+        [InlineData(7726640u, 10071777u)]
+        [InlineData(10071777u, 33071741u)]
+        [InlineData(33071741u, 17552253u)]
+        [InlineData(17552253u, 21345942u)]
+        [InlineData(21345942u, 7981243u)]
+        [InlineData(7981243u, 15514188u)]
+        [InlineData(15514188u, 33511524u)]
+        public static void Day25_GenerateCode(ulong value, ulong expected)
+        {
+            // Act
+            ulong actual = Day25.GenerateCode(value);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 20151125u)]
+        [InlineData(2, 1, 31916031u)]
+        [InlineData(1, 2, 18749137u)]
+        [InlineData(3, 1, 16080970u)]
+        [InlineData(2, 2, 21629792u)]
+        [InlineData(1, 3, 17289845u)]
+        [InlineData(4, 1, 24592653u)]
+        [InlineData(3, 2, 8057251u)]
+        [InlineData(2, 3, 16929656u)]
+        [InlineData(4, 1, 24592653u)]
+        [InlineData(5, 1, 77061u)]
+        [InlineData(4, 2, 32451966u)]
+        [InlineData(3, 3, 1601130u)]
+        [InlineData(2, 4, 7726640u)]
+        [InlineData(1, 5, 10071777u)]
+        [InlineData(6, 1, 33071741u)]
+        [InlineData(5, 2, 17552253u)]
+        [InlineData(4, 3, 21345942u)]
+        [InlineData(3, 4, 7981243u)]
+        [InlineData(5, 5, 9250759u)]
+        [InlineData(1, 6, 33511524u)]
+        public static void Day25_GetCodeForWeatherMachine(int row, int column, ulong expected)
+        {
+            // Act
+            ulong actual = Day25.GetCodeForWeatherMachine(row, column);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Day25_Solve_Returns_Correct_Solution()
+        {
+            // Arrange
+            string[] args = new string[] { "2947", "3029" };
+            var target = new Day25();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.Equal(19980801u, target.Code);
+        }
     }
 }
--- src/AdventOfCode2015/Puzzles/Day25.cs
+++ src/AdventOfCode2015/Puzzles/Day25.cs
@@ -3,10 +3,91 @@
 
 namespace MartinCostello.AdventOfCode2015.Puzzles
 {
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+
     /// <summary>
     /// A class representing the puzzle for <c>http://adventofcode.com/day/25</c>. This class cannot be inherited.
     /// </summary>
-    internal sealed class Day25 : NotImplementedPuzzle
+    internal sealed class Day25 : IPuzzle
     {
+        /// <summary>
+        /// Gets the code for the weather machine.
+        /// </summary>
+        internal ulong Code { get; private set; }
+
+        /// <inheritdoc />
+        public int Solve(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("No row number and column number specified.");
+                return -1;
+            }
+
+            int row = int.Parse(args[0], CultureInfo.InvariantCulture);
+            int column = int.Parse(args[1], CultureInfo.InvariantCulture);
+
+            Code = GetCodeForWeatherMachine(row, column);
+
+            Console.WriteLine(
+                "The code for row {0:N0} and column {1:N0} is {2:N0}.",
+                row,
+                column,
+                Code);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets the code for the weather machine at the specified row and column.
+        /// </summary>
+        /// <param name="row">The row number.</param>
+        /// <param name="column">The column number.</param>
+        /// <returns>
+        /// The weather machine code for the specified row and column.
+        /// </returns>
+        internal static ulong GetCodeForWeatherMachine(int row, int column)
+        {
+            // Zero-index the row and column
+            row--;
+            column--;
+
+            ulong result = 20151125;
+
+            Point current = new Point(0, 0);
+            Point target = new Point(column, row);
+
+            int currentRow = 0;
+
+            while (current != target)
+            {
+                currentRow++;
+
+                for (int i = 0; i <= currentRow && current != target; i++)
+                {
+                    current = new Point(i, currentRow - i);
+                    result = GenerateCode(result);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Generates the next code from the specified value.
+        /// </summary>
+        /// <param name="value">The value to generate the code from.</param>
+        /// <returns>
+        /// The code generated from the specified value.
+        /// </returns>
+        internal static ulong GenerateCode(ulong value)
+        {
+            ulong result = value * 252533;
+            result = result % 33554393;
+
+            return result;
+        }
     }
 }
