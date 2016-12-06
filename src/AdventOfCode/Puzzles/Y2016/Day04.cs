8e44c874597b7307656a14a6dabcbf666ef46769
martincostello <martin@martincostello.com>
2016-12-06 13:32:42 +0000

    Solve puzzle 4 part 1
    Solve part 1 of the puzzle for day 4.

---
+++ src/AdventOfCode/Puzzles/Y2016/Day04.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2016
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2016/day/4</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day04 : Puzzle2016
+    {
+        /// <summary>
+        /// An array containing dashes. This field is read-only.
+        /// </summary>
+        private static readonly char[] Separator = new[] { '-' };
+
+        /// <summary>
+        /// Gets the sum of the sector Ids of all real rooms.
+        /// </summary>
+        public int SumOfSectorIdsOfRealRooms { get; private set; }
+
+        /// <summary>
+        /// Returns whether the specified room is real.
+        /// </summary>
+        /// <param name="name">The encrypted name of the room.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="name"/> is a real room; otherwise <see langword="false"/>.
+        /// </returns>
+        internal static bool IsRoomReal(string name)
+        {
+            string unused;
+            return IsRoomReal(name, out unused);
+        }
+
+        /// <summary>
+        /// Returns the sum of the sector Ids of the specified room names which are real.
+        /// </summary>
+        /// <param name="names">The names of the rooms to compute the sum from.</param>
+        /// <returns>
+        /// The sum of the sector Ids for room names specified by <paramref name="names"/> which are real.
+        /// </returns>
+        internal static int SumOfRealRoomSectorIds(IEnumerable<string> names)
+        {
+            int sum = 0;
+
+            foreach (string name in names)
+            {
+                string sectorIdString;
+
+                if (IsRoomReal(name, out sectorIdString))
+                {
+                    sum += int.Parse(sectorIdString, CultureInfo.InvariantCulture);
+                }
+            }
+
+            return sum;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> names = ReadResourceAsLines();
+
+            SumOfSectorIdsOfRealRooms = SumOfRealRoomSectorIds(names);
+
+            Console.WriteLine("The sum of the sector Ids of the real rooms is {0:N0}.", SumOfSectorIdsOfRealRooms);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns whether the specified room is real.
+        /// </summary>
+        /// <param name="name">The encrypted name of the room.</param>
+        /// <param name="sectorId">When the method returns, contains the sector Id of the room.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="name"/> is a real room; otherwise <see langword="false"/>.
+        /// </returns>
+        private static bool IsRoomReal(string name, out string sectorId)
+        {
+            sectorId = null;
+
+            string[] split = name.Split(Separator, StringSplitOptions.None);
+
+            string encryptedName = string.Join(string.Empty, split.Take(split.Length - 1));
+            string sectorIdAndChecksum = split[split.Length - 1];
+
+            int index = sectorIdAndChecksum.IndexOf('[');
+
+            sectorId = sectorIdAndChecksum.Substring(0, index);
+            string checksum = sectorIdAndChecksum.Substring(index + 1, 5);
+
+            var top5Letters = encryptedName
+                .GroupBy((p) => p)
+                .OrderByDescending((p) => p.Count())
+                .ThenBy((p) => p.Key)
+                .Take(5)
+                .Select((p) => p.Key)
+                .ToArray();
+
+            string computedChecksum = string.Join(string.Empty, top5Letters);
+
+            return string.Equals(computedChecksum, checksum, StringComparison.Ordinal);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2016/Day04Tests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2016
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day04"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Day04Tests
+    {
+        [Theory]
+        [InlineData("aaaaa-bbb-z-y-x-123[abxyz]", true)]
+        [InlineData("a-b-c-d-e-f-g-h-987[abcde]", true)]
+        [InlineData("not-a-real-room-404[oarel]", true)]
+        [InlineData("totally-real-room-200[decoy]", false)]
+        public static void Y2016_Day04_IsRoomReal_Returns_Correct_Solution(string name, bool expected)
+        {
+            // Act
+            bool actual = Day04.IsRoomReal(name);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new[] { "aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]" }, 1514)]
+        public static void Y2016_Day04_SumOfRealRoomSectorIds_Returns_Correct_Solution(string[] names, int expected)
+        {
+            // Act
+            int actual = Day04.SumOfRealRoomSectorIds(names);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Y2016_Day04_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = PuzzleTestHelpers.SolvePuzzle<Day04>();
+
+            // Assert
+            Assert.Equal(137896, puzzle.SumOfSectorIdsOfRealRooms);
+        }
+    }
+}
