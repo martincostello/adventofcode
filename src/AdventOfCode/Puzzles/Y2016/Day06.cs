f90e58e27936531d8f23b89d3c5714be8cefb19e
martincostello <martin@martincostello.com>
2016-12-07 18:43:38 +0000

    Solve day 6 part 1
    Solve part 1 of the puzzle for day 6.

---
+++ src/AdventOfCode/Puzzles/Y2016/Day06.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2016
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2016/day/6</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day06 : Puzzle2016
+    {
+        /// <summary>
+        /// Gets the error corrected message.
+        /// </summary>
+        public string ErrorCorrectedMessage { get; private set; }
+
+        /// <summary>
+        /// Decrypts the specified message using a repetition code.
+        /// </summary>
+        /// <param name="messages">The jammed messages.</param>
+        /// <returns>
+        /// The decrypted message derived from <paramref name="messages"/>.
+        /// </returns>
+        internal static string DecryptMessage(ICollection<string> messages)
+        {
+            int length = messages.First().Length;
+
+            var result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                char ch = messages
+                    .Select((p) => p[i])
+                    .GroupBy((p) => p)
+                    .OrderByDescending((p) => p.Count())
+                    .Select((p) => p.Key)
+                    .First();
+
+                result.Append(ch);
+            }
+
+            return result.ToString();
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> messages = ReadResourceAsLines();
+
+            ErrorCorrectedMessage = DecryptMessage(messages);
+
+            Console.WriteLine($"The error-corrected message is: {ErrorCorrectedMessage}.");
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2016/Day06Tests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2016
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day06"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Day06Tests
+    {
+        [Theory]
+        [InlineData(new[] { "eedadn", "drvtee", "eandsr", "raavrd", "atevrs", "tsrnev", "sdttsa", "rasrtv", "nssdts", "ntnada", "svetve", "tesnvt", "vntsnd", "vrdear", "dvrsen", "enarar" }, "easter")]
+        public static void Y2016_Day06_DecryptMessage_Returns_Correct_Solution(string[] messages, string expected)
+        {
+            // Act
+            string actual = Day06.DecryptMessage(messages);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Y2016_Day06_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = PuzzleTestHelpers.SolvePuzzle<Day06>();
+
+            // Assert
+            Assert.Equal("qzedlxso", puzzle.ErrorCorrectedMessage);
+        }
+    }
+}
