70992a488c1df0f184b0d9c3f4a555ec6b077dd7
martincostello <martin@martincostello.com>
2016-12-08 12:34:26 +0000

    Solve day 7 part 2
    Solve part 2 of the puzzle for day 7.

--- src/AdventOfCode/Puzzles/Y2016/Day07.cs
+++ src/AdventOfCode/Puzzles/Y2016/Day07.cs
@@ -14,9 +14,56 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
     internal sealed class Day07 : Puzzle2016
     {
         /// <summary>
+        /// Gets the number of IPv7 addresses that support SSL.
+        /// </summary>
+        public int IPAddressesSupportingSsl { get; private set; }
+
+        /// <summary>
         /// Gets the number of IPv7 addresses that support TLS.
         /// </summary>
         public int IPAddressesSupportingTls { get; private set; }
+
+        /// <summary>
+        /// Returns whether the specified IPv7 address supports SSL.
+        /// </summary>
+        /// <param name="address">The IPv7 address to test for SSL.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="address"/> supports SSL; otherwise <see langword="false"/>.
+        /// </returns>
+        internal static bool DoesIPAddressSupportSsl(string address)
+        {
+            IList<string> supernets;
+            IList<string> hypernets;
+
+            ParseIPAddress(address, out supernets, out hypernets);
+
+            List<string> abas = new List<string>();
+
+            foreach (string value in supernets)
+            {
+                abas.AddRange(ExtractAbas(value));
+            }
+
+            if (abas.Count < 1)
+            {
+                return false;
+            }
+
+            foreach (string aba in abas)
+            {
+                string bab = new string(new[] { aba[1], aba[0], aba[1] });
+
+                foreach (string hypernet in hypernets)
+                {
+                    if (hypernet.Contains(bab))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
 
         /// <summary>
         /// Returns whether the specified IPv7 address supports TLS.
@@ -27,53 +74,15 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
         /// </returns>
         internal static bool DoesIPAddressSupportTls(string address)
         {
-            IList<string> nonhypernets = new List<string>();
-            IList<string> hypernets = new List<string>();
+            IList<string> supernets;
+            IList<string> hypernets;
 
-            StringBuilder builder = new StringBuilder();
+            ParseIPAddress(address, out supernets, out hypernets);
 
-            bool isInHypernet = false;
-
-            foreach (char ch in address)
-            {
-                if (isInHypernet && ch == ']')
-                {
-                    isInHypernet = false;
-
-                    if (builder.Length > 0)
-                    {
-                        hypernets.Add(builder.ToString());
-                        builder.Clear();
-                    }
-
-                    continue;
-                }
-                else if (ch == '[')
-                {
-                    isInHypernet = true;
-
-                    if (builder.Length > 0)
-                    {
-                        nonhypernets.Add(builder.ToString());
-                        builder.Clear();
-                    }
-
-                    continue;
-                }
-
-                builder.Append(ch);
-            }
-
-            if (builder.Length > 0)
-            {
-                nonhypernets.Add(builder.ToString());
-                builder.Clear();
-            }
-
-            bool foundAbba = nonhypernets.Any(DoesStringContainAbba);
+            bool foundAbbaInSupernet = supernets.Any(DoesStringContainAbba);
             bool foundAbbaInHypernet = hypernets.Any(DoesStringContainAbba);
 
-            return foundAbba && !foundAbbaInHypernet;
+            return foundAbbaInSupernet && !foundAbbaInHypernet;
         }
 
         /// <inheritdoc />
@@ -82,8 +91,10 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
             IList<string> addresses = ReadResourceAsLines();
 
             IPAddressesSupportingTls = addresses.Count(DoesIPAddressSupportTls);
+            IPAddressesSupportingSsl = addresses.Count(DoesIPAddressSupportSsl);
 
             Console.WriteLine("{0:N0} IPv7 addresses support TLS.", IPAddressesSupportingTls);
+            Console.WriteLine("{0:N0} IPv7 addresses support SSL.", IPAddressesSupportingSsl);
 
             return 0;
         }
@@ -111,6 +122,84 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Returns the ABAs in the specified <see cref="string"/>.
+        /// </summary>
+        /// <param name="value">The value to extract ABA values from.</param>
+        /// <returns>
+        /// An <see cref="IList{T}"/> containing the extracted ABA values.
+        /// </returns>
+        private static IList<string> ExtractAbas(string value)
+        {
+            IList<string> result = new List<string>();
+
+            for (int i = 0; i < value.Length - 2; i++)
+            {
+                char ch1 = value[i];
+                char ch2 = value[i + 1];
+                char ch3 = value[i + 2];
+
+                if (ch1 != ch2 && ch1 == ch3)
+                {
+                    result.Add(new string(new[] { ch1, ch2, ch3 }));
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Parses the specified IP address into supernets and hypernets.
+        /// </summary>
+        /// <param name="address">The IP addresses to parse.</param>
+        /// <param name="supernets">When the method returns, contains the supernets.</param>
+        /// <param name="hypernets">When the method returns, contains the hypernets.</param>
+        private static void ParseIPAddress(string address, out IList<string> supernets, out IList<string> hypernets)
+        {
+            supernets = new List<string>();
+            hypernets = new List<string>();
+
+            StringBuilder builder = new StringBuilder();
+
+            bool isInHypernet = false;
+
+            foreach (char ch in address)
+            {
+                if (isInHypernet && ch == ']')
+                {
+                    isInHypernet = false;
+
+                    if (builder.Length > 0)
+                    {
+                        hypernets.Add(builder.ToString());
+                        builder.Clear();
+                    }
+
+                    continue;
+                }
+                else if (ch == '[')
+                {
+                    isInHypernet = true;
+
+                    if (builder.Length > 0)
+                    {
+                        supernets.Add(builder.ToString());
+                        builder.Clear();
+                    }
+
+                    continue;
+                }
+
+                builder.Append(ch);
+            }
+
+            if (builder.Length > 0)
+            {
+                supernets.Add(builder.ToString());
+                builder.Clear();
+            }
         }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2016/Day07Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2016/Day07Tests.cs
@@ -11,6 +11,20 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
     public static class Day07Tests
     {
         [Theory]
+        [InlineData("aba[bab]xyz", true)]
+        [InlineData("xyx[xyx]xyx", false)]
+        [InlineData("aaa[kek]eke", true)]
+        [InlineData("zazbz[bzb]cdb", true)]
+        public static void Y2016_Day07_DoesIPAddressSupportSsl_Returns_Correct_Solution(string address, bool expected)
+        {
+            // Act
+            bool actual = Day07.DoesIPAddressSupportSsl(address);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
         [InlineData("abba[mnop]qrst", true)]
         [InlineData("abcd[bddb]xyyx", false)]
         [InlineData("aaaa[qwer]tyui", false)]
@@ -32,6 +46,7 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
 
             // Assert
             Assert.Equal(118, puzzle.IPAddressesSupportingTls);
+            Assert.Equal(260, puzzle.IPAddressesSupportingSsl);
         }
     }
 }
