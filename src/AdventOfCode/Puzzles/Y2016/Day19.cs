feab373b099cfb83d4e4179abbf173ce3a75899b
martincostello <martin@martincostello.com>
2016-12-29 17:54:04 +0000

    Solve day 19 part 2
    Naive solution to part 2 of the puzzle for day 19 that is very slow.

--- src/AdventOfCode/Puzzles/Y2016/Day19.cs
+++ src/AdventOfCode/Puzzles/Y2016/Day19.cs
@@ -23,57 +23,114 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
         /// Finds the elf that receives all of the presents.
         /// </summary>
         /// <param name="count">The number of elves participating.</param>
+        /// <param name="version">The version of the rules to use.</param>
         /// <returns>
         /// The number of the elf that receives all the presents.
         /// </returns>
-        internal static int FindElfThatGetsAllPresents(int count)
+        internal static int FindElfThatGetsAllPresents(int count, int version)
         {
-            LinkedList<int> circle = new LinkedList<int>();
-
-            var first = circle.AddFirst(1);
-            var current = first;
-
-            while (circle.Count < count)
-            {
-                current = circle.AddAfter(current, circle.Count + 1);
-            }
-
-            current = first;
-
-            while (circle.Count > 1)
-            {
-                if (current == circle.Last)
-                {
-                    circle.Remove(circle.First);
-                }
-                else
-                {
-                    circle.Remove(current.Next);
-                }
-
-                if (current == circle.Last)
-                {
-                    current = circle.First;
-                }
-                else
-                {
-                    current = current.Next;
-                }
-            }
-
-            return circle.First.Value;
+            return
+                version == 2 ?
+                FindElfThatGetsAllPresentsV2(count) :
+                FindElfThatGetsAllPresentsV1(count);
         }
 
         /// <inheritdoc />
         protected override int SolveCore(string[] args)
         {
             int count = ParseInt32(args[0]);
+            int version = args.Length > 1 ? ParseInt32(args[1]) : 1;
 
-            ElfWithAllPresents = FindElfThatGetsAllPresents(count);
+            ElfWithAllPresents = FindElfThatGetsAllPresents(count, version);
 
-            Console.WriteLine($"The elf that gets all the presents is {ElfWithAllPresents:N0}.");
+            Console.WriteLine($"The elf that gets all the presents using version {version} of the rules is {ElfWithAllPresents:N0}.");
 
             return 0;
+        }
+
+        /// <summary>
+        /// Finds the elf that receives all of the presents using version 1 of the rules.
+        /// </summary>
+        /// <param name="count">The number of elves participating.</param>
+        /// <returns>
+        /// The number of the elf that receives all the presents.
+        /// </returns>
+        private static int FindElfThatGetsAllPresentsV1(int count)
+        {
+            var circle = CreateCircle(count);
+            var current = circle.First;
+
+            while (circle.Count > 1)
+            {
+                circle.Remove(current.Next ?? circle.First);
+                current = current.Next ?? circle.First;
+            }
+
+            return circle.First.Value;
+        }
+
+        /// <summary>
+        /// Finds the elf that receives all of the presents using version 2 of the rules.
+        /// </summary>
+        /// <param name="count">The number of elves participating.</param>
+        /// <returns>
+        /// The number of the elf that receives all the presents.
+        /// </returns>
+        private static int FindElfThatGetsAllPresentsV2(int count)
+        {
+            var circle = CreateCircle(count);
+            var current = circle.First;
+
+            while (circle.Count > 1)
+            {
+                var opposite = FindOppositeElf(current, circle);
+                circle.Remove(opposite);
+
+                current = current.Next ?? circle.First;
+            }
+
+            return circle.First.Value;
+        }
+
+        /// <summary>
+        /// Finds the elf sitting opposite the specified elf.
+        /// </summary>
+        /// <param name="current">The number of the elf to find the opposite elf for.</param>
+        /// <param name="circle">The circle of elves.</param>
+        /// <returns>
+        /// The <see cref="LinkedListNode{T}"/> representing the elf opposite the
+        /// elf with the number specified by <paramref name="current"/>.
+        /// </returns>
+        private static LinkedListNode<int> FindOppositeElf(LinkedListNode<int> current, LinkedList<int> circle)
+        {
+            int steps = circle.Count / 2;
+
+            for (int i = 0; i < steps; i++)
+            {
+                current = current.Next ?? circle.First;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// Creates the circle of elves.
+        /// </summary>
+        /// <param name="count">The number of elves in the circle.</param>
+        /// <returns>
+        /// The <see cref="LinkedList{T}"/> containing the number of elves specified by <paramref name="count"/>.
+        /// </returns>
+        private static LinkedList<int> CreateCircle(int count)
+        {
+            LinkedList<int> circle = new LinkedList<int>();
+            var current = circle.AddFirst(1);
+
+            while (circle.Count < count)
+            {
+                current = circle.AddAfter(current, circle.Count + 1);
+            }
+
+            return circle;
         }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2016/Day19Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2016/Day19Tests.cs
@@ -11,23 +11,26 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2016
     public static class Day19Tests
     {
         [Theory]
-        [InlineData(5, 3)]
-        public static void Y2016_Day19_FindElfThatGetsAllPresents_Returns_Correct_Solution(int count, int expected)
+        [InlineData(5, 1, 3)]
+        [InlineData(5, 2, 2)]
+        public static void Y2016_Day19_FindElfThatGetsAllPresents_Returns_Correct_Solution(int count, int version, int expected)
         {
             // Act
-            int actual = Day19.FindElfThatGetsAllPresents(count);
+            int actual = Day19.FindElfThatGetsAllPresents(count, version);
 
             // Assert
             Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("5", 3)]
-        [InlineData("3014387", 1834471)]
-        public static void Y2016_Day19_Solve_Returns_Correct_Solution(string elves, int expected)
+        [InlineData("5", "1", 3)]
+        [InlineData("5", "2", 2)]
+        [InlineData("3014387", "1", 1834471)]
+        [InlineData("3014387", "2", 0, Skip = "Too slow.")]
+        public static void Y2016_Day19_Solve_Returns_Correct_Solution(string elves, string version, int expected)
         {
             // Arrange
-            string[] args = new[] { elves };
+            string[] args = new[] { elves, version };
 
             // Act
             var puzzle = PuzzleTestHelpers.SolvePuzzle<Day19>(args);
