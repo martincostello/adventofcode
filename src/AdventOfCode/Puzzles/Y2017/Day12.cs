0e641a4f5f43701b79ef8746887ea023e807c0ab
martincostello <martin@martincostello.com>
2017-12-16 13:25:34 +0000

    Solve day 12 part 1
    Solve part 1 of the puzzle for day 12.

---
+++ src/AdventOfCode/Puzzles/Y2017/Day12.cs
@@ -0,0 +1,123 @@
+// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2017
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2017/day/12</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day12 : Puzzle2017
+    {
+        /// <summary>
+        /// Gets the number of programs in the group that contains program zero.
+        /// </summary>
+        public int ProgramsInGroupOfProgram0 { get; private set; }
+
+        /// <summary>
+        /// Gets the number of programs in the same group as the specified program Id.
+        /// </summary>
+        /// <param name="programId">The Id of the program to find the number of programs in its group.</param>
+        /// <param name="pipes">A collection of strings describing the pipes connecting the programs.</param>
+        /// <returns>
+        /// The number of programs in the same group as the program with the Id specified by <paramref name="programId"/>.
+        /// </returns>
+        public static int GetProgramsInGroup(int programId, ICollection<string> pipes)
+        {
+            var graph = pipes.Select((p) => new Node(p)).ToDictionary((p) => p.Id, (p) => p);
+
+            foreach (Node node in graph.Values)
+            {
+                foreach (int edge in node.EdgeIds)
+                {
+                    node.Edges.Add(graph[edge]);
+                }
+            }
+
+            var target = graph[programId];
+
+            var members = new List<Node>(graph.Count);
+            members.Add(target);
+
+            foreach (Node edge in target.Edges)
+            {
+                Visit(edge, members);
+            }
+
+            return members.Count;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> pipes = ReadResourceAsLines();
+
+            ProgramsInGroupOfProgram0 = GetProgramsInGroup(0, pipes);
+
+            Console.WriteLine($"There are {ProgramsInGroupOfProgram0:N0} programs in the group that contains program ID 0.");
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Visits the specified node.
+        /// </summary>
+        /// <param name="node">The node to visit.</param>
+        /// <param name="members">The current members of the group of the target node.</param>
+        private static void Visit(Node node, ICollection<Node> members)
+        {
+            if (!members.Contains(node))
+            {
+                members.Add(node);
+            }
+
+            foreach (var edge in node.Edges)
+            {
+                if (!members.Contains(edge))
+                {
+                    Visit(edge, members);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A class representing a node in the network of programs.
+        /// </summary>
+        private sealed class Node
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Node"/> class.
+            /// </summary>
+            /// <param name="pipe">A string describing the pipes connecting the node with its edges.</param>
+            internal Node(string pipe)
+            {
+                string[] split = pipe.Split(Arrays.Space);
+
+                Id = ParseInt32(split[0]);
+
+                EdgeIds = split
+                    .Skip(2)
+                    .Select((p) => ParseInt32(p.TrimEnd(Arrays.Comma)))
+                    .ToList();
+            }
+
+            /// <summary>
+            /// Gets the Id of the node.
+            /// </summary>
+            public int Id { get; }
+
+            /// <summary>
+            /// Gets the Ids of the edges of the node.
+            /// </summary>
+            public ICollection<int> EdgeIds { get; }
+
+            /// <summary>
+            /// Gets the edges of the node.
+            /// </summary>
+            public ICollection<Node> Edges { get; } = new List<Node>();
+        }
+    }
+}
