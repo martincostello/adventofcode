4049da7a1a1494704010eba9cfa06461e18ed10b
martincostello <martin@martincostello.com>
2018-12-31 16:48:50 +0000

    Solve day 2 part 1
    Solve part 1 of day 2 for 2018.

---
+++ src/AdventOfCode/Puzzles/Y2018/Day02.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2018/day/2</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day02 : Puzzle2018
+    {
+        /// <summary>
+        /// Gets the checksum of the box ids.
+        /// </summary>
+        public int Checksum { get; private set; }
+
+        /// <summary>
+        /// Calculates the checksum for the specified box Ids.
+        /// </summary>
+        /// <param name="boxIds">The box Ids to calculate the checksum for.</param>
+        /// <returns>
+        /// The checksum calculated from <paramref name="boxIds"/>.
+        /// </returns>
+        public static int CalculateChecksum(IEnumerable<string> boxIds)
+        {
+            int count2 = 0;
+            int count3 = 0;
+
+            foreach (string id in boxIds)
+            {
+                (int c2, int c3) = GetBoxScore(id);
+
+                count2 += c2;
+                count3 += c3;
+            }
+
+            return count2 * count3;
+        }
+
+        /// <summary>
+        /// Gets the scores for the box with the specified Id
+        /// </summary>
+        /// <param name="id">The box Id to get the score for.</param>
+        /// <returns>
+        /// The scores of the specified Id.
+        /// </returns>
+        public static(int count2, int count3) GetBoxScore(string id)
+        {
+            var counts = new Dictionary<char, int>();
+
+            foreach (char ch in id)
+            {
+                if (!counts.ContainsKey(ch))
+                {
+                    counts[ch] = 1;
+                }
+                else
+                {
+                    counts[ch]++;
+                }
+            }
+
+            bool hasDoubles = counts.Values.Any((p) => p == 2);
+            bool hasTriples = counts.Values.Any((p) => p == 3);
+
+            return (hasDoubles ? 1 : 0, hasTriples ? 1 : 0);
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> ids = ReadResourceAsLines();
+
+            Checksum = CalculateChecksum(ids);
+
+            if (Verbose)
+            {
+                Logger.WriteLine($"The checksum is {Checksum:N0}.");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2018/Day02Tests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day02"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day02Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day02Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day02Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData("abcdef", 0, 0)]
+        [InlineData("bababc", 1, 1)]
+        [InlineData("abbcde", 1, 0)]
+        [InlineData("abcccd", 0, 1)]
+        [InlineData("aabcdd", 1, 0)]
+        [InlineData("abcdee", 1, 0)]
+        [InlineData("ababab", 0, 1)]
+        public static void Y2018_Day02_GetBoxScore_Returns_Correct_Solution(
+            string id,
+            int expected2,
+            int expected3)
+        {
+            // Act
+            (int actual2, int actual3) = Day02.GetBoxScore(id);
+
+            // Assert
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+        }
+
+        [Theory]
+        [InlineData(new[] { "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" }, 12)]
+        public static void Y2018_Day02_CalculateChecksum_Returns_Correct_Solution(
+            string[] boxIds,
+            int expected)
+        {
+            // Act
+            int actual = Day02.CalculateChecksum(boxIds);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Y2018_Day02_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day02>();
+
+            // Assert
+            Assert.Equal(5880, puzzle.Checksum);
+        }
+    }
+}
