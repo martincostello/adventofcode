3a346c31ab630113519fa345259f44d64a58d255
martincostello <martin@martincostello.com>
2018-12-31 14:05:32 +0000

    Solve day 1 part 1
    Solve part 1 of day 1 for 2018.

---
+++ src/AdventOfCode/Puzzles/Y2018/Day01.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2018/day/1</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day01 : Puzzle2018
+    {
+        /// <summary>
+        /// Gets the frequency of the device calculated from the sequence.
+        /// </summary>
+        public int Frequency { get; private set; }
+
+        /// <summary>
+        /// Calculates the resulting frequency after applying the specified sequence.
+        /// </summary>
+        /// <param name="sequence">A sequence of frequency shifts to apply.</param>
+        /// <returns>
+        /// The resulting frequency from applying the sequence from <paramref name="sequence"/>.
+        /// </returns>
+        public static int CalculateFrequency(IEnumerable<string> sequence)
+        {
+            return sequence.Select((p) => ParseInt32(p)).Sum();
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> sequence = ReadResourceAsLines();
+
+            Frequency = CalculateFrequency(sequence);
+
+            if (Verbose)
+            {
+                Console.WriteLine($"The resulting frequency is {Frequency:N0}.");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2018/Day01Tests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day01"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Day01Tests
+    {
+        [Theory]
+        [InlineData(new[] { "+1", "-2", "+3", "+1" }, 3)]
+        [InlineData(new[] { "+1", "+1", "+1" }, 3)]
+        [InlineData(new[] { "+1", "+1", "-2" }, 0)]
+        [InlineData(new[] { "-1", "-2", "-3" }, -6)]
+        public static void Y2018_Day01_CalculateFrequency_Returns_Correct_Solution(
+            string[] sequence,
+            int expected)
+        {
+            // Act
+            int actual = Day01.CalculateFrequency(sequence);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Y2018_Day01_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = PuzzleTestHelpers.SolvePuzzle<Day01>();
+
+            // Assert
+            Assert.Equal(543, puzzle.Frequency);
+        }
+    }
+}
