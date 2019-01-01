1bd1075e925722defa06ac1b194ab7ae786a697c
martincostello <martin@martincostello.com>
2019-01-01 11:14:13 +0000

    Solve day 4 part 1
    Solve part 1 of day 4 for 2018.

---
+++ src/AdventOfCode/Puzzles/Y2018/Day04.cs
@@ -0,0 +1,206 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2018/day/4</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Day04 : Puzzle2018
+    {
+        /// <summary>
+        /// Gets the product of the guard who slept the most and the minute they were asleep the most.
+        /// </summary>
+        public int SleepiestGuardMinute { get; private set; }
+
+        /// <summary>
+        /// Calculates the product of the guard who slept the most and the minute they were asleep the most from the specified log.
+        /// </summary>
+        /// <param name="log">The log of guard activity.</param>
+        /// <returns>
+        /// The product of the guard who slept the most and the minute they were asleep the most as specified by <paramref name="log"/>.
+        /// </returns>
+        public static int GetSleepiestGuardMinute(IEnumerable<string> log)
+        {
+            var parsedAndSortedLog = log
+                .Select(LogEntry.Parse)
+                .OrderBy((p) => p.Timestamp)
+                .ToList();
+
+            var groupedLog = parsedAndSortedLog.GroupBy((p) => p.Timestamp.Date);
+
+            var guardsAsleep = groupedLog
+                .Select((p) => p.Key)
+                .Distinct()
+                .ToDictionary((k) => k, (v) => new int[60]);
+
+            var first = groupedLog.First().First();
+            int lastGuard = first.Id.Value;
+            var lastTimestamp = first.Timestamp;
+            bool isAwake = true;
+
+            var midnight = new TimeSpan(0, 0, 0);
+            var oneAM = new TimeSpan(1, 0, 0);
+            var oneMinute = TimeSpan.FromMinutes(1);
+
+            foreach (var day in groupedLog)
+            {
+                foreach (var entry in day)
+                {
+                    if (!isAwake &&
+                        lastTimestamp.TimeOfDay >= midnight &&
+                        lastTimestamp.TimeOfDay < oneAM)
+                    {
+                        var time = lastTimestamp;
+                        var midnightHour = guardsAsleep[day.Key];
+
+                        while (time.TimeOfDay < oneAM && time < entry.Timestamp)
+                        {
+                            midnightHour[time.TimeOfDay.Minutes] = lastGuard;
+                            time = time.Add(oneMinute);
+                        }
+                    }
+
+                    if (entry.Wakefulness.HasValue)
+                    {
+                        isAwake = entry.Wakefulness.Value;
+                    }
+                    else if (entry.Id.HasValue)
+                    {
+                        lastGuard = entry.Id.Value;
+                    }
+
+                    lastTimestamp = entry.Timestamp;
+                }
+            }
+
+            var sleepinessPerGuard = parsedAndSortedLog
+                .Where((p) => p.Id.HasValue)
+                .Select((p) => p.Id.Value)
+                .Distinct()
+                .ToDictionary((k) => k, (v) => 0);
+
+            foreach (var activity in guardsAsleep)
+            {
+                foreach (int guard in activity.Value.Where((p) => p != 0))
+                {
+                    sleepinessPerGuard[guard]++;
+                }
+            }
+
+            int sleepiestGuard = sleepinessPerGuard
+                .OrderByDescending((p) => p.Value)
+                .Select((p) => p.Key)
+                .First();
+
+            var minutesWhereAsleep = new Dictionary<int, int>();
+
+            foreach (var pair in guardsAsleep)
+            {
+                for (int i = 0; i < pair.Value.Length; i++)
+                {
+                    bool isSleepiest = pair.Value[i] == sleepiestGuard;
+
+                    if (isSleepiest)
+                    {
+                        if (!minutesWhereAsleep.ContainsKey(i))
+                        {
+                            minutesWhereAsleep.Add(i, 0);
+                        }
+
+                        minutesWhereAsleep[i]++;
+                    }
+                }
+            }
+
+            int sleepiestMinute = minutesWhereAsleep
+                .OrderByDescending((p) => p.Value)
+                .Select((p) => p.Key)
+                .First();
+
+            return sleepiestGuard * sleepiestMinute;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> log = ReadResourceAsLines();
+
+            SleepiestGuardMinute = GetSleepiestGuardMinute(log);
+
+            if (Verbose)
+            {
+                Logger.WriteLine($"The ID of the sleepiest guard multiplied by the most common minute is {SleepiestGuardMinute:N0}.");
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// A class representing a log entry. This class cannot be inherited.
+        /// </summary>
+        private sealed class LogEntry
+        {
+            /// <summary>
+            /// Gets the timestamp of the log entry.
+            /// </summary>
+            internal DateTime Timestamp { get; private set; }
+
+            /// <summary>
+            /// Gets the Id of the guard associated with the log entry, if any.
+            /// </summary>
+            internal int? Id { get; private set; }
+
+            /// <summary>
+            /// Gets the wakefulness of the guard.
+            /// </summary>
+            internal bool? Wakefulness { get; private set; }
+
+            /// <summary>
+            /// Parses a string to an instance of <see cref="LogEntry"/>.
+            /// </summary>
+            /// <param name="entry">The log entry to parse.</param>
+            /// <returns>
+            /// The instance of <see cref="LogEntry"/> representing <paramref name="entry"/>.
+            /// </returns>
+            internal static LogEntry Parse(string entry)
+            {
+                string[] split = entry.Split(' ');
+
+                string date1 = split[0].TrimStart('[');
+                string date2 = split[1].TrimEnd(']');
+
+                var timestamp = DateTime.ParseExact(date1 + " " + date2, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+                string firstWord = split[2];
+
+                int? id = null;
+                bool? wakefulness = null;
+
+                if (string.Equals(firstWord, "wakes", StringComparison.Ordinal))
+                {
+                    wakefulness = true;
+                }
+                else if (string.Equals(firstWord, "falls", StringComparison.Ordinal))
+                {
+                    wakefulness = false;
+                }
+                else
+                {
+                    id = ParseInt32(split[3].TrimStart('#'));
+                }
+
+                return new LogEntry()
+                {
+                    Timestamp = timestamp,
+                    Id = id,
+                    Wakefulness = wakefulness,
+                };
+            }
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2018/Day04Tests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day04"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day04Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day04Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day04Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public static void Y2018_Day04_GetSleepiestGuardMinute_Returns_Correct_Solution()
+        {
+            // Arrange
+            int expected = 240;
+
+            var log = new[]
+            {
+                "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up",
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-03 00:05] Guard #10 begins shift",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes up",
+            };
+
+            // Act
+            int actual = Day04.GetSleepiestGuardMinute(log);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Y2018_Day04_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day04>();
+
+            // Assert
+            Assert.Equal(4716, puzzle.SleepiestGuardMinute);
+        }
+    }
+}
