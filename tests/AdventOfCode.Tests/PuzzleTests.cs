039715f49227ee911bb8f78df7a848efeac62d13
martincostello <martin@martincostello.com>
2019-01-01 20:54:21 +0000

    Route console to xunit
    Re-route console logs to xunit for Puzzle's unit tests.

--- tests/AdventOfCode.Tests/PuzzleTests.cs
+++ tests/AdventOfCode.Tests/PuzzleTests.cs
@@ -1,22 +1,41 @@
-// Copyright (c) Martin Costello, 2015. All rights reserved.
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 namespace MartinCostello.AdventOfCode
 {
     using System;
     using Xunit;
+    using Xunit.Abstractions;
 
     /// <summary>
     /// A class containing tests for the <see cref="Puzzle"/> class. This class cannot be inherited.
     /// </summary>
-    public static class PuzzleTests
+    public sealed class PuzzleTests
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PuzzleTests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public PuzzleTests(ITestOutputHelper outputHelper)
+        {
+            Logger = new TestLogger(outputHelper);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ILogger"/> to use.
+        /// </summary>
+        private ILogger Logger { get; }
+
         [Fact]
-        public static void Puzzle_Solve_Returns_Correct_Value_Based_On_Args_Length()
+        public void Puzzle_Solve_Returns_Correct_Value_Based_On_Args_Length()
         {
             // Arrange
             string[] args = new[] { "1" };
-            var target = new MyPuzzle(2);
+
+            var target = new MyPuzzle(2)
+            {
+                Logger = Logger,
+            };
 
             // Act
             int actual = target.Solve(args);
