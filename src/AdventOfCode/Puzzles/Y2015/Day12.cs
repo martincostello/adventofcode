70e23f73c2f336b785db65d58833bdeb385ee4d9
martincostello <martin@martincostello.com>
2019-08-18 16:54:17 +0100

    Use System.Text.Json
    Use System.Text.Json instead of Newtonsoft.Json.

--- src/AdventOfCode/Puzzles/Y2015/Day12.cs
+++ src/AdventOfCode/Puzzles/Y2015/Day12.cs
@@ -3,8 +3,7 @@
 
 namespace MartinCostello.AdventOfCode.Puzzles.Y2015
 {
-    using System.Linq;
-    using Newtonsoft.Json.Linq;
+    using System.Text.Json;
 
     /// <summary>
     /// A class representing the puzzle for <c>http://adventofcode.com/2015/day/12</c>. This class cannot be inherited.
@@ -17,47 +16,38 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2015
         internal long Sum { get; private set; }
 
         /// <summary>
-        /// Sums the integer values in the specified JSON token, ignoring any values from
-        /// child tokens that contain the specified string value, if specified.
+        /// Sums the integer values in the specified JSON element, ignoring any values from
+        /// child elements that contain the specified string value, if specified.
         /// </summary>
-        /// <param name="token">The JSON token.</param>
-        /// <param name="valueToIgnore">The tokens to ignore if they contain this value.</param>
-        /// <returns>The sum of the tokens in <paramref name="token"/>.</returns>
-        internal static long SumIntegerValues(JToken token, string valueToIgnore)
+        /// <param name="element">The JSON element.</param>
+        /// <param name="valueToIgnore">The elements to ignore if they contain this value.</param>
+        /// <returns>The sum of the elements in <paramref name="element"/>.</returns>
+        internal static long SumIntegerValues(JsonElement element, string valueToIgnore)
         {
             long sum = 0;
 
-            if (token is JValue value && value.Type == JTokenType.Integer)
+            if (element.ValueKind == JsonValueKind.Number)
             {
-                sum = (long)value.Value;
+                sum = element.GetInt64();
             }
-            else
+            else if (element.ValueKind == JsonValueKind.Array)
             {
-                JToken[] children = token
-                    .Children()
-                    .ToArray();
-
-                bool ignore = children
-                    .OfType<JProperty>()
-                    .Where((p) => p.Value.Type == JTokenType.String)
-                    .Where((p) => (string)((JValue)p.Value).Value == valueToIgnore)
-                    .Any();
-
-                if (!ignore)
+                foreach (JsonElement child in element.EnumerateArray())
                 {
-                    foreach (JToken child in children)
+                    sum += SumIntegerValues(child, valueToIgnore);
+                }
+            }
+            else if (element.ValueKind == JsonValueKind.Object)
+            {
+                foreach (JsonProperty child in element.EnumerateObject())
+                {
+                    if (child.Value.ValueKind == JsonValueKind.String &&
+                        child.Value.GetString() == valueToIgnore)
                     {
-                        ignore = child
-                            .Children<JProperty>()
-                            .Where((p) => p.Value.Type == JTokenType.String)
-                            .Where((p) => (string)((JValue)p.Value).Value == valueToIgnore)
-                            .Any();
-
-                        if (!ignore)
-                        {
-                            sum += SumIntegerValues(child, valueToIgnore);
-                        }
+                        return 0;
                     }
+
+                    sum += SumIntegerValues(child.Value, valueToIgnore);
                 }
             }
 
@@ -67,10 +57,10 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2015
         /// <inheritdoc />
         protected override int SolveCore(string[] args)
         {
-            var document = JToken.Parse(ReadResourceAsString());
+            using var document = JsonDocument.Parse(ReadResource());
             string keyToIgnore = args.Length > 0 ? args[0] : string.Empty;
 
-            Sum = SumIntegerValues(document, keyToIgnore);
+            Sum = SumIntegerValues(document.RootElement, keyToIgnore);
 
             if (Verbose)
             {
