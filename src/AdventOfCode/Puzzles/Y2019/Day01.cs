781e1c396d1a4af7b7fed634bfc3cc2fdda5465f
martincostello <martin@martincostello.com>
2019-12-01 09:58:54 +0000

    Solve day 1 part 1
    Solve part 1 of day 1 for 2019.

---
+++ src/AdventOfCode/Puzzles/Y2019/Day01.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>http://adventofcode.com/2019/day/1</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day01 : Puzzle2019
+    {
+        /// <summary>
+        /// Gets the total fuel requirement.
+        /// </summary>
+        public double TotalFuelRequired { get; private set; }
+
+        /// <summary>
+        /// Gets the fuel requirements for the specified mass.
+        /// </summary>
+        /// <param name="mass">The mass to get the fuel requirements for.</param>
+        /// <returns>
+        /// The fuel requirements for the mass specified by <paramref name="mass"/>.
+        /// </returns>
+        public static double GetFuelRequirementsForModule(string mass)
+        {
+            int massValue = ParseInt32(mass);
+
+            double requirement = massValue / 3.0;
+
+            requirement = Math.Floor(requirement);
+
+            requirement -= 2;
+
+            return requirement;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> masses = ReadResourceAsLines();
+
+            double total = 0;
+
+            foreach (string mass in masses)
+            {
+                total += GetFuelRequirementsForModule(mass);
+            }
+
+            TotalFuelRequired = total;
+
+            if (Verbose)
+            {
+                Logger.WriteLine("{0} fuel is required for the modules.", TotalFuelRequired);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2019/Day01Tests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day01"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day01Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day01Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day01Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData("12", 2)]
+        [InlineData("14", 2)]
+        [InlineData("1969", 654)]
+        [InlineData("100756", 33583)]
+        public static void Y2019_Day01_GetFuelRequirementsForModule(string mass, double expected)
+        {
+            // Act
+            double actual = Day01.GetFuelRequirementsForModule(mass);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Y2019_Day01_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day01>();
+
+            // Assert
+            Assert.Equal(3226407, puzzle.TotalFuelRequired);
+        }
+    }
+}
