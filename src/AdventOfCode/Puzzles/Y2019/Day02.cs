c2130164496471dfdbca0d272d367731f0bd6035
martincostello <martin@martincostello.com>
2019-12-03 13:17:52 +0000

    Solve day 2 part 1
    Solve part 1 of day 2 for 2019.

--- src/AdventOfCode/Puzzles/Y2019/Day02.cs
+++ src/AdventOfCode/Puzzles/Y2019/Day02.cs
@@ -3,17 +3,75 @@
 
 namespace MartinCostello.AdventOfCode.Puzzles.Y2019
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     /// <summary>
     /// A class representing the puzzle for <c>http://adventofcode.com/2019/day/2</c>. This class cannot be inherited.
     /// </summary>
     public sealed class Day02 : Puzzle2019
     {
+        /// <summary>
+        /// Gets the output of the program.
+        /// </summary>
+        public IList<int> Output { get; private set; } = Array.Empty<int>();
+
+        /// <summary>
+        /// Runs the specified Intcode program.
+        /// </summary>
+        /// <param name="program">The Intcode program to run.</param>
+        /// <param name="adjust">Whether to adjust the state for <c>1202 program alarm</c> before running.</param>
+        /// <returns>
+        /// The memory values of the program once run.
+        /// </returns>
+        public static IList<int> RunProgram(string program, bool adjust = false)
+        {
+            int[] memory = program
+                .Split(',')
+                .Select((p) => ParseInt32(p))
+                .ToArray();
+
+            if (adjust)
+            {
+                memory[1] = 12;
+                memory[2] = 2;
+            }
+
+            for (int i = 0; i < memory.Length; i += 4)
+            {
+                int opcode = memory[i];
+
+                if (opcode == 99)
+                {
+                    break;
+                }
+
+                int first = memory[i + 1];
+                int second = memory[i + 2];
+                int address = memory[i + 3];
+
+                memory[address] = opcode switch
+                {
+                    1 => memory[first] + memory[second],
+                    2 => memory[first] * memory[second],
+                    _ => throw new InvalidOperationException($"{opcode} is not a supported opcode."),
+                };
+            }
+
+            return memory;
+        }
+
         /// <inheritdoc />
         protected override int SolveCore(string[] args)
         {
+            string program = ReadResourceAsString();
+
+            Output = RunProgram(program, adjust: true);
+
             if (Verbose)
             {
-                Logger.WriteLine();
+                Logger.WriteLine("The value at position 0 after the program halts is {0}.", Output[0]);
             }
 
             return 0;
--- tests/AdventOfCode.Tests/Puzzles/Y2019/Day02Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2019/Day02Tests.cs
@@ -3,6 +3,7 @@
 
 namespace MartinCostello.AdventOfCode.Puzzles.Y2019
 {
+    using System.Collections.Generic;
     using Xunit;
     using Xunit.Abstractions;
 
@@ -20,6 +21,21 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2019
         {
         }
 
+        [Theory]
+        [InlineData("1,9,10,3,2,3,11,0,99,30,40,50", new[] { 3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50 })]
+        [InlineData("1,0,0,0,99", new[] { 2, 0, 0, 0, 99 })]
+        [InlineData("2,3,0,3,99", new[] { 2, 3, 0, 6, 99 })]
+        [InlineData("2,4,4,5,99,0", new[] { 2, 4, 4, 5, 99, 9801 })]
+        [InlineData("1,1,1,4,99,5,6,0,99", new[] { 30, 1, 1, 4, 2, 5, 6, 0, 99 })]
+        public void Y2019_Day02_RunProgram_Returns_Correct_State(string program, int[] expected)
+        {
+            // Act
+            IList<int> actual = Day02.RunProgram(program);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
         [Fact]
         public void Y2019_Day02_Solve_Returns_Correct_Solution()
         {
@@ -27,7 +43,9 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2019
             var puzzle = SolvePuzzle<Day02>();
 
             // Assert
-            Assert.NotNull(puzzle);
+            Assert.NotNull(puzzle.Output);
+            Assert.NotEmpty(puzzle.Output);
+            Assert.Equal(9581917, puzzle.Output[0]);
         }
     }
 }
