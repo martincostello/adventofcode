d85361d732d471da2ffcbdf3ecef55311ba1c483
martincostello <martin@martincostello.com>
2019-12-07 19:23:27 +0000

    Solve day 4 part 1
    Solve part 1 of day 4 for 2019.

---
+++ src/AdventOfCode/Puzzles/Y2019/Day04.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2019/day/4</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day04 : Puzzle2019
+    {
+        /// <summary>
+        /// Gets the number of valid passwords in the given range.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <summary>
+        /// Gets the number of valid passwords in the specified range.
+        /// </summary>
+        /// <param name="range">The range of numbers to get the number of passwords for.</param>
+        /// <returns>
+        /// The Manhattan distance from the central port to the closest intersection and the
+        /// minimum number of combined steps to reach an intersection.
+        /// </returns>
+        public static int GetPasswordsInRange(string range)
+        {
+            string[] split = range.Split('-');
+
+            int start = ParseInt32(split[0]);
+            int end = ParseInt32(split[1]) + 1;
+
+            int count = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                if (IsValid(i.ToString(CultureInfo.InvariantCulture)))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Determines whether the specified password is valid.
+        /// </summary>
+        /// <param name="password">The password to test for validity.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="password"/> is valid; otherwise <see langword="false"/>.
+        /// </returns>
+        public static bool IsValid(string password)
+        {
+            bool foundAdjacent = false;
+
+            for (int i = 0; i < password.Length - 1; i++)
+            {
+                int first = password[i] - '0';
+                int second = password[i + 1] - '0';
+
+                if (first == second)
+                {
+                    foundAdjacent = true;
+                }
+
+                if (first > second)
+                {
+                    return false;
+                }
+            }
+
+            return foundAdjacent;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            Count = GetPasswordsInRange(args[0]);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("{0} different passwords within the range meet the criteria.", Count);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2019/Day04Tests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day04"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day04Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day04Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day04Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData("111111", true)]
+        [InlineData("111123", true)]
+        [InlineData("122345", true)]
+        [InlineData("135679", false)]
+        [InlineData("123789", false)]
+        [InlineData("223450", false)]
+        public void Y2019_Day04_Is_Valid_Returns_Correct_Value(string password, bool expected)
+        {
+            // Act
+            bool actual = Day04.IsValid(password);
+
+            // Assert
+            expected.ShouldBe(actual);
+        }
+
+        [Fact]
+        public void Y2019_Day04_Solve_Returns_Correct_Solution()
+        {
+            // Arrange
+            string[] args = new[] { "138241-674034" };
+
+            // Act
+            var puzzle = SolvePuzzle<Day04>(args);
+
+            // Assert
+            puzzle.Count.ShouldBe(1890);
+        }
+    }
+}
