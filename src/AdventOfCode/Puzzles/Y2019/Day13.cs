d87648884afb06c94c9ee673225be463ac0a8974
martincostello <martin@martincostello.com>
2019-12-15 22:16:36 +0000

    Solve day 13 part 1
    Solve part 1 of day 13 for 2019.

---
+++ src/AdventOfCode/Puzzles/Y2019/Day13.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2019/day/13</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day13 : Puzzle2019
+    {
+        /// <summary>
+        /// Gets the number of block tiles on the screen.
+        /// </summary>
+        public long BlockTileCount { get; private set; }
+
+        /// <summary>
+        /// Gets the number of block tiles on the screen after the game is run.
+        /// </summary>
+        /// <param name="program">The Intcode program to run.</param>
+        /// <returns>
+        /// The number of block tiles on the screen.
+        /// </returns>
+        public static async Task<long> GetBlockTileCountAsync(string program)
+        {
+            long[] instructions = IntcodeVM.ParseProgram(program);
+
+            var vm = new IntcodeVM(instructions, 10_000);
+
+            if (!await vm.RunAsync())
+            {
+                throw new InvalidProgramException();
+            }
+
+            var outputs = await vm.Output.ToListAsync();
+
+            int blockTiles = 0;
+
+            for (int i = 0; i < outputs.Count; i += 3)
+            {
+                long tileId = outputs[i + 2];
+
+                if (tileId == 2)
+                {
+                    blockTiles++;
+                }
+            }
+
+            return blockTiles;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            string program = ReadResourceAsString();
+
+            BlockTileCount = GetBlockTileCountAsync(program).Result;
+
+            if (Verbose)
+            {
+                Logger.WriteLine("There are {0} block tiles on the screen when the game exits.", BlockTileCount);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2019/Day13Tests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2019
+{
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day13"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day13Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day13Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day13Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void Y2019_Day13_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day13>();
+
+            // Assert
+            puzzle.BlockTileCount.ShouldBe(315);
+        }
+    }
+}
