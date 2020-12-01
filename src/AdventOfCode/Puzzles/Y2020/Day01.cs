dc9e264d1ac42b9385932d54f891b2b23839ce0a
martincostello <martin@martincostello.com>
2020-12-01 17:31:57 +0000

    Solve day 1 part 1
    Solve part 1 of day 1 for 2020.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day01.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/1</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day01 : Puzzle2020
+    {
+        /// <summary>
+        /// Gets the product of the two input values that sum to a value of 2020.
+        /// </summary>
+        public int ProductOf2020Sum { get; private set; }
+
+        /// <summary>
+        /// Gets the product of two values from the specified set of values that
+        /// which when added together equal 2020.
+        /// </summary>
+        /// <param name="values">The values to find the 2020 sum's product from.</param>
+        /// <returns>
+        /// The product of the two values that sum to a value of 2020.
+        /// </returns>
+        public static int Get2020Product(IEnumerable<string> values)
+        {
+            int[] expenses = values
+                .Select((p) => ParseInt32(p))
+                .ToArray();
+
+            const int Limit = 2020;
+
+            for (int i = 0; i < expenses.Length; i++)
+            {
+                int x = expenses[i];
+
+                if (x > Limit)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < expenses.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    int y = expenses[j];
+
+                    if (x + y == Limit)
+                    {
+                        return x * y;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> values = ReadResourceAsLines();
+
+            ProductOf2020Sum = Get2020Product(values);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("The product of the two entries that sum to 2020 is {0}.", ProductOf2020Sum);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day01Tests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day01"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day01Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day01Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day01Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void Y2020_Day01_Get2020Product_Returns_Correct_Value()
+        {
+            string[] values = new[]
+            {
+                "1721",
+                "979",
+                "366",
+                "299",
+                "675",
+                "1456",
+            };
+
+            // Act
+            int actual = Day01.Get2020Product(values);
+
+            // Assert
+            actual.ShouldBe(514579);
+        }
+
+        [Fact]
+        public void Y2020_Day01_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day01>();
+
+            // Assert
+            puzzle.ProductOf2020Sum.ShouldBe(63616);
+        }
+    }
+}
