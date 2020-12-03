ccf01e622dbf651eb8fadd55b10beba9efc0febc
martincostello <martin@martincostello.com>
2020-12-03 10:04:07 +0000

    Solve day 3 part 1
    Solve part 1 of day 3 for 2020.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day03.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/3</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day03 : Puzzle2020
+    {
+        /// <summary>
+        /// Gets the number of trees collided with when traversing the grid.
+        /// </summary>
+        public int TreeCollisions { get; private set; }
+
+        /// <summary>
+        /// Gets the number of trees that would be collided with by
+        /// traversing the specified grid in the specified direction.
+        /// </summary>
+        /// <param name="grid">The grid to traverse.</param>
+        /// <param name="x">The horizontal component of the slope to traverse with.</param>
+        /// <param name="y">The vertical component of the slope to traverse with.</param>
+        /// <returns>
+        /// The number of trees that would be collided with when traversing the grid.
+        /// </returns>
+        public static int GetTreeCollisionCount(IList<string> grid, int x, int y)
+        {
+            int width = grid[0].Length;
+            int height = grid.Count;
+
+            bool[,] space = new bool[height, width];
+
+            for (int i = 0; i < height; i++)
+            {
+                string row = grid[i];
+
+                for (int j = 0; j < width; j++)
+                {
+                    if (row[j] == '#')
+                    {
+                        space[i, j] = true;
+                    }
+                }
+            }
+
+            var deltaX = new Size(x, 0);
+            var deltaY = new Size(0, y);
+            var wrap = new Size(width, 0);
+
+            int collisions = 0;
+            var position = Point.Empty;
+
+            while (position.Y < height)
+            {
+                if (space[position.Y, position.X])
+                {
+                    collisions++;
+                }
+
+                position += deltaX;
+                position += deltaY;
+
+                if (position.X >= width)
+                {
+                    position -= wrap;
+                }
+            }
+
+            return collisions;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> grid = ReadResourceAsLines();
+
+            TreeCollisions = GetTreeCollisionCount(grid, 3, 1);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("{0} trees would be encountered using a right-3/down-1 slope.", TreeCollisions);
+            }
+
+            return 0;
+        }
+    }
+}
