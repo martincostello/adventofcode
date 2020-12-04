d8d58b4b844b532142e96d6d022cfce7dd14cf70
martincostello <martin@martincostello.com>
2020-12-04 13:21:29 +0000

    Solve day 4 part 1
    Solve part 1 of day 4 for 2020.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day04.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/4</c>. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day04 : Puzzle2020
+    {
+        /// <summary>
+        /// The required keys for passports. This field is read-only.
+        /// </summary>
+        private static readonly string[] RequiredKeys = { "byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid" };
+
+        /// <summary>
+        /// Gets the number of valid passports.
+        /// </summary>
+        public int ValidPassports { get; private set; }
+
+        /// <summary>
+        /// Gets the number of valid passports in the specified batch file.
+        /// </summary>
+        /// <param name="batch">The batch file to get the number of valid passports from.</param>
+        /// <returns>
+        /// The number of valid passports specified by <paramref name="batch"/>.
+        /// </returns>
+        public static int GetValidPassportCount(ICollection<string> batch)
+        {
+            var passports = new List<NameValueCollection>();
+            var current = new NameValueCollection();
+
+            foreach (string line in batch)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    passports.Add(current);
+                    current = new NameValueCollection();
+                    continue;
+                }
+
+                string[] split = line.Split(' ');
+
+                foreach (string pair in split)
+                {
+                    string[] attribute = pair.Split(':');
+                    current[attribute[0]] = attribute[1];
+                }
+            }
+
+            if (current.Count > 0)
+            {
+                passports.Add(current);
+            }
+
+            int result = 0;
+
+            foreach (var passport in passports)
+            {
+                bool isValid = passport.AllKeys.Intersect(RequiredKeys).Count() == RequiredKeys.Length;
+
+                if (isValid)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            IList<string> batch = ReadResourceAsLines();
+
+            ValidPassports = GetValidPassportCount(batch);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("There are {0} valid passports.", ValidPassports);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day04Tests.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day04"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day04Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day04Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day04Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void Y2020_Day04_GetValidPassportCount_Returns_Correct_Value()
+        {
+            // Arrange
+            string[] batch = new[]
+            {
+                "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+                "byr:1937 iyr:2017 cid:147 hgt:183cm",
+                string.Empty,
+                "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+                "hcl:#cfa07d byr:1929",
+                string.Empty,
+                "hcl:#ae17e1 iyr:2013",
+                "eyr:2024",
+                "ecl:brn pid:760753108 byr:1931",
+                "hgt:179cm",
+                string.Empty,
+                "hcl:#cfa07d eyr:2025 pid:166559648",
+                "iyr:2011 ecl:brn hgt:59in",
+            };
+
+            // Act
+            int actual = Day04.GetValidPassportCount(batch);
+
+            // Assert
+            actual.ShouldBe(2);
+        }
+
+        [Fact]
+        public void Y2020_Day04_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = SolvePuzzle<Day04>();
+
+            // Assert
+            puzzle.ValidPassports.ShouldBe(226);
+        }
+    }
+}
