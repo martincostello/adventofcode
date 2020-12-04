fac4e76713bce295aa665e334dd8e0b36af3ae8c
martincostello <martin@martincostello.com>
2020-12-04 13:46:25 +0000

    Solve day 4 part 2
    Solve part 2 of day 4 for 2020.

--- src/AdventOfCode/Puzzles/Y2020/Day04.cs
+++ src/AdventOfCode/Puzzles/Y2020/Day04.cs
@@ -3,9 +3,11 @@
 
 namespace MartinCostello.AdventOfCode.Puzzles.Y2020
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     /// <summary>
     /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/4</c>. This class cannot be inherited.
@@ -23,13 +25,18 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
         public int ValidPassports { get; private set; }
 
         /// <summary>
+        /// Gets the number of verified passports.
+        /// </summary>
+        public int VerifiedPassports { get; private set; }
+
+        /// <summary>
         /// Gets the number of valid passports in the specified batch file.
         /// </summary>
         /// <param name="batch">The batch file to get the number of valid passports from.</param>
         /// <returns>
         /// The number of valid passports specified by <paramref name="batch"/>.
         /// </returns>
-        public static int GetValidPassportCount(ICollection<string> batch)
+        public static (int valid, int verified) VerifyPassports(ICollection<string> batch)
         {
             var passports = new List<NameValueCollection>();
             var current = new NameValueCollection();
@@ -57,7 +64,8 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
                 passports.Add(current);
             }
 
-            int result = 0;
+            int valid = 0;
+            int verified = 0;
 
             foreach (var passport in passports)
             {
@@ -65,11 +73,16 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
 
                 if (isValid)
                 {
-                    result++;
+                    valid++;
+
+                    if (IsPasswordVerified(passport))
+                    {
+                        verified++;
+                    }
                 }
             }
 
-            return result;
+            return (valid, verified);
         }
 
         /// <inheritdoc />
@@ -77,14 +90,106 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
         {
             IList<string> batch = ReadResourceAsLines();
 
-            ValidPassports = GetValidPassportCount(batch);
+            (ValidPassports, VerifiedPassports) = VerifyPassports(batch);
 
             if (Verbose)
             {
                 Logger.WriteLine("There are {0} valid passports.", ValidPassports);
+                Logger.WriteLine("There are {0} verified passports.", VerifiedPassports);
             }
 
             return 0;
+        }
+
+        /// <summary>
+        /// Returns whether the specified passport is verified.
+        /// </summary>
+        /// <param name="passport">The passport to verify.</param>
+        /// <returns>
+        /// <see langword="true"/> if the passport is verified; otherwise <see langword="false"/>.
+        /// </returns>
+        private static bool IsPasswordVerified(NameValueCollection passport)
+        {
+            static bool IsValidEyeColor(string? value)
+            {
+                return value switch
+                {
+                    "amb" => true,
+                    "blu" => true,
+                    "brn" => true,
+                    "grn" => true,
+                    "gry" => true,
+                    "hzl" => true,
+                    "oth" => true,
+                    _ => false,
+                };
+            }
+
+            static bool IsValidHairColor(string? value)
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return false;
+                }
+
+                return Regex.IsMatch(value, "#[0-9a-f]{6}", RegexOptions.IgnoreCase);
+            }
+
+            static bool IsValidHeight(string? value)
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return false;
+                }
+
+                if (value.EndsWith("cm", StringComparison.OrdinalIgnoreCase))
+                {
+                    return IsValidNumber(value[..^2], 150, 193);
+                }
+                else if (value.EndsWith("in", StringComparison.OrdinalIgnoreCase))
+                {
+                    return IsValidNumber(value[..^2], 59, 76);
+                }
+
+                return false;
+            }
+
+            static bool IsValidNumber(string? value, int minimum, int maximum)
+            {
+                if (!TryParseInt32(value, out int number))
+                {
+                    return false;
+                }
+
+                return number >= minimum && number <= maximum;
+            }
+
+            static bool IsValidYear(string? value, int minimum, int maximum)
+                => IsValidNumber(value, minimum, maximum) && value!.Length == 4;
+
+            foreach (string? key in passport.AllKeys)
+            {
+                string? value = passport[key];
+
+                bool isValid = key switch
+                {
+                    "byr" => IsValidYear(value, 1920, 2002),
+                    "ecl" => IsValidEyeColor(value),
+                    "eyr" => IsValidYear(value, 2020, 2030),
+                    "hcl" => IsValidHairColor(value),
+                    "hgt" => IsValidHeight(value),
+                    "iyr" => IsValidYear(value, 2010, 2020),
+                    "pid" => TryParseInt32(value, out _) && value.Length == 9,
+                    _ => true,
+                };
+
+                if (!isValid)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2020/Day04Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day04Tests.cs
@@ -22,7 +22,7 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
         }
 
         [Fact]
-        public void Y2020_Day04_GetValidPassportCount_Returns_Correct_Value()
+        public void Y2020_Day04_VerifyPassports_Returns_Correct_Valid_Value()
         {
             // Arrange
             string[] batch = new[]
@@ -43,10 +43,65 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
             };
 
             // Act
-            int actual = Day04.GetValidPassportCount(batch);
+            (int actual, _) = Day04.VerifyPassports(batch);
 
             // Assert
             actual.ShouldBe(2);
+        }
+
+        [Fact]
+        public void Y2020_Day04_VerifyPassports_Returns_Correct_Verified_Value_For_Invalid_Passports()
+        {
+            // Arrange
+            string[] batch = new[]
+            {
+                "eyr:1972 cid:100",
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+                string.Empty,
+                "iyr:2019",
+                "hcl:#602927 eyr:1967 hgt:170cm",
+                "ecl:grn pid:012533040 byr:1946",
+                string.Empty,
+                "hcl:dab227 iyr:2012",
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+                string.Empty,
+                "hgt:59cm ecl:zzz",
+                "eyr:2038 hcl:74454a iyr:2023",
+                "pid:3556412378 byr:2007",
+            };
+
+            // Act
+            (_, int actual) = Day04.VerifyPassports(batch);
+
+            // Assert
+            actual.ShouldBe(0);
+        }
+
+        [Fact]
+        public void Y2020_Day04_VerifyPassports_Returns_Correct_Verified_Value_For_Valid_Passports()
+        {
+            // Arrange
+            string[] batch = new[]
+            {
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+                "hcl:#623a2f",
+                string.Empty,
+                "eyr:2029 ecl:blu cid:129 byr:1989",
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+                string.Empty,
+                "hcl:#888785",
+                "hgt:164cm byr:2001 iyr:2015 cid:88",
+                "pid:545766238 ecl:hzl",
+                "eyr:2022",
+                string.Empty,
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+            };
+
+            // Act
+            (_, int actual) = Day04.VerifyPassports(batch);
+
+            // Assert
+            actual.ShouldBe(4);
         }
 
         [Fact]
@@ -57,6 +112,7 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2020
 
             // Assert
             puzzle.ValidPassports.ShouldBe(226);
+            puzzle.VerifiedPassports.ShouldBe(160);
         }
     }
 }
