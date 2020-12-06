0a0d9c358dbdaf93a8ec1235bf3f77470e508742
martincostello <martin@martincostello.com>
2020-12-06 17:25:43 +0000

    Add integration test for API
    Add an integration test for the API for solving puzzles.

---
+++ tests/AdventOfCode.Tests/Api/ApiTests.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the API.
+    /// </summary>
+    public class ApiTests : IntegrationTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiTests"/> class.
+        /// </summary>
+        /// <param name="fixture">The fixture to use.</param>
+        /// <param name="outputHelper">The test output helper to use.</param>
+        public ApiTests(HttpServerFixture fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData(2020, 05, new string[0], true, new object[] { 878, 504 }, 0)]
+        public async Task Can_Solve_Puzzle(
+            int year,
+            int day,
+            string[] arguments,
+            bool sendResource,
+            object[] expectedSolutions,
+            int expectedVisualizations)
+        {
+            // Arrange
+            using var client = Fixture.CreateClient();
+
+            var parameters = new Dictionary<string, string>()
+            {
+                ["arguments"] = string.Join(Environment.NewLine, arguments),
+            };
+
+            if (sendResource)
+            {
+                parameters["resource"] = GetPuzzleInput(year, day);
+            }
+
+            // Act
+            using var content = new FormUrlEncodedContent((IEnumerable<KeyValuePair<string?, string?>>)parameters);
+            using var response = await client.PostAsync($"/api/puzzles/{year}/{day}/solve", content);
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            response.Content.ShouldNotBeNull();
+            response.Content!.Headers.ContentType.ShouldNotBeNull();
+            response.Content.Headers.ContentType.MediaType.ShouldBe("application/json");
+
+            using var solution = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+
+            solution.RootElement.GetProperty("year").GetInt32().ShouldBe(year);
+            solution.RootElement.GetProperty("day").GetInt32().ShouldBe(day);
+            solution.RootElement.GetProperty("timeToSolve").GetDouble().ShouldBeGreaterThan(0);
+            solution.RootElement.GetProperty("visualizations").GetArrayLength().ShouldBe(expectedVisualizations);
+
+            solution.RootElement.TryGetProperty("solutions", out var solutions).ShouldBeTrue();
+            solutions.GetArrayLength().ShouldBe(expectedSolutions.Length);
+
+            var actualSolutions = solutions.EnumerateArray().ToArray();
+
+            for (int i = 0; i < expectedSolutions.Length; i++)
+            {
+                object? expected = expectedSolutions[i];
+                var actual = actualSolutions[i];
+
+                actual.GetRawText().ShouldBe(expected.ToString());
+            }
+        }
+
+        private static string GetPuzzleInput(int year, int day)
+        {
+            var type = typeof(Puzzle);
+
+            string name = FormattableString.Invariant(
+                $"MartinCostello.{type.Assembly.GetName().Name}.Input.Y{year}.Day{day:00}.input.txt");
+
+            using var stream = type.Assembly.GetManifestResourceStream(name) !;
+            using var reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Api/HttpServerFixture.cs
@@ -0,0 +1,211 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Api
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Sockets;
+    using System.Reflection;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using MartinCostello.Logging.XUnit;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A test fixture representing an HTTP server hosting the application. This class cannot be inherited.
+    /// </summary>
+    public sealed class HttpServerFixture : WebApplicationFactory<Startup>, IAsyncLifetime, ITestOutputHelperAccessor
+    {
+        private IHost? _host;
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpServerFixture"/> class.
+        /// </summary>
+        public HttpServerFixture()
+            : base()
+        {
+            ClientOptions.AllowAutoRedirect = false;
+            ClientOptions.BaseAddress = new Uri("https://localhost");
+        }
+
+        /// <inheritdoc />
+        public ITestOutputHelper? OutputHelper { get; set; }
+
+        /// <summary>
+        /// Gets the server address of the application.
+        /// </summary>
+        public Uri ServerAddress => ClientOptions.BaseAddress;
+
+        /// <inheritdoc />
+        public override IServiceProvider? Services => _host?.Services;
+
+        /// <summary>
+        /// Clears the current <see cref="ITestOutputHelper"/>.
+        /// </summary>
+        public void ClearOutputHelper()
+            => OutputHelper = null;
+
+        /// <summary>
+        /// Sets the <see cref="ITestOutputHelper"/> to use.
+        /// </summary>
+        /// <param name="value">The <see cref="ITestOutputHelper"/> to use.</param>
+        public void SetOutputHelper(ITestOutputHelper value)
+            => OutputHelper = value;
+
+        /// <inheritdoc />
+        async Task IAsyncLifetime.InitializeAsync()
+            => await EnsureHttpServerAsync();
+
+        /// <inheritdoc />
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            if (_host != null)
+            {
+                await _host.StopAsync();
+                _host.Dispose();
+                _host = null;
+            }
+        }
+
+        /// <summary>
+        /// Creates an <see cref="HttpClient"/> to communicate with the application.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="HttpClient"/> that can be to used to make application requests.
+        /// </returns>
+        public HttpClient CreateHttpClient()
+        {
+#pragma warning disable CA2000
+            var handler = new HttpClientHandler()
+#pragma warning restore CA2000
+            {
+                AllowAutoRedirect = ClientOptions.AllowAutoRedirect,
+                MaxAutomaticRedirections = ClientOptions.MaxAutomaticRedirections,
+                UseCookies = ClientOptions.HandleCookies,
+            };
+
+            try
+            {
+                if (ClientOptions.BaseAddress.IsLoopback &&
+                    string.Equals(ClientOptions.BaseAddress.Scheme, "https", StringComparison.OrdinalIgnoreCase))
+                {
+                    handler.ServerCertificateCustomValidationCallback = (request, cert, chain, errors) => true;
+                }
+
+#pragma warning disable CA5400
+                var client = new HttpClient(handler);
+#pragma warning restore CA5400
+
+                ConfigureClient(client);
+
+                client.BaseAddress = ClientOptions.BaseAddress;
+
+                return client;
+            }
+            catch (Exception)
+            {
+                handler.Dispose();
+                throw;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging((loggingBuilder) => loggingBuilder.ClearProviders().AddXUnit(this))
+                   .UseContentRoot(GetApplicationContentRootPath());
+
+            builder.ConfigureKestrel(
+                (p) => p.ConfigureHttpsDefaults(
+                    (r) => r.ServerCertificate = new X509Certificate2("localhost-dev.pfx", "Pa55w0rd!")));
+
+            builder.UseUrls(ServerAddress.ToString());
+        }
+
+        /// <inheritdoc />
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _host?.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        private static Uri FindFreeServerAddress()
+        {
+            int port = GetFreePortNumber();
+
+            return new UriBuilder()
+            {
+                Scheme = "https",
+                Host = "localhost",
+                Port = port,
+            }.Uri;
+        }
+
+        private static int GetFreePortNumber()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+
+            try
+            {
+                return ((IPEndPoint)listener.LocalEndpoint).Port;
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+
+        private async Task EnsureHttpServerAsync()
+        {
+            if (_host == null)
+            {
+                await CreateHttpServer();
+            }
+        }
+
+        private async Task CreateHttpServer()
+        {
+            // Configure the server address for the server to listen on for HTTP requests
+            ClientOptions.BaseAddress = FindFreeServerAddress();
+
+            var builder = CreateHostBuilder().ConfigureWebHost(ConfigureWebHost);
+
+            _host = builder.Build();
+
+            // Force creation of the Kestrel server and start it
+            var hostedService = _host.Services.GetService<IHostedService>();
+            await hostedService!.StartAsync(default);
+        }
+
+        private string GetApplicationContentRootPath()
+        {
+            var attribute = GetTestAssemblies()
+                .SelectMany((p) => p.GetCustomAttributes<WebApplicationFactoryContentRootAttribute>())
+                .Where((p) => string.Equals(p.Key, "AdventOfCode", StringComparison.OrdinalIgnoreCase))
+                .OrderBy((p) => p.Priority)
+                .First();
+
+            return attribute.ContentRootPath;
+        }
+    }
+}
