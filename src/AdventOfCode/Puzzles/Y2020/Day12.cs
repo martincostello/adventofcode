10aad3c12914f336a8832bb73f1dbf90ce3afcab
martincostello <martin@martincostello.com>
2020-12-12 10:10:50 +0000

    Solve day 12 part 1
    Solve part 1 of day 12 for 2020.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day12.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/12</c>. This class cannot be inherited.
+    /// </summary>
+    [Puzzle(2020, 12, RequiresData = true)]
+    public sealed class Day12 : Puzzle
+    {
+        /// <summary>
+        /// Gets the Manhattan distance the ship has travelled.
+        /// </summary>
+        public int ManhattanDistance { get; private set; }
+
+        /// <summary>
+        /// Gets the Manhattan distance travelled by the ship navigating the specified instructions.
+        /// </summary>
+        /// <param name="instructions">The navigation instructions for the ship.</param>
+        /// <returns>
+        /// The Manhattan distance the ship travels by following the instructions.
+        /// </returns>
+        public static int GetDistanceTravelled(IEnumerable<string> instructions)
+        {
+            var vectors = new Dictionary<int, (int x, int y)>()
+            {
+                [0] = (0, 1),
+                [90] = (1, 0),
+                [180] = (0, -1),
+                [270] = (-1, 0),
+            };
+
+            int heading = 90;
+            var position = Point.Empty;
+
+            foreach (string instruction in instructions)
+            {
+                int? moveHeading = null;
+                int units = ParseInt32(instruction[1..]);
+
+                switch (instruction[0..1])
+                {
+                    case "F":
+                        moveHeading = heading;
+                        break;
+
+                    case "L":
+                        heading = Math.Abs((heading - units + 360) % 360);
+                        break;
+
+                    case "R":
+                        heading = Math.Abs((heading + units) % 360);
+                        break;
+
+                    case "N":
+                        moveHeading = 0;
+                        break;
+
+                    case "S":
+                        moveHeading = 180;
+                        break;
+
+                    case "E":
+                        moveHeading = 90;
+                        break;
+
+                    case "W":
+                        moveHeading = 270;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (moveHeading.HasValue)
+                {
+                    (int x, int y) = vectors[moveHeading.Value];
+                    position += new Size(units * x, units * y);
+                }
+            }
+
+            return Math.Abs(position.X) + Math.Abs(position.Y);
+        }
+
+        /// <inheritdoc />
+        protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+        {
+            IList<string> instructions = await ReadResourceAsLinesAsync();
+
+            ManhattanDistance = GetDistanceTravelled(instructions);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("The Manhattan distance travelled by the ship is {0}.", ManhattanDistance);
+            }
+
+            return PuzzleResult.Create(ManhattanDistance);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day12Tests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Threading.Tasks;
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day12"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day12Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day12Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day12Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void Y2020_Day12_GetDistanceTravelled_Returns_Correct_Value()
+        {
+            // Arrange
+            string[] values = { "F10", "N3", "F7", "R90", "F11" };
+
+            // Act
+            int actual = Day12.GetDistanceTravelled(values);
+
+            // Assert
+            actual.ShouldBe(25);
+        }
+
+        [Fact]
+        public async Task Y2020_Day12_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = await SolvePuzzleAsync<Day12>();
+
+            // Assert
+            puzzle.ManhattanDistance.ShouldBe(439);
+        }
+    }
+}
