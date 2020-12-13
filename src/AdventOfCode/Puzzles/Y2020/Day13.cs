278bee4e103749cf1195fc81e693f8cca9a351ce
martincostello <martin@martincostello.com>
2020-12-13 10:11:26 +0000

    Solve day 13 part 1
    Solve part 1 of day 13 for 2020.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day13.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/13</c>. This class cannot be inherited.
+    /// </summary>
+    [Puzzle(2020, 13, RequiresData = true)]
+    public sealed class Day13 : Puzzle
+    {
+        /// <summary>
+        /// Gets the product of the ID of the earliest bus and the number of minutes to wait for it.
+        /// </summary>
+        public int BusWaitProduct { get; private set; }
+
+        /// <summary>
+        /// Gets the product of the earliest busand the number of minutes to wait for it.
+        /// </summary>
+        /// <param name="notes">The notes for the bus schedules.</param>
+        /// <returns>
+        /// The product of the ID of the earliest bus and the number of minutes to wait for it.
+        /// </returns>
+        public static int GetEarliestBusWaitProduct(IList<string> notes)
+        {
+            int timestamp = ParseInt32(notes[0]);
+
+            int[] buses = notes[1]
+                .Split(',')
+                .Where((p) => !string.Equals(p, "x", StringComparison.Ordinal))
+                .Select((p) => ParseInt32(p))
+                .ToArray();
+
+            var nextBusesInMinutes = new Dictionary<int, int>(buses.Length);
+
+            foreach (int id in buses)
+            {
+                int busesSoFar = timestamp / id;
+                int lastBusAt = busesSoFar * id;
+                int nextBusAt = lastBusAt + id;
+
+                nextBusesInMinutes[id] = nextBusAt - timestamp;
+            }
+
+            var nextBus = nextBusesInMinutes
+                .OrderBy((p) => p.Value)
+                .Select((p) => new { Id = p.Key, WaitInMinutes = p.Value })
+                .First();
+
+            return nextBus.Id * nextBus.WaitInMinutes;
+        }
+
+        /// <inheritdoc />
+        protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+        {
+            IList<string> notes = await ReadResourceAsLinesAsync();
+
+            BusWaitProduct = GetEarliestBusWaitProduct(notes);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("The product of the ID of the earliest and the number of minutes to wait is {0}.", BusWaitProduct);
+            }
+
+            return PuzzleResult.Create(BusWaitProduct);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day13Tests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Threading.Tasks;
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day13"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day13Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day13Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day13Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Fact]
+        public void Y2020_Day13_GetEarliestBusWaitProduct_Returns_Correct_Value()
+        {
+            // Arrange
+            string[] notes = { "939", "7,13,x,x,59,x,31,19" };
+
+            // Act
+            int actual = Day13.GetEarliestBusWaitProduct(notes);
+
+            // Assert
+            actual.ShouldBe(295);
+        }
+
+        [Fact]
+        public async Task Y2020_Day13_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = await SolvePuzzleAsync<Day13>();
+
+            // Assert
+            puzzle.BusWaitProduct.ShouldBe(2935);
+        }
+    }
+}
