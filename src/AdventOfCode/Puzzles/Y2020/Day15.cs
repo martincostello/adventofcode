95fddce0f9059d576c1b2b9b5f6256bc25287f0a
martincostello <martin@martincostello.com>
2020-12-16 17:03:38 +0000

    Solve day 15 part 1
    Solve part 1 of the puzzle for day 15.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day15.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/15</c>. This class cannot be inherited.
+    /// </summary>
+    [Puzzle(2020, 15, MinimumArguments = 1)]
+    public sealed class Day15 : Puzzle
+    {
+        /// <summary>
+        /// Gets the 2020th number spoken.
+        /// </summary>
+        public int Number2020 { get; private set; }
+
+        /// <summary>
+        /// Gets the value of the specified number to be spoken.
+        /// </summary>
+        /// <param name="startingNumbers">The starting numbers for the game.</param>
+        /// <param name="number">The number to get the value for.</param>
+        /// <returns>
+        /// The number spoken on the turn specified by <paramref name="number"/>.
+        /// </returns>
+        public static int GetSpokenNumber(IList<int> startingNumbers, int number)
+        {
+            var numbers = new Dictionary<int, IList<int>>();
+
+            int lastSpoken = 0;
+
+            for (int i = 0; i < number && i < startingNumbers.Count; i++)
+            {
+                lastSpoken = startingNumbers[i];
+                UpdateNumber(lastSpoken, i);
+            }
+
+            for (int i = startingNumbers.Count; i < number; i++)
+            {
+                if (!numbers.TryGetValue(lastSpoken, out IList<int>? times))
+                {
+                    lastSpoken = 0;
+                }
+                else if (times.Count == 1)
+                {
+                    lastSpoken = 0;
+                }
+                else
+                {
+                    lastSpoken = times[^1] - times[^2];
+                }
+
+                UpdateNumber(lastSpoken, i);
+            }
+
+            return lastSpoken;
+
+            void UpdateNumber(int value, int time)
+            {
+                if (!numbers!.TryGetValue(value, out IList<int>? occasions))
+                {
+                    occasions = numbers[value] = new List<int>();
+                }
+
+                occasions.Add(time);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+        {
+            IList<int> startingNumbers = args[0]
+                .Split(',')
+                .Select((p) => ParseInt32(p))
+                .ToList();
+
+            Number2020 = GetSpokenNumber(startingNumbers, 2020);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("The 2020th number spoken is {0}.", Number2020);
+            }
+
+            return PuzzleResult.Create(Number2020);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day15Tests.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Threading.Tasks;
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day15"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day15Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day15Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day15Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 3, 6 }, 1, 0)]
+        [InlineData(new[] { 0, 3, 6 }, 2, 3)]
+        [InlineData(new[] { 0, 3, 6 }, 3, 6)]
+        [InlineData(new[] { 0, 3, 6 }, 4, 0)]
+        [InlineData(new[] { 0, 3, 6 }, 5, 3)]
+        [InlineData(new[] { 0, 3, 6 }, 6, 3)]
+        [InlineData(new[] { 0, 3, 6 }, 7, 1)]
+        [InlineData(new[] { 0, 3, 6 }, 8, 0)]
+        [InlineData(new[] { 0, 3, 6 }, 9, 4)]
+        [InlineData(new[] { 0, 3, 6 }, 10, 0)]
+        [InlineData(new[] { 0, 3, 6 }, 2020, 436)]
+        [InlineData(new[] { 1, 3, 2 }, 2020, 1)]
+        [InlineData(new[] { 2, 1, 3 }, 2020, 10)]
+        [InlineData(new[] { 1, 2, 3 }, 2020, 27)]
+        [InlineData(new[] { 2, 3, 1 }, 2020, 78)]
+        [InlineData(new[] { 3, 2, 1 }, 2020, 438)]
+        [InlineData(new[] { 3, 1, 2 }, 2020, 1836)]
+        public void Y2020_Day15_GetSpokenNumber_Returns_Correct_Value(
+            int[] startingNumbers,
+            int number,
+            int expected)
+        {
+            // Act
+            int actual = Day15.GetSpokenNumber(startingNumbers, number);
+
+            // Assert
+            actual.ShouldBe(expected);
+        }
+
+        [Fact]
+        public async Task Y2020_Day15_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = await SolvePuzzleAsync<Day15>("0,5,4,1,10,14,7");
+
+            // Assert
+            puzzle.Number2020.ShouldBe(203);
+        }
+    }
+}
