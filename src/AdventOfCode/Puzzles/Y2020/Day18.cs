dafc3411cf3fc3e8caa72fe25ea14c3e3607ced8
martincostello <martin@martincostello.com>
2020-12-18 16:35:21 +0000

    Solve day 18 part 1
    Solve part 1 of the puzzle for day 18.

---
+++ src/AdventOfCode/Puzzles/Y2020/Day18.cs
@@ -0,0 +1,136 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class representing the puzzle for <c>https://adventofcode.com/2020/day/18</c>. This class cannot be inherited.
+    /// </summary>
+    [Puzzle(2020, 18, RequiresData = true)]
+    public sealed class Day18 : Puzzle
+    {
+        /// <summary>
+        /// Gets the sum of evaluating the expression(s).
+        /// </summary>
+        public long Sum { get; private set; }
+
+        /// <summary>
+        /// Evaluates the result of the specified expression(s) and returns the sum.
+        /// </summary>
+        /// <param name="expressions">The expression(s) to evaluate.</param>
+        /// <returns>
+        /// The sum of from evaluating the expression(s).
+        /// </returns>
+        public static long Evaluate(IEnumerable<string> expressions)
+        {
+            return expressions.Sum(Evaluate);
+
+            static long Evaluate(string expression)
+            {
+                var tokens = new List<string>();
+                var current = new StringBuilder();
+
+                for (int i = 0; i < expression.Length; i++)
+                {
+                    char ch = expression[i];
+
+                    if (char.IsDigit(ch))
+                    {
+                        current.Append(ch);
+                    }
+                    else if (ch == ' ')
+                    {
+                        if (current.Length > 0)
+                        {
+                            tokens.Add(current.ToString());
+                            current.Clear();
+                        }
+                    }
+                    else if (ch == '(')
+                    {
+                        int openCount = 0;
+                        int closedCount = 0;
+
+                        int end = -1;
+
+                        for (int j = i; j < expression.Length; j++)
+                        {
+                            char thisChar = expression[j];
+
+                            if (thisChar == '(')
+                            {
+                                openCount++;
+                            }
+                            else if (thisChar == ')')
+                            {
+                                closedCount++;
+                            }
+
+                            if (openCount == closedCount)
+                            {
+                                end = j;
+                                break;
+                            }
+                        }
+
+                        string subexpression = expression.Substring(i + 1, end - i - 1);
+
+                        tokens.Add(Evaluate(subexpression).ToString(CultureInfo.InvariantCulture));
+                        i += end - i;
+                    }
+                    else
+                    {
+                        tokens.Add(ch.ToString());
+                    }
+                }
+
+                if (current.Length > 0)
+                {
+                    tokens.Add(current.ToString());
+                    current.Clear();
+                }
+
+                long result = ParseInt64(tokens[0]);
+
+                for (int i = 1; i < tokens.Count; i += 2)
+                {
+                    string token = tokens[i];
+
+                    if (string.Equals("+", token, StringComparison.Ordinal))
+                    {
+                        result += ParseInt64(tokens[i + 1]);
+                    }
+                    else if (string.Equals("*", token, StringComparison.Ordinal))
+                    {
+                        result *= ParseInt64(tokens[i + 1]);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+        {
+            IList<string> expressions = await ReadResourceAsLinesAsync();
+
+            Sum = Evaluate(expressions);
+
+            if (Verbose)
+            {
+                Logger.WriteLine("The sum of the evaluated expressions is {0}.", Sum);
+            }
+
+            return PuzzleResult.Create(Sum);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2020/Day18Tests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2020
+{
+    using System.Threading.Tasks;
+    using Shouldly;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Day18"/> class. This class cannot be inherited.
+    /// </summary>
+    public sealed class Day18Tests : PuzzleTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Day18Tests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+        public Day18Tests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("11", 11)]
+        [InlineData("5 * 3", 15)]
+        [InlineData("(5 * 3)", 15)]
+        [InlineData("((5 * 3))", 15)]
+        [InlineData("11 + 3", 14)]
+        [InlineData("(11 + 3)", 14)]
+        [InlineData("((11 + 3))", 14)]
+        [InlineData("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        [InlineData("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [InlineData("2 * 3 + (4 * 5)", 26)]
+        [InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+        [InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        [InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void Y2020_Day18_Evaluate_Returns_Correct_Value(string expression, long expected)
+        {
+            // Act
+            long actual = Day18.Evaluate(new[] { expression });
+
+            // Assert
+            actual.ShouldBe(expected);
+        }
+
+        [Fact]
+        public async Task Y2020_Day18_Solve_Returns_Correct_Solution()
+        {
+            // Act
+            var puzzle = await SolvePuzzleAsync<Day18>();
+
+            // Assert
+            puzzle.Sum.ShouldBe(2743012121210L);
+        }
+    }
+}
