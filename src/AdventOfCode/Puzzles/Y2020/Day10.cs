f6bd9929e74610693ce7dae00afd9273c381bed1
martincostello <martin@martincostello.com>
2021-10-30 10:19:27 +0100

    Use List<T>.Sort()
    Use lists and their in-built sorting, rather than LINQ's OrderBy() and ToList().
    Take the last element on a sorted list, rather than using Max().

--- src/AdventOfCode/Puzzles/Y2020/Day10.cs
+++ src/AdventOfCode/Puzzles/Y2020/Day10.cs
@@ -30,11 +30,10 @@ public sealed class Day10 : Puzzle
     public static int GetJoltageProduct(IEnumerable<int> joltageRatings)
     {
         // Sort the ratings so the search space is efficient
-        IList<int> sorted = joltageRatings
-            .OrderBy((p) => p)
-            .ToList();
+        var sorted = joltageRatings.ToList();
+        sorted.Sort();
 
-        int maxRating = sorted.Max();
+        int maxRating = sorted[^1];
 
         // Start with the charging outlet with a joltage of 0
         var chain = new Stack<int>();
@@ -115,9 +114,8 @@ public sealed class Day10 : Puzzle
     /// </returns>
     public static long GetValidArrangements(IEnumerable<int> joltageRatings)
     {
-        IList<int> sorted = joltageRatings
-            .OrderBy((p) => p)
-            .ToList();
+        var sorted = joltageRatings.ToList();
+        sorted.Sort();
 
         // Adapted from https://github.com/thatsumoguy/Advent-of-Code-2020.
         // My original solution was brute force and worked out the valid
