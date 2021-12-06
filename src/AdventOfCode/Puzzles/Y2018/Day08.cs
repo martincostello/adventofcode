7f6b010e1daf9d5c1b66e6f13c17eeb5f96b4a1d
martincostello <martin@martincostello.com>
2021-12-06 09:14:31 +0000

    Solve day 8 part 1
    Solve part 1 of the puzzle for day 8.

---
+++ src/AdventOfCode/Puzzles/Y2018/Day08.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018;
+
+/// <summary>
+/// A class representing the puzzle for <c>https://adventofcode.com/2018/day/8</c>. This class cannot be inherited.
+/// </summary>
+[Puzzle(2018, 08, RequiresData = true)]
+public sealed class Day08 : Puzzle
+{
+    /// <summary>
+    /// Gets the sum of the tree's metadata entries.
+    /// </summary>
+    public long SumOfMetadata { get; private set; }
+
+    /// <summary>
+    /// Parses the specified tree of nodes.
+    /// </summary>
+    /// <param name="data">The raw node data for the tree.</param>
+    /// <returns>
+    /// The sum of the tree's metadata entries.
+    /// </returns>
+    public static long ParseTree(IEnumerable<int> data)
+    {
+        int[] span = data.ToArray();
+
+        Parse(span, out _, out long metadataSum);
+
+        return metadataSum;
+
+        static Node Parse(ReadOnlySpan<int> data, out int valuesRead, out long metadataSum)
+        {
+            int index = 0;
+            long sum = 0;
+
+            var node = new Node()
+            {
+                ChildCount = data[index++],
+                MetadataCount = data[index++],
+            };
+
+            if (node.ChildCount > 0)
+            {
+                for (int i = 0; i < node.ChildCount; i++)
+                {
+                    var child = Parse(data[index..], out int childValuesRead, out long childMetadataSum);
+
+                    index += childValuesRead;
+                    sum += childMetadataSum;
+
+                    node.Children.Add(child);
+                }
+            }
+
+            if (node.MetadataCount > 0)
+            {
+                for (int i = 0; i < node.MetadataCount; i++)
+                {
+                    node.Metadata.Add(data[index++]);
+                }
+            }
+
+            sum += node.Metadata.Sum();
+
+            valuesRead = index;
+            metadataSum = sum;
+
+            return node;
+        }
+    }
+
+    /// <inheritdoc />
+    protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+    {
+        var data = (await ReadResourceAsStringAsync()).AsNumbers<int>(' ');
+
+        SumOfMetadata = ParseTree(data);
+
+        if (Verbose)
+        {
+            Logger.WriteLine("The sum of the tree's metadata entries is {0}.", SumOfMetadata);
+        }
+
+        return PuzzleResult.Create(SumOfMetadata);
+    }
+
+    private sealed class Node
+    {
+        public int ChildCount { get; init; }
+
+        public int MetadataCount { get; init; }
+
+        public IList<Node> Children { get; } = new List<Node>();
+
+        public IList<int> Metadata { get; } = new List<int>();
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2018/Day08Tests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2018;
+
+public sealed class Day08Tests : PuzzleTest
+{
+    public Day08Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public static void Y2018_Day08_ParseTree_Returns_Correct_Solution()
+    {
+        // Arrange
+        int[] tree = { 2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2 };
+
+        // Act
+        long actual = Day08.ParseTree(tree);
+
+        // Assert
+        actual.ShouldBe(138);
+    }
+
+    [Fact]
+    public async Task Y2018_Day07_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day08>();
+
+        // Assert
+        puzzle.SumOfMetadata.ShouldBe(45210);
+    }
+}
