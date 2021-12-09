6dab8686c17b2fe005af16effc78f94591c62d9a
martincostello <martin@martincostello.com>
2021-12-09 18:15:07 +0000

    Add AddOrIncrement() extension
    Add an AddOrIncrement() extension method to simplify some dictionary code.

--- src/AdventOfCode/Puzzles/Y2018/Day04.cs
+++ src/AdventOfCode/Puzzles/Y2018/Day04.cs
@@ -118,8 +118,7 @@ public sealed class Day04 : Puzzle
 
                 if (isSleepiest)
                 {
-                    minutesAsleepForSleepiestGuard.TryAdd(i, 0);
-                    minutesAsleepForSleepiestGuard[i]++;
+                    minutesAsleepForSleepiestGuard.AddOrIncrement(i, 0);
                 }
 
                 if (!sleepiestGuardsByMinute.TryGetValue(i, out var guardsAsleepInMinute))
@@ -127,8 +126,7 @@ public sealed class Day04 : Puzzle
                     guardsAsleepInMinute = sleepiestGuardsByMinute[i] = new Dictionary<int, int>();
                 }
 
-                guardsAsleepInMinute.TryAdd(guard, 0);
-                guardsAsleepInMinute[guard]++;
+                guardsAsleepInMinute.AddOrIncrement(guard, 0);
             }
         }
 
