1f2407c3bae8cd32c823623eb6bb9df6a39c2c55
martincostello <martin@martincostello.com>
2021-12-09 15:01:17 +0000

    Add System.Drawing global using
    Add System.Drawing to the global usings to make Point easily available.

--- src/AdventOfCode/Puzzles/Y2016/Day17.cs
+++ src/AdventOfCode/Puzzles/Y2016/Day17.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Drawing;
 using System.Security.Cryptography;
 
 namespace MartinCostello.AdventOfCode.Puzzles.Y2016;
