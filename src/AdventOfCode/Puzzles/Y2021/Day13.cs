2bb0f1e9146ab61fbf2bfee81225b4ee5dbafc4e
martincostello <martin@martincostello.com>
2021-12-13 10:23:36 +0000

    Solve day 13 part 1
    Solve part 1 of the puzzle for day 13.

---
+++ src/AdventOfCode/Puzzles/Y2021/Day13.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2021;
+
+/// <summary>
+/// A class representing the puzzle for <c>https://adventofcode.com/2021/day/13</c>. This class cannot be inherited.
+/// </summary>
+[Puzzle(2021, 13, RequiresData = true)]
+public sealed class Day13 : Puzzle
+{
+    /// <summary>
+    /// Gets the number of dots that are visible after completing the first fold.
+    /// </summary>
+    public int DotCountAfterFold1 { get; private set; }
+
+    /// <summary>
+    /// Folds the transparent paper the specified number of times.
+    /// </summary>
+    /// <param name="instructions">The instructions to follow.</param>
+    /// <param name="folds">The number of times to fold the paper.</param>
+    /// <returns>
+    /// The number of dots that are visible after completing the specified number of folds.
+    /// </returns>
+    public static int Fold(IList<string> instructions, int folds)
+    {
+        var paper = new Dictionary<Point, int>(instructions.Count - 1);
+        var foldLines = new List<Point>();
+
+        foreach (string instruction in instructions)
+        {
+            if (string.IsNullOrEmpty(instruction))
+            {
+                continue;
+            }
+
+            if (instruction[0] == 'f')
+            {
+                string fold = instruction.Split(' ')[2];
+                string[] parts = fold.Split('=');
+                int size = Parse<int>(parts[1]);
+
+                if (parts[0][0] == 'x')
+                {
+                    foldLines.Add(new(size, 0));
+                }
+                else
+                {
+                    foldLines.Add(new(0, size));
+                }
+
+                continue;
+            }
+
+            int[] points = instruction.AsNumbers<int>().ToArray();
+
+            paper[new(points[0], points[1])] = 1;
+        }
+
+        for (int i = 0; i < folds && i < foldLines.Count; i++)
+        {
+            var fold = foldLines[i];
+
+            foreach (var point in paper.Keys.ToArray())
+            {
+                if (paper.GetValueOrDefault(point) == 0)
+                {
+                    continue;
+                }
+
+                Point? transform = null;
+
+                if (fold.X > 0 && point.X > fold.X)
+                {
+                    var delta = new Size(Math.Abs(point.X - fold.X) * 2, 0);
+                    transform = point - delta;
+                }
+                else if (fold.Y > 0 && point.Y > fold.Y)
+                {
+                    var delta = new Size(0, Math.Abs(point.Y - fold.Y) * 2);
+                    transform = point - delta;
+                }
+
+                if (transform.HasValue)
+                {
+                    paper.AddOrIncrement(transform.Value, 1);
+                    paper[point]--;
+                }
+            }
+        }
+
+        return paper.Values.Count((p) => p != 0);
+    }
+
+    /// <inheritdoc />
+    protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+    {
+        IList<string> instructions = await ReadResourceAsLinesAsync();
+
+        DotCountAfterFold1 = Fold(instructions, folds: 1);
+
+        if (Verbose)
+        {
+            Logger.WriteLine(
+                "{0:N0} dots are visible after completing the first fold instruction.",
+                DotCountAfterFold1);
+        }
+
+        return PuzzleResult.Create(DotCountAfterFold1);
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2021/Day13Tests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2021;
+
+public sealed class Day13Tests : PuzzleTest
+{
+    public Day13Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Theory]
+    [InlineData(0, 18)]
+    [InlineData(1, 17)]
+    public void Y2021_Day13_Navigate_Returns_Correct_Value(int folds, int expected)
+    {
+        // Arrange
+        string[] instructions =
+        {
+            "6,10",
+            "0,14",
+            "9,10",
+            "0,3",
+            "10,4",
+            "4,11",
+            "6,0",
+            "6,12",
+            "4,1",
+            "0,13",
+            "10,12",
+            "3,4",
+            "3,0",
+            "8,4",
+            "1,10",
+            "2,14",
+            "8,10",
+            "9,0",
+            string.Empty,
+            "fold along y=7",
+            "fold along x=5",
+        };
+
+        // Act
+        int actual = Day13.Fold(instructions, folds);
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task Y2021_Day13_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day13>();
+
+        // Assert
+        puzzle.DotCountAfterFold1.ShouldBe(647);
+    }
+}
