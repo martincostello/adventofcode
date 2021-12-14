f66a9d322c4a4b10e617ae16e3bc65c3a155cc4c
martincostello <martin@martincostello.com>
2021-12-14 08:34:40 +0000

    Solve day 14 part 1
    Solve part 1 of the puzzle for day 14.

---
+++ src/AdventOfCode/Puzzles/Y2021/Day14.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2021;
+
+/// <summary>
+/// A class representing the puzzle for <c>https://adventofcode.com/2021/day/14</c>. This class cannot be inherited.
+/// </summary>
+[Puzzle(2021, 14, RequiresData = true)]
+public sealed class Day14 : Puzzle
+{
+    /// <summary>
+    /// Gets the "score" of the polymer after being expanded 10 times,
+    /// which is the quantity of the most common element subtracted by
+    /// the quantity of the least common element.
+    /// </summary>
+    public int Score { get; private set; }
+
+    /// <summary>
+    /// Expands the polymer template as specified by the instructions.
+    /// </summary>
+    /// <param name="instructions">The instructions to expand a polymer from.</param>
+    /// <param name="steps">The number of steps to expand the polymer by.</param>
+    /// <returns>
+    /// The polymer's score after expansion.
+    /// </returns>
+    public static int Expand(IList<string> instructions, int steps)
+    {
+        string template = instructions[0];
+
+        var pairs = new Dictionary<string, string>(instructions.Count - 2);
+
+        foreach (string instruction in instructions.Skip(2))
+        {
+            string[] split = instruction.Split(" -> ");
+            pairs[split[0]] = split[1];
+        }
+
+        var current = new StringBuilder(template);
+        StringBuilder next;
+
+        for (int i = 0; i < steps; i++)
+        {
+            string previous = current.ToString();
+            next = new StringBuilder(previous);
+
+            for (int j = 0, k = 0; j < previous.Length - 1; j++)
+            {
+                string pair = new(new[] { previous[j], previous[j + 1] });
+
+                if (pairs.TryGetValue(pair, out string? element))
+                {
+                    next.Insert(j + k++ + 1, element);
+                }
+            }
+
+            current = next;
+        }
+
+        string final = current.ToString();
+
+        var frequencies = final
+            .GroupBy((p) => p)
+            .Select((p) => new
+            {
+                Letter = p.Key,
+                Count = p.Count(),
+            });
+
+        int max = frequencies.MaxBy((p) => p.Count)!.Count;
+        int min = frequencies.MinBy((p) => p.Count)!.Count;
+
+        return max - min;
+    }
+
+    /// <inheritdoc />
+    protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
+    {
+        IList<string> instructions = await ReadResourceAsLinesAsync();
+
+        Score = Expand(instructions, steps: 10);
+
+        if (Verbose)
+        {
+            Logger.WriteLine(
+                "The \"score\" of the polymer after 10 steps of expansion is {0:N0}.",
+                Score);
+        }
+
+        return PuzzleResult.Create(Score);
+    }
+}
---
+++ tests/AdventOfCode.Tests/Puzzles/Y2021/Day14Tests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2021;
+
+public sealed class Day14Tests : PuzzleTest
+{
+    public Day14Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public void Y2021_Day14_Expand_Returns_Correct_Value()
+    {
+        // Arrange
+        string[] instructions =
+        {
+            "NNCB",
+            string.Empty,
+            "CH -> B",
+            "HH -> N",
+            "CB -> H",
+            "NH -> C",
+            "HB -> C",
+            "HC -> B",
+            "HN -> C",
+            "NN -> C",
+            "BH -> H",
+            "NC -> B",
+            "NB -> B",
+            "BN -> B",
+            "BB -> N",
+            "BC -> B",
+            "CC -> N",
+            "CN -> C",
+        };
+
+        // Act
+        int actual = Day14.Expand(instructions, steps: 10);
+
+        // Assert
+        actual.ShouldBe(1588);
+    }
+
+    [Fact]
+    public async Task Y2021_Day14_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day14>();
+
+        // Assert
+        puzzle.Score.ShouldBe(3587);
+    }
+}
