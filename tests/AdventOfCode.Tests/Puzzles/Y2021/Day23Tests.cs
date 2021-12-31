15e47d1f5c8c1b07a194d0751425a9f883e119f1
martincostello <martin@martincostello.com>
2021-12-31 10:25:18 +0000

    Refactor cost tests
    Refactor the tests for costs to be data-driven.

--- tests/AdventOfCode.Tests/Puzzles/Y2021/Day23Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2021/Day23Tests.cs
@@ -10,6 +10,34 @@ public sealed class Day23Tests : PuzzleTest
     {
     }
 
+    public static IEnumerable<object[]> Costs()
+    {
+        yield return new object[] { ("           ", "BA", "CD", "BC", "DA"), ("   B       ", "BA", "CD", " C", "DA"), 40 };
+        yield return new object[] { ("   B       ", "BA", "CD", " C", "DA"), ("   B       ", "BA", " D", "CC", "DA"), 400 };
+        yield return new object[] { ("   B       ", "BA", " D", "CC", "DA"), ("   B D     ", "BA", "  ", "CC", "DA"), 3000 };
+        yield return new object[] { ("   B D     ", "BA", "  ", "CC", "DA"), ("     D     ", "BA", " B", "CC", "DA"), 30 };
+        yield return new object[] { ("     D     ", "BA", " B", "CC", "DA"), ("     D     ", " A", "BB", "CC", "DA"), 40 };
+        yield return new object[] { ("     D     ", " A", "BB", "CC", "DA"), ("     D D   ", " A", "BB", "CC", " A"), 2000 };
+        yield return new object[] { ("     D D   ", " A", "BB", "CC", " A"), ("     D D A ", " A", "BB", "CC", "  "), 3 };
+        yield return new object[] { ("     D D A ", " A", "BB", "CC", "  "), ("     D   A ", " A", "BB", "CC", " D"), 3000 };
+        yield return new object[] { ("     D   A ", " A", "BB", "CC", " D"), ("         A ", " A", "BB", "CC", "DD"), 4000 };
+        yield return new object[] { ("         A ", " A", "BB", "CC", "DD"), ("           ", "AA", "BB", "CC", "DD"), 8 };
+    }
+
+    [Theory]
+    [MemberData(nameof(Costs))]
+    public void Y2021_Day23_Cost_Returns_Correct_Value(
+    (string Hallway, string Amber, string Bronze, string Copper, string Desert) x,
+    (string Hallway, string Amber, string Bronze, string Copper, string Desert) y,
+    int expected)
+    {
+        // Act
+        int actual = Day23.Cost(x, y);
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
     [Fact]
     public void Y2021_Day23_Organize_Returns_Correct_Value()
     {
@@ -28,146 +56,6 @@ public sealed class Day23Tests : PuzzleTest
 
         // Assert
         actual.ShouldBe(12521);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_1()
-    {
-        // Arrange
-        var x = ("           ", "BA", "CD", "BC", "DA");
-        var y = ("   B       ", "BA", "CD", " C", "DA");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(40);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_2()
-    {
-        // Arrange
-        var x = ("   B       ", "BA", "CD", " C", "DA");
-        var y = ("   B       ", "BA", " D", "CC", "DA");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(400);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_3()
-    {
-        // Arrange
-        var x = ("   B       ", "BA", " D", "CC", "DA");
-        var y = ("   B D     ", "BA", "  ", "CC", "DA");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(3000);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_4()
-    {
-        // Arrange
-        var x = ("   B D     ", "BA", "  ", "CC", "DA");
-        var y = ("     D     ", "BA", " B", "CC", "DA");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(30);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_5()
-    {
-        // Arrange
-        var x = ("     D     ", "BA", " B", "CC", "DA");
-        var y = ("     D     ", " A", "BB", "CC", "DA");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(40);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_6()
-    {
-        // Arrange
-        var x = ("     D    ", " A", "BB", "CC", "DA");
-        var y = ("     D D  ", " A", "BB", "CC", " A");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(2000);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_7()
-    {
-        // Arrange
-        var x = ("     D D   ", " A", "BB", "CC", " A");
-        var y = ("     D D A ", " A", "BB", "CC", "  ");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(3);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_8()
-    {
-        // Arrange
-        var x = ("     D D A ", " A", "BB", "CC", "  ");
-        var y = ("     D   A ", " A", "BB", "CC", " D");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(3000);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_9()
-    {
-        // Arrange
-        var x = ("     D   A ", " A", "BB", "CC", " D");
-        var y = ("         A ", " A", "BB", "CC", "DD");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(4000);
-    }
-
-    [Fact]
-    public void Y2021_Day23_Cost_Returns_Correct_Value_10()
-    {
-        // Arrange
-        var x = ("         A ", " A", "BB", "CC", "DD");
-        var y = ("           ", "AA", "BB", "CC", "DD");
-
-        // Act
-        int actual = Day23.Cost(x, y);
-
-        // Assert
-        actual.ShouldBe(8);
     }
 
     [Fact]
