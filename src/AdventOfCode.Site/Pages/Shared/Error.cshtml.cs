4cf9402285adc9fe82ca37366c2a1372f08875fb
martincostello <martin@martincostello.com>
2022-02-11 09:27:29 +0000

    Use crank from NuGet
    Use crank from NuGet instead of local packages again.
    Update StyleCop.Analyzers to its latest version.

--- src/AdventOfCode.Site/Pages/Shared/Error.cshtml.cs
+++ src/AdventOfCode.Site/Pages/Shared/Error.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+#pragma warning disable SA1649
+
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
