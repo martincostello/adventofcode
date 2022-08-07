53bf553e8c372de7f12295e42572d6fdb5829a49
martincostello <martin@martincostello.com>
2022-08-07 08:56:56 +0100

    Remove Feature-Policy
    Remove `Feature-Policy` as it creates Chrome warnings when used with `Permissions-Policy`.

--- src/AdventOfCode.Site/CustomHttpHeadersMiddleware.cs
+++ src/AdventOfCode.Site/CustomHttpHeadersMiddleware.cs
@@ -74,7 +74,6 @@ public sealed class CustomHttpHeadersMiddleware
                 context.Response.Headers.Add("Expect-CT", "max-age=1800");
             }
 
-            context.Response.Headers.Add("Feature-Policy", "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'");
             context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
             context.Response.Headers.Add("Referrer-Policy", "no-referrer-when-downgrade");
             context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
