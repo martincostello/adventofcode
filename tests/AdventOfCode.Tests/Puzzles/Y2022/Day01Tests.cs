ab733a5187642422748e39a0f929c63f910086bb
martincostello <martin@martincostello.com>
2022-11-19 09:24:25 +0000

    Prepare for 2022
    Add the infrastructure ready for 2022's puzzles.

---
+++ tests/AdventOfCode.Tests/Puzzles/Y2022/Day01Tests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2022;
+
+/// <summary>
+/// A class containing tests for the <see cref="Day01"/> class. This class cannot be inherited.
+/// </summary>
+public sealed class Day01Tests : PuzzleTest
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Day01Tests"/> class.
+    /// </summary>
+    /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+    public Day01Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact(Skip = "Not implemented.")]
+    public async Task Y2022_Day01_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day01>();
+
+        // Assert
+        puzzle.ShouldNotBeNull();
+    }
+}
