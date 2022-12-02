cde26f4756b2bf1974897f3ef0737b1dbf7ceaf3
martincostello <martin@martincostello.com>
2022-12-02 15:40:48 +0000

    Add test helper
    Remove duplicated code and add class to solve the puzzles.

--- src/AdventOfCode.Site/scripts/ts/puzzles/2022/Day02.test.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2022/Day02.test.ts
@@ -2,41 +2,8 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 import { describe, expect, test } from '@jest/globals';
-import { readFileSync } from 'fs';
-import { join, resolve } from 'path';
-import { Puzzle } from '../Puzzle';
+import { PuzzleSolver } from '../PuzzleSolver';
 import { Day02 } from './Day02';
-
-async function solvePuzzle<T extends Puzzle>(type: { new (): T }, inputs: string[] = []): Promise<T> {
-    // Arrange
-    const puzzle = new type();
-
-    const locale = 'en-US';
-    const year = puzzle.year.toLocaleString(locale, {
-        minimumIntegerDigits: 4,
-        useGrouping: false,
-    });
-
-    const day = puzzle.day.toLocaleString(locale, {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-    });
-
-    let input = join(__dirname, `../../../../../AdventOfCode/Input/Y${year}/Day${day}/input.txt`);
-    input = resolve(input);
-
-    puzzle.resource = readFileSync(input, 'utf-8');
-
-    // Act
-    const result = await puzzle.solve(inputs);
-
-    // Assert
-    expect(result).not.toBeNull();
-    expect(result.solutions).not.toBeNull();
-    expect(result.solutions.length).toBeGreaterThan(0);
-
-    return puzzle;
-}
 
 describe('2022', () => {
     describe('Day 02', () => {
@@ -55,7 +22,7 @@ describe('2022', () => {
         });
         test('returns the correct solution', async () => {
             // Act
-            const puzzle = await solvePuzzle(Day02);
+            const puzzle = await PuzzleSolver.solve(Day02);
 
             // Assert
             expect(puzzle.totalScoreForMoves).toBe(13675);
