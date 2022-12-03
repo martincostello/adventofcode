99f86042f449875f6bb77b4bc7bde53a8f5c0953
martincostello <martin@martincostello.com>
2022-12-03 12:22:33 +0000

    Make LINQ-ier
    Add a custom `selectMany()` implementation that matches the LINQ version that has a collection selector and a result selector.

--- src/AdventOfCode.Site/scripts/ts/puzzles/Maths.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/Maths.ts
@@ -9,23 +9,46 @@ export class Maths {
     }
 
     static getPermutationsWithCount<T>(collection: T[], count: number): IEnumerable<IEnumerable<T>> {
-        let enumerable = from(collection);
+        const enumerable = from(collection);
         if (count === 1) {
             return enumerable.select((p: T) => from([p]));
         }
 
         const permutations = Maths.getPermutationsWithCount(collection, count - 1);
+        return Maths.selectMany(
+            permutations,
+            (p) => enumerable.except(p),
+            (set, value) => Maths.append(set, value)
+        );
+    }
 
-        const result: IEnumerable<T>[] = [];
+    private static append<T>(source: IEnumerable<T>, value: T): IEnumerable<T> {
+        return source.concatenate(from([value]));
+    }
 
-        for (const permutation of permutations) {
-            for (const item of collection) {
-                if (!permutation.contains(item)) {
-                    result.push(permutation.concatenate(from([item])));
+    // Adapted from https://github.com/arogozine/LinqToTypeScript/blob/9f6fc3bac2f48f746257f9a41a4192c02b3aa733/src/sync/_private/selectMany.ts
+
+    private static selectMany<TSource, TCollection, TResult>(
+        source: IEnumerable<TSource>,
+        collectionSelector: (x: TSource) => IEnumerable<TCollection>,
+        resultSelector: (x: TSource, collection: TCollection) => IEnumerable<TResult>
+    ) {
+        function* iterator() {
+            for (const element of source) {
+                for (const subElement of collectionSelector(element)) {
+                    yield resultSelector(element, subElement);
                 }
             }
         }
 
-        return from(result);
+        return from(new BasicEnumerable(iterator));
+    }
+}
+
+class BasicEnumerable<TSource> implements Iterable<TSource> {
+    public constructor(private readonly iterator: () => IterableIterator<TSource>) {}
+
+    public [Symbol.iterator](): IterableIterator<TSource> {
+        return this.iterator();
     }
 }
