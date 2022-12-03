a369fd9310dc134edab17f1a0c46666f4dda8bb2
martincostello <martin@martincostello.com>
2022-12-03 08:53:34 +0000

    Solve day 3 part 1
    Solve part 1 of the puzzle for day 3.

---
+++ tests/AdventOfCode.Tests/Puzzles/Y2022/Day03Tests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2022;
+
+/// <summary>
+/// A class containing tests for the <see cref="Day03"/> class. This class cannot be inherited.
+/// </summary>
+public sealed class Day03Tests : PuzzleTest
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Day03Tests"/> class.
+    /// </summary>
+    /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+    public Day03Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public void Y2022_Day03_GetSumOfDuplicateItemPriorities_Returns_Correct_Values()
+    {
+        // Arrange
+        string[] inventories = new[]
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw",
+        };
+
+        // Act
+        int actual = Day03.GetSumOfDuplicateItemPriorities(inventories);
+
+        // Assert
+        actual.ShouldBe(157);
+    }
+
+    [Fact]
+    public async Task Y2022_Day03_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day03>();
+
+        // Assert
+        puzzle.ShouldNotBeNull();
+        puzzle.SumOfPriorities.ShouldBe(7568);
+    }
+}
