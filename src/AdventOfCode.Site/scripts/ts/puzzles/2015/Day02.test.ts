b36c9baadcb9730608d0e257f3ca468a5926dd17
martincostello <martin@martincostello.com>
2022-12-03 14:58:10 +0000

    Implement 2015 day 2 in TypeScript
    Implement the puzzle for day 2 of 2015 in TypeScript.

---
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2015/Day02.test.ts
@@ -0,0 +1,21 @@
+// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import { describe, expect, test } from '@jest/globals';
+import { Day02 } from './Day02';
+
+describe('2015', () => {
+    describe('Day 02', () => {
+        test.each([
+            ['2x3x4', 58, 34],
+            ['1x1x10', 43, 14],
+        ])('returns correct length and area for "%s"', (dimensions: string, expectedArea: number, expectedLength: number) => {
+            // Act
+            const [actualArea, actualLength] = Day02.getTotalWrappingPaperAreaAndRibbonLength([dimensions]);
+
+            // Assert
+            expect(actualArea).toEqual(expectedArea);
+            expect(actualLength).toEqual(expectedLength);
+        });
+    });
+});
