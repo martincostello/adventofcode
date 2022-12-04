b8a91fdc232a25eec6f6804584a3252b85dad2a9
martincostello <martin@martincostello.com>
2022-12-04 09:45:14 +0000

    Solve day 4 part 2
    Solve part 2 of the puzzle for day 4.

--- src/AdventOfCode/Puzzles/Y2022/Day04.cs
+++ src/AdventOfCode/Puzzles/Y2022/Day04.cs
@@ -15,13 +15,19 @@ public sealed class Day04 : Puzzle
     public int FullyOverlappingAssignments { get; private set; }
 
     /// <summary>
-    /// Gets the number of assignment pairs where one range fully contains the other.
+    /// Gets the number of assignment pairs where one range partially contains the other.
+    /// </summary>
+    public int PartiallyOverlappingAssignments { get; private set; }
+
+    /// <summary>
+    /// Gets the number of assignment pairs where one range fully or partially contains the other.
     /// </summary>
     /// <param name="assignments">The section assignments.</param>
+    /// <param name="partial">Whether to test that the pair only partially overlaps.</param>
     /// <returns>
-    /// The number of assignment pairs where one range fully contains the other.
+    /// The number of assignment pairs where one range fully or partially contains the other.
     /// </returns>
-    public static int GetOverlappingAssignments(IList<string> assignments)
+    public static int GetOverlappingAssignments(IList<string> assignments, bool partial)
     {
         int count = 0;
 
@@ -31,20 +37,36 @@ public sealed class Day04 : Puzzle
             Range first = AsRange(split[0].AsNumberPair<int>('-'));
             Range second = AsRange(split[1].AsNumberPair<int>('-'));
 
-            if (IsWithinRange(first, second) ||
-                IsWithinRange(second, first))
+            if (partial)
             {
-                count++;
+                if (Overlaps(first, second) || Overlaps(second, first))
+                {
+                    count++;
+                }
+            }
+            else
+            {
+                if (IsWithinRange(first, second) || IsWithinRange(second, first))
+                {
+                    count++;
+                }
             }
         }
 
         return count;
 
         static Range AsRange((int Start, int End) pair)
-            => new Range(pair.Start, pair.End);
+            => new(pair.Start, pair.End);
 
         static bool IsWithinRange(Range range, Range other)
-            => range.Start.Value >= other.Start.Value && range.End.Value <= other.End.Value;
+            => range.Start.Value >= other.Start.Value &&
+               range.End.Value <= other.End.Value;
+
+        static bool Overlaps(Range range, Range other)
+        {
+            return (range.Start.Value <= other.Start.Value && range.End.Value >= other.Start.Value) ||
+                   (range.Start.Value <= other.End.Value && range.End.Value >= other.End.Value);
+        }
     }
 
     /// <inheritdoc />
@@ -54,15 +76,20 @@ public sealed class Day04 : Puzzle
 
         var assignments = await ReadResourceAsLinesAsync();
 
-        FullyOverlappingAssignments = GetOverlappingAssignments(assignments);
+        FullyOverlappingAssignments = GetOverlappingAssignments(assignments, false);
+        PartiallyOverlappingAssignments = GetOverlappingAssignments(assignments, true);
 
         if (Verbose)
         {
             Logger.WriteLine(
                 "There are {0:N0} assignment pairs where one range is entirely contained within the other.",
                 FullyOverlappingAssignments);
+
+            Logger.WriteLine(
+                "There are {0:N0} assignment pairs where one range overlaps with the other.",
+                PartiallyOverlappingAssignments);
         }
 
-        return PuzzleResult.Create(FullyOverlappingAssignments);
+        return PuzzleResult.Create(FullyOverlappingAssignments, PartiallyOverlappingAssignments);
     }
 }
