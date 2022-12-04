7a43e83e03cf9d6a3383fec34db1bae140b43b37
martincostello <martin@martincostello.com>
2022-12-04 12:54:00 +0000

    Simplify puzzles
    Refactor 2 old puzzles to be less verbose.

--- src/AdventOfCode/Puzzles/Y2015/Day02.cs
+++ src/AdventOfCode/Puzzles/Y2015/Day02.cs
@@ -36,8 +36,8 @@ public sealed class Day02 : Puzzle
             .ToList();
 
         // Determine the total area of wrapping paper required and the amount of ribbon
-        int totalArea = presents.Sum(GetWrappingPaperArea);
-        int length = presents.Sum(GetRibbonLength);
+        int totalArea = presents.Sum((p) => p.GetWrappingPaperArea());
+        int length = presents.Sum((p) => p.GetRibbonLength());
 
         return (totalArea, length);
     }
@@ -62,42 +62,9 @@ public sealed class Day02 : Puzzle
     }
 
     /// <summary>
-    /// Returns the length of ribbon required to wrap the specified present.
-    /// </summary>
-    /// <param name="present">The present to calculate the required length of ribbon for.</param>
-    /// <returns>The length of ribbon, in feet, required to wrap the present.</returns>
-    private static int GetRibbonLength(Present present)
-    {
-        int smallestPerimeter = Math.Min((present.Length + present.Width) * 2, (present.Width + present.Height) * 2);
-        smallestPerimeter = Math.Min(smallestPerimeter, (present.Height + present.Length) * 2);
-
-        int lengthForBow = present.Height * present.Length * present.Width;
-
-        return smallestPerimeter + lengthForBow;
-    }
-
-    /// <summary>
-    /// Returns the area of wrapping paper required to wrap the specified present.
-    /// </summary>
-    /// <param name="present">The present to calculate the required area of wrapping paper for.</param>
-    /// <returns>The area of wrapping paper, in square feet, required to wrap the present.</returns>
-    private static int GetWrappingPaperArea(Present present)
-    {
-        int surfaceArea =
-            (2 * present.Length * present.Width) +
-            (2 * present.Width * present.Height) +
-            (2 * present.Height * present.Length);
-
-        int extra = Math.Min(present.Length * present.Width, present.Width * present.Height);
-        extra = Math.Min(extra, present.Height * present.Length);
-
-        return surfaceArea + extra;
-    }
-
-    /// <summary>
     /// Represents a Christmas present.
     /// </summary>
-    internal readonly struct Present
+    private readonly struct Present
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Present"/> struct.
@@ -105,7 +72,7 @@ public sealed class Day02 : Puzzle
         /// <param name="length">The length of the present.</param>
         /// <param name="width">The width of the present.</param>
         /// <param name="height">The height of the present.</param>
-        internal Present(int length, int width, int height)
+        private Present(int length, int width, int height)
         {
             Length = length;
             Width = width;
@@ -115,27 +82,61 @@ public sealed class Day02 : Puzzle
         /// <summary>
         /// Gets the length of the present.
         /// </summary>
-        internal readonly int Length { get; }
+        public readonly int Length { get; }
 
         /// <summary>
         /// Gets the width of the present.
         /// </summary>
-        internal readonly int Width { get; }
+        public readonly int Width { get; }
 
         /// <summary>
         /// Gets the height of the present.
         /// </summary>
-        internal readonly int Height { get; }
+        public readonly int Height { get; }
+
+        /// <summary>
+        /// Gets the volume of the present.
+        /// </summary>
+        public int Volume => Height * Length * Width;
 
         /// <summary>
         /// Parses the specified <see cref="string"/> to an instance of <see cref="Present"/>.
         /// </summary>
         /// <param name="value">The present to parse.</param>
         /// <returns>The result of parsing <paramref name="value"/>.</returns>
-        internal static Present Parse(string value)
+        public static Present Parse(string value)
         {
             (int length, int width, int height) = value.AsNumberTriple<int>('x');
             return new Present(length, width, height);
+        }
+
+        /// <summary>
+        /// Returns the length of ribbon required to wrap this present.
+        /// </summary>
+        /// <returns>The length of ribbon, in feet, required to wrap this present.</returns>
+        public int GetRibbonLength()
+        {
+            int smallestPerimeter = Math.Min((Length + Width) * 2, (Width + Height) * 2);
+            smallestPerimeter = Math.Min(smallestPerimeter, (Height + Length) * 2);
+
+            return smallestPerimeter + Volume;
+        }
+
+        /// <summary>
+        /// Returns the area of wrapping paper required to wrap this present.
+        /// </summary>
+        /// <returns>The area of wrapping paper, in square feet, required to wrap this present.</returns>
+        public int GetWrappingPaperArea()
+        {
+            int surfaceArea =
+                (2 * Length * Width) +
+                (2 * Width * Height) +
+                (2 * Height * Length);
+
+            int extra = Math.Min(Length * Width, Width * Height);
+            extra = Math.Min(extra, Height * Length);
+
+            return surfaceArea + extra;
         }
     }
 }
