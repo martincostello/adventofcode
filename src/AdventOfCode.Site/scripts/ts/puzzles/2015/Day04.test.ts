5e3e0052381a9a49f51e3900f9edf27479ad0010
martincostello <martin@martincostello.com>
2022-12-04 13:44:35 +0000

    Implement 2015 day 4 in TypeScript
    Implement the puzzle for day 4 of 2015 in TypeScript.
    Tests are skipped as the performance is too slow.

---
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2015/Day04.test.ts
@@ -0,0 +1,20 @@
+// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import { describe, expect, xtest } from '@jest/globals';
+import { Day04 } from './Day04';
+
+describe('2015', () => {
+    describe('Day 04', () => {
+        xtest.each([
+            ['abcdef', 609043],
+            ['pqrstuv', 1048970],
+        ])('returns correct value for key %s', (secretKey: string, expected: number) => {
+            // Act
+            const actual = Day04.getLowestPositiveNumberWithStartingZeroesAsync(secretKey, 5);
+
+            // Assert
+            expect(actual).toEqual(expected);
+        });
+    });
+});
