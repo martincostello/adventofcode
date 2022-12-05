d225cd5c1a330c311b10ecdbe4b60be2106a5fc0
martincostello <martin@martincostello.com>
2022-12-05 12:13:33 +0000

    Import all types
    Import all types from the index to simplify consumption.

--- src/AdventOfCode.Site/scripts/ts/puzzles/2015/PuzzleFactory2015.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2015/PuzzleFactory2015.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 import { Puzzle, PuzzleFactory } from '../index';
-import { Day01, Day02, Day03, Day04, Day05, Day06 } from './index';
+import * as Y2015 from './index';
 
 export class PuzzleFactory2015 implements PuzzleFactory {
     create(year: number, day: number): Puzzle | null {
@@ -12,17 +12,17 @@ export class PuzzleFactory2015 implements PuzzleFactory {
 
         switch (day) {
             case 1:
-                return new Day01();
+                return new Y2015.Day01();
             case 2:
-                return new Day02();
+                return new Y2015.Day02();
             case 3:
-                return new Day03();
+                return new Y2015.Day03();
             case 4:
-                return new Day04();
+                return new Y2015.Day04();
             case 5:
-                return new Day05();
+                return new Y2015.Day05();
             case 6:
-                return new Day06();
+                return new Y2015.Day06();
             default:
                 return null;
         }
