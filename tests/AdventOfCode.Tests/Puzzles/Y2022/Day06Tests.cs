7b661841c5f1cbc4da54fdd482c3e4181c4745ee
martincostello <martin@martincostello.com>
2022-12-06 09:26:02 +0000

    Solve day 6 part 1
    Solve part 1 of the puzzle for day 6.

---
+++ tests/AdventOfCode.Tests/Puzzles/Y2022/Day06Tests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.AdventOfCode.Puzzles.Y2022;
+
+/// <summary>
+/// A class containing tests for the <see cref="Day06"/> class. This class cannot be inherited.
+/// </summary>
+public sealed class Day06Tests : PuzzleTest
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Day06Tests"/> class.
+    /// </summary>
+    /// <param name="outputHelper">The <see cref="ITestOutputHelper"/> to use.</param>
+    public Day06Tests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+    }
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void Y2022_Day06_FindFirstPacket_Returns_Correct_Value(string datastream, int expected)
+    {
+        // Act
+        int actual = Day06.FindFirstPacket(datastream);
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public async Task Y2022_Day06_Solve_Returns_Correct_Solution()
+    {
+        // Act
+        var puzzle = await SolvePuzzleAsync<Day06>();
+
+        // Assert
+        puzzle.ShouldNotBeNull();
+        puzzle.IndexOfFirstStartOfPacketMarker.ShouldBe(1850);
+    }
+}
