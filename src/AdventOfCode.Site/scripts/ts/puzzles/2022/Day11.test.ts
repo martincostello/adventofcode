b1599c0506bddc1bdc2ac2a5b3d4f868d51e04cb
martincostello <martin@martincostello.com>
2022-12-11 11:42:58 +0000

    Solve day 11 for 2022
    - Solve the puzzle for day 11 of 2022 in TypeScript.
    - More trim more efficient.
    - Remove unused variable.

--- src/AdventOfCode.Site/scripts/ts/puzzles/2022/Day11.test.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2022/Day11.test.ts
@@ -6,9 +6,43 @@ import { Day11 } from './index';
 
 describe('2022', () => {
     describe.skip('Day 11', () => {
-        test.each([[[], -1]])('returns correct value for %s', (inputs: string[], expected: number) => {
+        test.each([
+            [20, false, 10605],
+            [10000, true, 2713310158],
+        ])('returns correct value for %s and %s', (rounds: number, highAnxiety: boolean, expected: number) => {
+            // Arrange
+            const observations = [
+                'Monkey 0:',
+                '  Starting items: 79, 98',
+                '  Operation: new = old * 19',
+                '  Test: divisible by 23',
+                '    If true: throw to monkey 2',
+                '    If false: throw to monkey 3',
+                '',
+                'Monkey 1:',
+                '  Starting items: 54, 65, 75, 74',
+                '  Operation: new = old + 6',
+                '  Test: divisible by 19',
+                '    If true: throw to monkey 2',
+                '    If false: throw to monkey 0',
+                '',
+                'Monkey 2:',
+                '  Starting items: 79, 60, 97',
+                '  Operation: new = old * old',
+                '  Test: divisible by 13',
+                '    If true: throw to monkey 1',
+                '    If false: throw to monkey 3',
+                '',
+                'Monkey 3:',
+                '  Starting items: 74',
+                '  Operation: new = old + 3',
+                '  Test: divisible by 17',
+                '    If true: throw to monkey 0',
+                '    If false: throw to monkey 1',
+            ];
+
             // Act
-            const actual = Day11.solve(inputs);
+            const actual = Day11.getMonkeyBusiness(observations, rounds, highAnxiety);
 
             // Assert
             expect(actual).toBe(expected);
