c77d206d59228bffdd2422b7cd5dcdb4b0edf2cc
martincostello <martin@martincostello.com>
2023-10-24 11:41:37 +0100

    Fix ESLint warnings
    Rename imports to fix new ESLint warnings.

--- src/AdventOfCode.Site/scripts/ts/puzzles/2022/PuzzleFactory2022.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2022/PuzzleFactory2022.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 import { Puzzle, PuzzleFactory } from '../index';
-import * as Y2022 from './index';
+import * as thisYear from './index';
 
 export class PuzzleFactory2022 implements PuzzleFactory {
     create(year: number, day: number): Puzzle | null {
@@ -12,55 +12,55 @@ export class PuzzleFactory2022 implements PuzzleFactory {
 
         switch (day) {
             case 1:
-                return new Y2022.Day01();
+                return new thisYear.Day01();
             case 2:
-                return new Y2022.Day02();
+                return new thisYear.Day02();
             case 3:
-                return new Y2022.Day03();
+                return new thisYear.Day03();
             case 4:
-                return new Y2022.Day04();
+                return new thisYear.Day04();
             case 5:
-                return new Y2022.Day05();
+                return new thisYear.Day05();
             case 6:
-                return new Y2022.Day06();
+                return new thisYear.Day06();
             case 7:
-                return new Y2022.Day07();
+                return new thisYear.Day07();
             case 8:
-                return new Y2022.Day08();
+                return new thisYear.Day08();
             case 9:
-                return new Y2022.Day09();
+                return new thisYear.Day09();
             case 10:
-                return new Y2022.Day10();
+                return new thisYear.Day10();
             case 11:
-                return new Y2022.Day11();
+                return new thisYear.Day11();
             case 12:
-                return new Y2022.Day12();
+                return new thisYear.Day12();
             case 13:
-                return new Y2022.Day13();
+                return new thisYear.Day13();
             case 14:
-                return new Y2022.Day14();
+                return new thisYear.Day14();
             case 15:
-                return new Y2022.Day15();
+                return new thisYear.Day15();
             case 16:
-                return new Y2022.Day16();
+                return new thisYear.Day16();
             case 17:
-                return new Y2022.Day17();
+                return new thisYear.Day17();
             case 18:
-                return new Y2022.Day18();
+                return new thisYear.Day18();
             case 19:
-                return new Y2022.Day19();
+                return new thisYear.Day19();
             case 20:
-                return new Y2022.Day20();
+                return new thisYear.Day20();
             case 21:
-                return new Y2022.Day21();
+                return new thisYear.Day21();
             case 22:
-                return new Y2022.Day22();
+                return new thisYear.Day22();
             case 23:
-                return new Y2022.Day23();
+                return new thisYear.Day23();
             case 24:
-                return new Y2022.Day24();
+                return new thisYear.Day24();
             case 25:
-                return new Y2022.Day25();
+                return new thisYear.Day25();
             default:
                 return null;
         }
