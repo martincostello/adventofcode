24adca2386dd63265f38f7f139c8ae8a22a83b05
martincostello <martin@martincostello.com>
2023-12-02 08:54:34 +0000

    Solve day 2 part 1
    Solve part 1 of the puzzle for day 2.

--- src/AdventOfCode/Puzzles/Y2023/Day02.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day02.cs
@@ -6,18 +6,71 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/02</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 02, "", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 02, "Cube Conundrum", RequiresData = true)]
 public sealed class Day02 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    /// <summary>
+    /// Gets the sum of the IDs of the games that are possible.
+    /// </summary>
+    public int SumOfPossibleSolutions { get; private set; }
 
-    public int Solution { get; private set; }
-
+    /// <summary>
+    /// Determines the sum of the IDs of the specified games that are possible.
+    /// </summary>
+    /// <param name="values">The games of Cube Conundrum to play.</param>
+    /// <returns>
+    /// The sum of the IDs of the games that are possible.
+    /// </returns>
     public static int Solve(IList<string> values)
     {
-        return -1;
+        var possible = new List<int>();
+
+        foreach (string game in values)
+        {
+            string[] split = game.Split(':');
+            string[] rounds = split[1].Split(';');
+
+            int blue = 0;
+            int green = 0;
+            int red = 0;
+
+            foreach (string round in rounds)
+            {
+                string[] cubes = round.Split(',');
+
+                foreach (string cube in cubes)
+                {
+                    (string count, string color) = cube.TrimStart().Bifurcate(' ');
+                    int number = Parse<int>(count);
+
+                    switch (color)
+                    {
+                        case "blue":
+                            blue = Math.Max(number, blue);
+                            break;
+
+                        case "green":
+                            green = Math.Max(number, green);
+                            break;
+
+                        case "red":
+                            red = Math.Max(number, red);
+                            break;
+
+                        default:
+                            throw new PuzzleException($"Unknown color '{color}'.");
+                    }
+                }
+            }
+
+            if (blue <= 14 && green <= 13 && red <= 12)
+            {
+                int id = Parse<int>(split[0]["Game ".Length..]);
+                possible.Add(id);
+            }
+        }
+
+        return possible.Sum();
     }
 
     /// <inheritdoc />
@@ -27,13 +80,13 @@ public sealed class Day02 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        SumOfPossibleSolutions = Solve(values);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The sum of the IDs of the possible games is {0}.", SumOfPossibleSolutions);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(SumOfPossibleSolutions);
     }
 }
