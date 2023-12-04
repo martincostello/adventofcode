b938988941dfbbc4cfa05b8f462fd5df52a1c66a
martincostello <martin@martincostello.com>
2023-12-04 09:06:10 +0000

    Solve day 4 part 1
    Solve part 1 of the puzzle for day 4.

--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day04Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day04Tests.cs
@@ -5,23 +5,28 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day04Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2023_Day04_Solve_Returns_Correct_Value()
+    [Fact]
+    public void Y2023_Day04_Score_Returns_Correct_Value()
     {
         // Arrange
         string[] values =
         [
-            "_",
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
         ];
 
         // Act
-        int actual = Day04.Solve(values);
+        int actual = Day04.Score(values);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(13);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day04_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +34,6 @@ public sealed class Day04Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.TotalPoints.ShouldBe(21138);
     }
 }
