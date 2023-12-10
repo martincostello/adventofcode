f28c4994373efd856327a427c8c0c06e1c4b0117
martincostello <martin@martincostello.com>
2023-12-10 09:30:19 +0000

    Solve day 9 part 2
    Solve part 2 of the puzzle for day 9.

--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day09Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day09Tests.cs
@@ -5,11 +5,13 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day09Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact]
-    public void Y2023_Day09_Analyze_Returns_Correct_Value()
+    [Theory]
+    [InlineData(false, 114)]
+    [InlineData(true, 2)]
+    public void Y2023_Day09_Analyze_Returns_Correct_Value(bool reverse, int expected)
     {
         // Arrange
-        string[] values =
+        string[] histories =
         [
             "0 3 6 9 12 15",
             "1 3 6 10 15 21",
@@ -17,10 +19,10 @@ public sealed class Day09Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
         ];
 
         // Act
-        int actual = Day09.Analyze(values);
+        int actual = Day09.Analyze(histories, reverse);
 
         // Assert
-        actual.ShouldBe(114);
+        actual.ShouldBe(expected);
     }
 
     [Fact]
@@ -31,6 +33,7 @@ public sealed class Day09Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Sum.ShouldBe(1882395907);
+        puzzle.SumNext.ShouldBe(1882395907);
+        puzzle.SumPrevious.ShouldBe(1005);
     }
 }
