183b89715a8a4ed7e97b83b5178553aebcfbf26a
martincostello <martin@martincostello.com>
2023-12-10 14:52:52 +0000

    Skeleton for part 2
    Add skeleton of a solution for part 2.
    Finds completely enclosed spaces, but does not account for ways to squeeze through the pipes or pipes inside pipes.

--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day10Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day10Tests.cs
@@ -19,10 +19,11 @@ public sealed class Day10Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
         ];
 
         // Act
-        int actual = Day10.Walk(sketch, CancellationToken.None);
+        (int actualSteps, int actualTiles) = Day10.Walk(sketch, CancellationToken.None);
 
         // Assert
-        actual.ShouldBe(4);
+        actualSteps.ShouldBe(4);
+        actualTiles.ShouldBe(1);
     }
 
     [Fact]
@@ -39,10 +40,109 @@ public sealed class Day10Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
         ];
 
         // Act
-        int actual = Day10.Walk(sketch, CancellationToken.None);
+        (int actualSteps, int actualTiles) = Day10.Walk(sketch, CancellationToken.None);
+
+        // Assert
+        actualSteps.ShouldBe(8);
+        actualTiles.ShouldBe(0);
+    }
+
+    [Fact]
+    public void Y2023_Day10_Walk_Returns_Correct_Value_For_Tiles_1()
+    {
+        // Arrange
+        string[] sketch =
+        [
+            "...........",
+            ".S-------7.",
+            ".|F-----7|.",
+            ".||.....||.",
+            ".||.....||.",
+            ".|L-7.F-J|.",
+            ".|..|.|..|.",
+            ".L--J.L--J.",
+            "...........",
+        ];
+
+        // Act
+        (_, int actual) = Day10.Walk(sketch, CancellationToken.None);
+
+        // Assert
+        actual.ShouldBe(4);
+    }
+
+    [Fact]
+    public void Y2023_Day10_Walk_Returns_Correct_Value_For_Tiles_2()
+    {
+        // Arrange
+        string[] sketch =
+        [
+            "..........",
+            ".S------7.",
+            ".|F----7|.",
+            ".||....||.",
+            ".||....||.",
+            ".|L-7F-J|.",
+            ".|..||..|.",
+            ".L--JL--J.",
+            "..........",
+        ];
+
+        // Act
+        (_, int actual) = Day10.Walk(sketch, CancellationToken.None);
+
+        // Assert
+        actual.ShouldBe(4);
+    }
+
+    [Fact]
+    public void Y2023_Day10_Walk_Returns_Correct_Value_For_Tiles_3()
+    {
+        // Arrange
+        string[] sketch =
+        [
+            ".F----7F7F7F7F-7....",
+            ".|F--7||||||||FJ....",
+            ".||.FJ||||||||L7....",
+            "FJL7L7LJLJ||LJ.L-7..",
+            "L--J.L7...LJS7F-7L7.",
+            "....F-J..F7FJ|L7L7L7",
+            "....L7.F7||L7|.L7L7|",
+            ".....|FJLJ|FJ|F7|.LJ",
+            "....FJL-7.||.||||...",
+            "....L---J.LJ.LJLJ...",
+        ];
+
+        // Act
+        (_, int actual) = Day10.Walk(sketch, CancellationToken.None);
 
         // Assert
         actual.ShouldBe(8);
+    }
+
+    [Fact]
+    public void Y2023_Day10_Walk_Returns_Correct_Value_For_Tiles_4()
+    {
+        // Arrange
+        string[] sketch =
+        [
+            "FF7FSF7F7F7F7F7F---7",
+            "L|LJ||||||||||||F--J",
+            "FL-7LJLJ||||||LJL-77",
+            "F--JF--7||LJLJ7F7FJ-",
+            "L---JF-JLJ.||-FJLJJ7",
+            "|F|F-JF---7F7-L7L|7|",
+            "|FFJF7L7F-JF7|JL---7",
+            "7-L-JL7||F7|L7F-7F7|",
+            "L.L7LFJ|||||FJL7||LJ",
+            "L7JLJL-JLJLJL--JLJ.L",
+        ];
+
+        // Act
+        (_, int actual) = Day10.Walk(sketch, CancellationToken.None);
+
+        // Assert
+        actual.ShouldBe(10);
     }
 
     [Fact]
@@ -54,5 +154,7 @@ public sealed class Day10Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
         // Assert
         puzzle.ShouldNotBeNull();
         puzzle.Steps.ShouldBe(6870);
+        puzzle.Tiles.ShouldBeLessThan(586);
+        puzzle.Tiles.ShouldBe(-1);
     }
 }
