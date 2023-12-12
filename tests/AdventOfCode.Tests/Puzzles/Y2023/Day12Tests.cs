52668749d220c444826fd9a7276189c46a91504a
martincostello <martin@martincostello.com>
2023-12-12 14:25:17 +0000

    Partially solve day 12 part 1
    Partially solve part 1 of the puzzle for day 12.

--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day12Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day12Tests.cs
@@ -5,23 +5,44 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day12Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2023_Day12_Solve_Returns_Correct_Value()
+    [Theory]
+    [InlineData("???.### 1,1,3", 1)]
+    [InlineData(".??..??...?##. 1,1,3", 4)]
+    [InlineData("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    [InlineData("????.#...#... 4,1,1", 1)]
+    [InlineData("????.######..#####. 1,6,5", 4)]
+    [InlineData("?###???????? 3,2,1", 10)]
+    public void Y2023_Day12_Analyze_Returns_Correct_Value(string record, int expected)
+    {
+        // Act
+        int actual = Day12.Analyze(record, CancellationToken.None);
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Y2023_Day12_Analyze_Returns_Correct_Value_For_Records()
     {
         // Arrange
-        string[] values =
+        string[] records =
         [
-            "_",
+            "???.### 1,1,3",
+            ".??..??...?##. 1,1,3",
+            "?#?#?#?#?#?#?#? 1,3,1,6",
+            "????.#...#... 4,1,1",
+            "????.######..#####. 1,6,5",
+            "?###???????? 3,2,1",
         ];
 
         // Act
-        int actual = Day12.Solve(values);
+        int actual = Day12.Analyze(records, CancellationToken.None);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(21);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day12_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +50,6 @@ public sealed class Day12Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.SumOfCounts.ShouldBe(-1);
     }
 }
