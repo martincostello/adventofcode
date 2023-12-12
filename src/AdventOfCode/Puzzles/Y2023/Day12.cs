1e2202eab70bad2b2384cb40a1d6eae9e383183e
martincostello <martin@martincostello.com>
2023-12-12 14:25:17 +0000

    Partially solve day 12 part 1
    Partially solve part 1 of the puzzle for day 12.

--- src/AdventOfCode/Puzzles/Y2023/Day12.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day12.cs
@@ -6,19 +6,49 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/12</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 12, "Hot Spring", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 12, "Hot Springs", RequiresData = true)]
 public sealed class Day12 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    /// <summary>
+    /// Gets the sum of the counts of the possible spring arrangements.
+    /// </summary>
+    public int SumOfCounts { get; private set; }
 
-    public int Solution { get; private set; }
-
-    public static int Solve(IList<string> values)
+    /// <summary>
+    /// Analyzes the specified spring arrangement.
+    /// </summary>
+    /// <param name="record">The spring arrangement to analyze.</param>
+    /// <param name="cancellationToken">The cancellation token to use.</param>
+    /// <returns>
+    /// The counts of possible spring arrangements.
+    /// </returns>
+    public static int Analyze(string record, CancellationToken cancellationToken)
     {
-        return -1;
+        (string values, string countList) = record.Bifurcate(' ');
+
+        int[] counts = countList.Split(',').Select(Parse<int>).ToArray();
+        int actual = values.Count('#');
+        int desired = counts.Sum();
+
+        var spring = new PartialSpringRecord();
+        var steps = PathFinding.BreadthFirst(spring, new State(values, counts, actual, desired), cancellationToken);
+
+        return steps
+            .Where((p) => p.IsValid())
+            .DistinctBy((p) => p.Values)
+            .Count();
     }
+
+    /// <summary>
+    /// Analyzes the specified spring arrangements.
+    /// </summary>
+    /// <param name="records">The spring arrangements to analyze.</param>
+    /// <param name="cancellationToken">The cancellation token to use.</param>
+    /// <returns>
+    /// The sum of the counts of the possible spring arrangements.
+    /// </returns>
+    public static int Analyze(IList<string> records, CancellationToken cancellationToken)
+        => records.Select((p) => Analyze(p, cancellationToken)).Sum();
 
     /// <inheritdoc />
     protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
@@ -27,13 +57,110 @@ public sealed class Day12 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        SumOfCounts = Analyze(values, cancellationToken);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The sum of the counts of spring arrangements is {0}.", SumOfCounts);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(SumOfCounts);
+    }
+
+    private record struct State(string Values, int[] Counts, int Actual, int Desired)
+    {
+        public readonly bool IsValid()
+        {
+            if (Actual != Desired)
+            {
+                return false;
+            }
+
+            var window = Values.AsSpan();
+
+            if (window.Contains('?'))
+            {
+                return false;
+            }
+
+            int index = window.IndexOf('#');
+
+            if (index < 0)
+            {
+                return false;
+            }
+
+            window = window[index..];
+
+            int validChunks = 0;
+
+            foreach (int expected in Counts)
+            {
+                int count = 0;
+
+                while (!window.IsEmpty && window[0] == '#')
+                {
+                    count++;
+                    window = window[1..];
+                }
+
+                if (count != expected)
+                {
+                    break;
+                }
+
+                validChunks++;
+
+                while (!window.IsEmpty && window[0] == '.')
+                {
+                    window = window[1..];
+                }
+
+                if (window.IsEmpty)
+                {
+                    break;
+                }
+            }
+
+            if (window.Contains('#'))
+            {
+                return false;
+            }
+
+            return validChunks == Counts.Length;
+        }
+    }
+
+    private sealed class PartialSpringRecord : IGraph<State>
+    {
+        public IEnumerable<State> Neighbors(State id)
+        {
+            string springs = id.Values;
+
+            for (int i = springs.IndexOf('?', StringComparison.Ordinal); i > -1;)
+            {
+                string next = string.Create(springs.Length, (id.Values, i), (span, state) =>
+                {
+                    for (int j = 0; j < span.Length; j++)
+                    {
+                        span[j] = j == state.i ? '#' : state.Values[j];
+                    }
+                });
+
+                yield return new State(next, id.Counts, id.Actual + 1, id.Desired);
+
+                next = string.Create(springs.Length, (id.Values, i), (span, state) =>
+                {
+                    for (int j = 0; j < span.Length; j++)
+                    {
+                        span[j] = j == state.i ? '.' : state.Values[j];
+                    }
+                });
+
+                yield return new State(next, id.Counts, id.Actual, id.Desired);
+
+                i = springs.IndexOf('?', i + 1);
+            }
+        }
     }
 }
