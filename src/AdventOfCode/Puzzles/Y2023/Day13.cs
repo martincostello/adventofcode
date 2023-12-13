f45cc851928fd9e9b34faf4e35230dbd0c2df424
martincostello <martin@martincostello.com>
2023-12-13 10:25:40 +0000

    Solve day 13 part 1
    Solve part 1 of the puzzle for day 13.

--- src/AdventOfCode/Puzzles/Y2023/Day13.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day13.cs
@@ -6,18 +6,106 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/13</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 13, "", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 13, "", RequiresData = true)]
 public sealed class Day13 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    /// <summary>
+    /// Gets the number after summarizing all of the notes.
+    /// </summary>
+    public int Summary { get; private set; }
 
-    public int Solution { get; private set; }
-
-    public static int Solve(IList<string> values)
+    /// <summary>
+    /// Finds the lines of symmetry for the specified mirrors.
+    /// </summary>
+    /// <param name="notes">The notes containing the mirrors.</param>
+    /// <returns>
+    /// The number after summarizing all of the notes.
+    /// </returns>
+    public static int Summarize(IList<string> notes)
     {
-        return -1;
+        var mirrors = notes.ToArray().AsSpan();
+        int sum = 0;
+
+        while (!mirrors.IsEmpty)
+        {
+            int length = mirrors.IndexOf(string.Empty);
+
+            if (length == -1)
+            {
+                length = mirrors.Length;
+            }
+
+            var pattern = mirrors[..length];
+            mirrors = mirrors[Math.Min(mirrors.Length, length + 1)..];
+
+            var bounds = new Rectangle(0, 0, pattern[0].Length, pattern.Length);
+
+            int symmetry = FindSymmetry(pattern, bounds);
+            sum += symmetry;
+        }
+
+        return sum;
+
+        static int FindSymmetry(ReadOnlySpan<string> pattern, Rectangle bounds)
+        {
+            for (int x = 1; x < bounds.Width; x++)
+            {
+                bool symmetric = true;
+
+                for (int leftX = x - 1, rightX = x; symmetric && leftX > -1 && rightX < bounds.Width; leftX--, rightX++)
+                {
+                    symmetric &= HasVerticalSymmetry(pattern, leftX, rightX, bounds);
+                }
+
+                if (symmetric)
+                {
+                    return x;
+                }
+            }
+
+            for (int y = 1; y < bounds.Height; y++)
+            {
+                bool symmetric = true;
+
+                for (int leftY = y - 1, rightY = y; symmetric && leftY > -1 && rightY < bounds.Height; leftY--, rightY++)
+                {
+                    symmetric &= HasHorizontalSymmetry(pattern, leftY, rightY, bounds);
+                }
+
+                if (symmetric)
+                {
+                    return y * 100;
+                }
+            }
+
+            throw new PuzzleException("Failed to find any symmetry.");
+
+            static bool HasHorizontalSymmetry(ReadOnlySpan<string> pattern, int topY, int bottomY, Rectangle bounds)
+            {
+                int count = 0;
+                int x = 0;
+
+                while (x < bounds.Width && pattern[topY][x] == pattern[bottomY][x++])
+                {
+                    count++;
+                }
+
+                return count == bounds.Width;
+            }
+
+            static bool HasVerticalSymmetry(ReadOnlySpan<string> pattern, int leftX, int rightX, Rectangle bounds)
+            {
+                int count = 0;
+                int y = 0;
+
+                while (y < bounds.Height && pattern[y][leftX] == pattern[y++][rightX])
+                {
+                    count++;
+                }
+
+                return count == bounds.Height;
+            }
+        }
     }
 
     /// <inheritdoc />
@@ -27,13 +115,13 @@ public sealed class Day13 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        Summary = Summarize(values);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The number after summarizing all of the notes is {0}.", Summary);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(Summary);
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day13Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day13Tests.cs
@@ -5,23 +5,37 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day13Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2023_Day13_Solve_Returns_Correct_Value()
+    [Fact]
+    public void Y2023_Day13_Summarize_Returns_Correct_Value()
     {
         // Arrange
-        string[] values =
+        string[] notes =
         [
-            "_",
+            "#.##..##.",
+            "..#.##.#.",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "..##..##.",
+            "#.#.##.#.",
+            string.Empty,
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###",
+            "#....#..#",
         ];
 
         // Act
-        int actual = Day13.Solve(values);
+        int actual = Day13.Summarize(notes);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(405);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day13_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +43,6 @@ public sealed class Day13Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.Summary.ShouldBe(35538);
     }
 }
