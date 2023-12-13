605ff552a0ef8a25bcfca0b019fe8c02d0b9bfaa
martincostello <martin@martincostello.com>
2023-12-13 12:47:49 +0000

    Partially solve day 13 part 2
    Partially solve part 2 of the puzzle for day 13.

--- tests/AdventOfCode.Tests/Api/ApiTests.cs
+++ tests/AdventOfCode.Tests/Api/ApiTests.cs
@@ -193,7 +193,7 @@ public class ApiTests(HttpServerFixture fixture, ITestOutputHelper outputHelper)
     [PuzzleData(2023, 10, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 11, 9605127, 458191688761)]
     [PuzzleData(2023, 12, -1, Skip = "Not implemented.")]
-    [PuzzleData(2023, 13, 35538)]
+    [PuzzleData(2023, 13, 35538, -1)]
     [PuzzleData(2023, 14, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 15, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 16, -1, Skip = "Not implemented.")]
