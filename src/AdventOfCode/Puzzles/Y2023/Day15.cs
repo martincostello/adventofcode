ce999ffb74c0d360cd011bedd503df47dd426043
martincostello <martin@martincostello.com>
2023-12-15 10:46:03 +0000

    Solve day 15 part 2
    Solve part 2 of the puzzle for day 15.

--- src/AdventOfCode/Puzzles/Y2023/Day15.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day15.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+#pragma warning disable SA1008
+
+using System.Runtime.CompilerServices;
+using Lens = (string Sticker, int FocalLength);
+
 namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 /// <summary>
@@ -15,39 +20,103 @@ public sealed class Day15 : Puzzle
     public int HashSum { get; private set; }
 
     /// <summary>
+    /// Gets the focusing power of the lens configuration.
+    /// </summary>
+    public int FocusingPower { get; private set; }
+
+    /// <summary>
     /// Computes the hash of the specified initialization sequence.
     /// </summary>
-    /// <param name="initializationSequence">The sequence to hash.</param>
+    /// <param name="sequence">The sequence to hash.</param>
     /// <returns>
-    /// The sum of the hash values of <paramref name="initializationSequence"/>.
+    /// The sum of the hash values of <paramref name="sequence"/>.
     /// </returns>
-    public static int Hash(ReadOnlySpan<char> initializationSequence)
+    public static int HashSequence(ReadOnlySpan<char> sequence)
     {
         int sum = 0;
         int next;
 
-        while ((next = initializationSequence.IndexOf(',')) != -1)
+        while ((next = sequence.IndexOf(',')) != -1)
         {
-            sum += Hash(initializationSequence[..next]);
-            initializationSequence = initializationSequence[(next + 1)..];
+            sum += Hash(sequence[..next]);
+            sequence = sequence[(next + 1)..];
         }
 
-        sum += Hash(initializationSequence);
+        sum += Hash(sequence);
 
         return sum;
+    }
 
-        static int Hash(ReadOnlySpan<char> value)
+    /// <summary>
+    /// Initializes the lenses using the specified initialization sequence.
+    /// </summary>
+    /// <param name="sequence">The initialization sequence.</param>
+    /// <returns>
+    /// The focusing power of the lens configuration.
+    /// </returns>
+    public static int Initialize(ReadOnlySpan<char> sequence)
+    {
+        var boxes = new Dictionary<int, List<Lens>>();
+
+        int power = 0;
+        int next;
+
+        while ((next = sequence.IndexOf(',')) != -1)
         {
-            int hash = 0;
+            Shuffle(sequence, next, boxes);
+            sequence = sequence[(next + 1)..];
+        }
 
-            for (int i = 0; i < value.Length; i++)
+        Shuffle(sequence, sequence.Length, boxes);
+
+        foreach (var (box, lenses) in boxes)
+        {
+            foreach (var ((_, focalLength), index) in lenses.Select((p, i) => (p, i + 1)))
             {
-                hash += value[i];
-                hash *= 17;
-                hash %= 256;
+                power += (box + 1) * index * focalLength;
+            }
+        }
+
+        return power;
+
+        static void Shuffle(ReadOnlySpan<char> sequence, int next, Dictionary<int, List<Lens>> boxes)
+        {
+            var step = sequence[..next];
+            int index = step.IndexOfAnyExceptInRange('a', 'z');
+
+            var label = step[..index];
+            int box = Hash(label);
+
+            if (!boxes.TryGetValue(box, out var lenses))
+            {
+                boxes[box] = lenses = [];
             }
 
-            return hash;
+            var operation = step[index..];
+            char symbol = operation[0];
+
+            string sticker = new(label);
+            index = lenses.FindIndex((p) => p.Sticker == sticker);
+
+            if (index > -1)
+            {
+                lenses.RemoveAt(index);
+            }
+
+            if (symbol is '=')
+            {
+                int focalLength = Parse<int>(operation[1..]);
+                var lens = (sticker, focalLength);
+
+                if (index is -1)
+                {
+                    lenses.Add(lens);
+                }
+                else
+                {
+                    lenses.Insert(index, lens);
+                }
+            }
         }
     }
 
@@ -57,14 +126,32 @@ public sealed class Day15 : Puzzle
         ArgumentNullException.ThrowIfNull(args);
 
         string initializationSequence = await ReadResourceAsStringAsync(cancellationToken);
+        initializationSequence = initializationSequence.ReplaceLineEndings(string.Empty);
 
-        HashSum = Hash(initializationSequence.ReplaceLineEndings(string.Empty));
+        HashSum = HashSequence(initializationSequence);
+        FocusingPower = Initialize(initializationSequence);
 
         if (Verbose)
         {
             Logger.WriteLine("The sum of the hash values of the initialization sequence is {0}", HashSum);
+            Logger.WriteLine("The focusing power of the lens configuration is {0}", FocusingPower);
         }
 
-        return PuzzleResult.Create(HashSum);
+        return PuzzleResult.Create(HashSum, FocusingPower);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int Hash(ReadOnlySpan<char> value)
+    {
+        int hash = 0;
+
+        for (int i = 0; i < value.Length; i++)
+        {
+            hash += value[i];
+            hash *= 17;
+            hash %= 256;
+        }
+
+        return hash;
     }
 }
--- tests/AdventOfCode.Tests/Api/ApiTests.cs
+++ tests/AdventOfCode.Tests/Api/ApiTests.cs
@@ -195,7 +195,7 @@ public class ApiTests(HttpServerFixture fixture, ITestOutputHelper outputHelper)
     [PuzzleData(2023, 12, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 13, 35538, 30442)]
     [PuzzleData(2023, 14, 108641, 84328, ExpectedVisualizations = 2)]
-    [PuzzleData(2023, 15, 511215)]
+    [PuzzleData(2023, 15, 511215, 236057)]
     [PuzzleData(2023, 16, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 17, -1, Skip = "Not implemented.")]
     [PuzzleData(2023, 18, -1, Skip = "Not implemented.")]
