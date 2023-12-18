26c5c2112b5ca14b97951da6445391612e9bc3da
martincostello <martin@martincostello.com>
2023-12-18 09:10:52 +0000

    Initial musings

--- src/AdventOfCode/Puzzles/Y2023/Day17.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day17.cs
@@ -6,18 +6,42 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/17</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 17, "Clumsy Crucible", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 17, "Clumsy Crucible", RequiresData = true)]
 public sealed class Day17 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    /// <summary>
+    /// Gets the minimum heat loss that can be incurred.
+    /// </summary>
+    public int MinimumHeatLoss { get; private set; }
 
-    public int Solution { get; private set; }
-
-    public static int Solve(IList<string> values)
+    /// <summary>
+    /// Finds the minimum heat loss that can be incurred moving the crucible.
+    /// </summary>
+    /// <param name="map">A map of the heat losses that are incurred.</param>
+    /// <returns>
+    /// The minimum heat loss that can be incurred moving the crucible.
+    /// </returns>
+    public static int Solve(IList<string> map)
     {
-        return -1;
+        var heatLosses = new Dictionary<Point, int>();
+
+        for (int y = 0; y < map.Count; y++)
+        {
+            string row = map[y];
+
+            for (int x = 0; x < row.Length; x++)
+            {
+                heatLosses[new(x, y)] = row[x] - '0';
+            }
+        }
+
+        int height = map.Count;
+        int width = map[0].Length;
+
+        var grid = new HeatLossMap(width, height, heatLosses);
+        var goal = new Point(width - 1, height - 1);
+
+        return (int)PathFinding.AStar(grid, new(Point.Empty, Size.Empty, 0), new(goal, Size.Empty, 0));
     }
 
     /// <inheritdoc />
@@ -27,13 +51,78 @@ public sealed class Day17 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        MinimumHeatLoss = Solve(values);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The minimum heat loss that can be incurred is {0}.", MinimumHeatLoss);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(MinimumHeatLoss);
+    }
+
+    private record struct Move(Point Location, Size Direction, int Steps)
+    {
+        public override readonly int GetHashCode() => Location.GetHashCode();
+    }
+
+    private sealed class HeatLossMap(int width, int height, Dictionary<Point, int> heatLosses) : IWeightedGraph<Move>
+    {
+        private static readonly ImmutableArray<Size> Directions =
+        [
+            new(0, 1),
+            new(1, 0),
+            new(0, -1),
+            new(-1, 0),
+        ];
+
+        private readonly Rectangle _bounds = new(0, 0, width, height);
+
+        public long Cost(Move a, Move b)
+        {
+            if (a.Location == b.Location)
+            {
+                return 0;
+            }
+            else if (a.Location.ManhattanDistance(b.Location) is 1)
+            {
+                return heatLosses.GetValueOrDefault(b.Location);
+            }
+            else
+            {
+                return long.MaxValue;
+            }
+        }
+
+        public bool Equals(Move x, Move y) => x.Location == y.Location;
+
+        public int GetHashCode(Move obj) => obj.Location.GetHashCode();
+
+        public IEnumerable<Move> Neighbors(Move id)
+        {
+            var backwards = id.Direction * -1;
+
+            for (int i = 0; i < Directions.Length; i++)
+            {
+                var direction = Directions[i];
+
+                if (direction == backwards)
+                {
+                    continue;
+                }
+
+                var next = id.Location + direction;
+
+                if (_bounds.Contains(next))
+                {
+                    int steps = direction == id.Direction ? id.Steps + 1 : 0;
+
+                    if (steps < 4)
+                    {
+                        yield return new(next, direction, steps);
+                    }
+                }
+            }
+        }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day17Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day17Tests.cs
@@ -5,23 +5,35 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day17Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public void Y2023_Day17_Solve_Returns_Correct_Value()
     {
         // Arrange
         string[] values =
         [
-            "_",
+            "2413432311323",
+            "3215453535623",
+            "3255245654254",
+            "3446585845452",
+            "4546657867536",
+            "1438598798454",
+            "4457876987766",
+            "3637877979653",
+            "4654967986887",
+            "4564679986453",
+            "1224686865563",
+            "2546548887735",
+            "4322674655533",
         ];
 
         // Act
         int actual = Day17.Solve(values);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(102);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day17_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +41,7 @@ public sealed class Day17Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.MinimumHeatLoss.ShouldBeGreaterThan(1009);
+        puzzle.MinimumHeatLoss.ShouldBe(-1);
     }
 }
