9eea2e7ffae63393e431bf7fa585530fa5f7f2fa
martincostello <martin@martincostello.com>
2023-12-19 09:58:35 +0000

    Solve day 19 part 1
    Solve part 1 of the puzzle for day 19.

--- src/AdventOfCode/Puzzles/Y2023/Day19.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day19.cs
@@ -6,18 +6,175 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/19</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 19, "Aplenty", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 19, "Aplenty", RequiresData = true)]
 public sealed class Day19 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    private delegate (string? Next, bool? Result) Analyzer(Part part);
 
-    public int Solution { get; private set; }
+    /// <summary>
+    /// Gets the sum of the rating numbers of all the parts that are accepted.
+    /// </summary>
+    public int RatingNumbersSum { get; private set; }
 
-    public static int Solve(IList<string> values)
+    /// <summary>
+    /// Runs the workflows for the specified parts.
+    /// </summary>
+    /// <param name="values">The workflows and parts to run.</param>
+    /// <returns>
+    /// The sum of the rating numbers of all the parts that are accepted.
+    /// </returns>
+    public static int Run(IList<string> values)
     {
-        return -1;
+        var rawWorkflows = new List<string>();
+        var rawParts = new List<string>();
+        var current = rawWorkflows;
+
+        foreach (string value in values)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                current = rawParts;
+                continue;
+            }
+
+            current.Add(value);
+        }
+
+        var parts = ParseParts(rawParts);
+        var workflows = ParseWorkflows(rawWorkflows);
+
+        var accepted = new List<Part>();
+        var workflow = workflows["in"];
+
+        foreach (var part in parts)
+        {
+            if (workflow.Analyze(part))
+            {
+                accepted.Add(part);
+            }
+        }
+
+        return accepted.Sum((p) => p.RatingNumber);
+
+        static List<Part> ParseParts(List<string> values)
+        {
+            var parts = new List<Part>(values.Count);
+
+            foreach (string part in values)
+            {
+                string[] categories = part[1..^1].Split(',');
+
+                int x = Parse<int>(categories[0][2..]);
+                int m = Parse<int>(categories[1][2..]);
+                int a = Parse<int>(categories[2][2..]);
+                int s = Parse<int>(categories[3][2..]);
+
+                parts.Add(new(x, m, a, s));
+            }
+
+            return parts;
+        }
+
+        static Dictionary<string, Workflow> ParseWorkflows(List<string> values)
+        {
+            var workflows = new Dictionary<string, Workflow>(values.Count);
+
+            var comparer = Comparer<int>.Default;
+            var accept = new Analyzer(static (p) => (null, true));
+            var reject = new Analyzer(static (p) => (null, false));
+
+            foreach (string value in values)
+            {
+                var span = value.AsSpan();
+                int index = span.IndexOf('{');
+
+                var name = span[..index];
+                var rules = span[(index + 1)..^1];
+
+                var analyzers = new List<Analyzer>();
+
+                while (rules.Length > 0)
+                {
+                    index = rules.IndexOf(',');
+
+                    var rule = rules;
+
+                    if (index is not -1)
+                    {
+                        rule = rules[..index];
+                    }
+
+                    int pointer = rule.IndexOf(':');
+
+                    if (pointer is -1)
+                    {
+                        string other = new(rule);
+
+                        analyzers.Add(rule[0] switch
+                        {
+                            'A' => accept,
+                            'R' => reject,
+                            _ => new((_) => (other, null)),
+                        });
+                    }
+                    else
+                    {
+                        char category = rule[0];
+                        char op = rule[1];
+                        int operand = Parse<int>(rule[2..pointer]);
+                        string other = new(rule[(pointer + 1)..]);
+
+                        int sign = op switch
+                        {
+                            '<' => -1,
+                            '>' => 1,
+                            _ => throw new PuzzleException($"Unknown operator '{op}'."),
+                        };
+
+                        Func<Part, int> categoryProvider = category switch
+                        {
+                            'x' => (p) => p.X,
+                            'm' => (p) => p.M,
+                            'a' => (p) => p.A,
+                            's' => (p) => p.S,
+                            _ => throw new PuzzleException($"Unknown category '{category}'."),
+                        };
+
+                        (string? Next, bool? Result) result = other switch
+                        {
+                            "A" => (null, true),
+                            "R" => (null, false),
+                            _ => (other, null),
+                        };
+
+                        analyzers.Add((p) =>
+                        {
+                            int value = categoryProvider(p);
+                            int comparison = comparer.Compare(value, operand);
+                            bool matches = Math.Sign(comparison) == sign;
+
+                            if (matches)
+                            {
+                                return result;
+                            }
+
+                            return (null, null);
+                        });
+                    }
+
+                    if (index is -1)
+                    {
+                        break;
+                    }
+
+                    rules = rules[(index + 1)..];
+                }
+
+                workflows[new(name)] = new(analyzers, workflows);
+            }
+
+            return workflows;
+        }
     }
 
     /// <inheritdoc />
@@ -27,13 +184,40 @@ public sealed class Day19 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        RatingNumbersSum = Run(values);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The sum of the rating numbers of all the accepted parts is {0}.", RatingNumbersSum);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(RatingNumbersSum);
+    }
+
+    private sealed record Part(int X, int M, int A, int S)
+    {
+        public int RatingNumber { get; } = X + M + A + S;
+    }
+
+    private sealed class Workflow(List<Analyzer> analyzers, Dictionary<string, Workflow> workflows)
+    {
+        public bool Analyze(Part part)
+        {
+            foreach (var analyzer in analyzers)
+            {
+                var (next, result) = analyzer(part);
+
+                if (next is { } name)
+                {
+                    return workflows[name].Analyze(part);
+                }
+                else if (result is { } decision)
+                {
+                    return decision;
+                }
+            }
+
+            return false;
+        }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day19Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day19Tests.cs
@@ -5,23 +5,39 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day19Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2023_Day19_Solve_Returns_Correct_Value()
+    [Fact]
+    public void Y2023_Day19_Run_Returns_Correct_Value()
     {
         // Arrange
         string[] values =
         [
-            "_",
+            "px{a<2006:qkq,m>2090:A,rfg}",
+            "pv{a>1716:R,A}",
+            "lnx{m>1548:A,A}",
+            "rfg{s<537:gd,x>2440:R,A}",
+            "qs{s>3448:A,lnx}",
+            "qkq{x<1416:A,crn}",
+            "crn{x>2662:A,R}",
+            "in{s<1351:px,qqz}",
+            "qqz{s>2770:qs,m<1801:hdj,R}",
+            "gd{a>3333:R,R}",
+            "hdj{m>838:A,pv}",
+            string.Empty,
+            "{x=787,m=2655,a=1222,s=2876}",
+            "{x=1679,m=44,a=2067,s=496}",
+            "{x=2036,m=264,a=79,s=2244}",
+            "{x=2461,m=1339,a=466,s=291}",
+            "{x=2127,m=1623,a=2188,s=1013}",
         ];
 
         // Act
-        int actual = Day19.Solve(values);
+        int actual = Day19.Run(values);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(19114);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day19_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +45,6 @@ public sealed class Day19Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.RatingNumbersSum.ShouldBe(398527);
     }
 }
