c3f5c13e242fdbf99662e1b7ac5286b0adfaec1b
martincostello <martin@martincostello.com>
2023-12-20 11:29:21 +0000

    Partially solve day 20 part 1
    Partially solve part 1 of the puzzle for day 20.

--- src/AdventOfCode/Puzzles/Y2023/Day20.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day20.cs
@@ -1,23 +1,113 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
+
 namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/20</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 20, "Pulse Propagation", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 20, "Pulse Propagation", RequiresData = true)]
 public sealed class Day20 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    private const string Broadcaster = "broadcaster";
+    private const string Button = "button";
 
-    public int Solution { get; private set; }
-
-    public static int Solve(IList<string> values)
+    private enum Pulse
     {
-        return -1;
+        Low = 1,
+        High,
+    }
+
+    /// <summary>
+    /// Gets the product of the count of the number of
+    /// high and low pulses sent ater 1,000 presses of the button.
+    /// </summary>
+    public int PulsesProduct { get; private set; }
+
+    /// <summary>
+    /// Presses the button connecting the modules the specified number of times.
+    /// </summary>
+    /// <param name="configuration">The configuration of the modules.</param>
+    /// <param name="presses">The number of times to press the button.</param>
+    /// <returns>
+    /// The product of the count of the number of high and low pulses sent.
+    /// </returns>
+    public static int Run(IList<string> configuration, int presses)
+    {
+        var button = new ButtonModule();
+
+        var modules = new Dictionary<string, Module>()
+        {
+            [Button] = button,
+        };
+
+        var connections = new Dictionary<string, IList<string>>();
+
+        foreach (string config in configuration)
+        {
+            string[] parts = config.Split(" -> ");
+            char type = parts[0][0];
+            string name = parts[0][1..];
+            string[] outputs = parts[1].Split(", ");
+
+            Module module = type switch
+            {
+                'b' => new BroadcasterModule(),
+                '&' => new ConjunctionModule(name),
+                '%' => new FlipFlopModule(name),
+                _ => throw new PuzzleException($"Unknown module type '{type}'."),
+            };
+
+            modules[module.Name] = module;
+            connections[module.Name] = outputs;
+        }
+
+        button.Outputs.Add(modules[Broadcaster]);
+
+        int highPulses = 0;
+        int lowPulses = 0;
+
+        foreach (var module in modules.Values.ToArray())
+        {
+            if (!connections.TryGetValue(module.Name, out var outputs))
+            {
+                continue;
+            }
+
+            module.Received += OnPulse;
+
+            foreach (string name in outputs)
+            {
+                if (!modules.TryGetValue(name, out var next))
+                {
+                    next = modules[name] = new OutputModule(name);
+                    next.Received += OnPulse;
+                }
+
+                module.Outputs.Add(next);
+            }
+        }
+
+        for (int i = 0; i < presses; i++)
+        {
+            button.Press();
+        }
+
+        return highPulses * lowPulses;
+
+        void OnPulse(object? sender, Pulse p)
+        {
+            if (p is Pulse.High)
+            {
+                highPulses++;
+            }
+            else
+            {
+                lowPulses++;
+            }
+        }
     }
 
     /// <inheritdoc />
@@ -25,15 +115,116 @@ public sealed class Day20 : Puzzle
     {
         ArgumentNullException.ThrowIfNull(args);
 
-        var values = await ReadResourceAsLinesAsync(cancellationToken);
+        var configuration = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        PulsesProduct = Run(configuration, presses: 1_000);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The total number of low pulses sent multiplied by the total number of high pulses sent is {0}.", PulsesProduct);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(PulsesProduct);
+    }
+
+    [DebuggerDisplay("{Name,nq} ({Type,nq})")]
+    private abstract class Module(string name)
+    {
+        public event EventHandler<Pulse>? Received;
+
+        public List<Module> Outputs { get; } = [];
+
+        public string Name { get; } = name;
+
+        public abstract string Type { get; }
+
+        public abstract void Receive(Module sender, Pulse pulse);
+
+        protected virtual void OnReceived(Pulse pulse)
+            => Received?.Invoke(this, pulse);
+    }
+
+    private sealed class FlipFlopModule(string name) : Module(name)
+    {
+        public bool On { get; private set; }
+
+        public override string Type { get; } = "Flip-flop";
+
+        public override void Receive(Module sender, Pulse pulse)
+        {
+            OnReceived(pulse);
+
+            if (pulse is Pulse.Low)
+            {
+                pulse = On ? Pulse.Low : Pulse.High;
+                On = !On;
+
+                foreach (var output in Outputs)
+                {
+                    output.Receive(this, pulse);
+                }
+            }
+        }
+    }
+
+    private sealed class ConjunctionModule(string name) : Module(name)
+    {
+        private readonly Dictionary<Module, Pulse> _inputs = [];
+
+        public override string Type { get; } = "Conjunction";
+
+        public override void Receive(Module sender, Pulse pulse)
+        {
+            OnReceived(pulse);
+
+            _inputs[sender] = pulse;
+
+            pulse = _inputs.Values.All((p) => p is Pulse.High) ? Pulse.Low : Pulse.High;
+
+            foreach (var output in Outputs)
+            {
+                output.Receive(this, pulse);
+            }
+        }
+    }
+
+    private sealed class BroadcasterModule() : Module(Broadcaster)
+    {
+        public override string Type { get; } = "Broadcast";
+
+        public override void Receive(Module sender, Pulse pulse)
+        {
+            OnReceived(pulse);
+
+            foreach (var output in Outputs)
+            {
+                output.Receive(this, pulse);
+            }
+        }
+    }
+
+    private sealed class ButtonModule() : Module(Button)
+    {
+        public override string Type { get; } = "Button";
+
+        public void Press() => Receive(this, Pulse.Low);
+
+        public override void Receive(Module sender, Pulse pulse)
+        {
+            OnReceived(pulse);
+
+            foreach (var output in Outputs)
+            {
+                output.Receive(this, pulse);
+            }
+        }
+    }
+
+    private sealed class OutputModule(string name) : Module(name)
+    {
+        public override string Type { get; } = "Output";
+
+        public override void Receive(Module sender, Pulse pulse)
+            => OnReceived(pulse);
     }
 }
