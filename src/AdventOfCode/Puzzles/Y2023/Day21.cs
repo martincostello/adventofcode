3a1a594ac6ccb18076554be7373a485bcc66521c
martincostello <martin@martincostello.com>
2023-12-21 10:27:15 +0000

    Solve day 21 part 1
    Solve part 1 of the puzzle for day 21.

--- src/AdventOfCode/Puzzles/Y2023/Day21.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day21.cs
@@ -6,18 +6,51 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/21</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 21, "Step Counter", RequiresData = true, IsHidden = true)]
+[Puzzle(2023, 21, "Step Counter", RequiresData = true)]
 public sealed class Day21 : Puzzle
 {
-#pragma warning disable IDE0022
-#pragma warning disable IDE0060
-#pragma warning disable SA1600
+    /// <summary>
+    /// Gets the number of plots the elf can reach in exactly 64 steps.
+    /// </summary>
+    public int Plots64 { get; private set; }
 
-    public int Solution { get; private set; }
-
-    public static int Solve(IList<string> values)
+    /// <summary>
+    /// Finds the number of plots the elf can reach in the specified number of steps.
+    /// </summary>
+    /// <param name="map">The map of the garden.</param>
+    /// <param name="steps">The number of steps to walk.</param>
+    /// <returns>
+    /// The number of plots the elf can reach in exactly <paramref name="steps"/> steps.
+    /// </returns>
+    public static int Walk(IList<string> map, int steps)
     {
-        return -1;
+        int height = map.Count;
+        int width = map[0].Length;
+
+        var origin = Point.Empty;
+        var grid = new SquareGrid(width, height);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                char tile = map[y][x];
+
+                if (tile is '#')
+                {
+                    grid.Borders.Add(new(x, y));
+                }
+                else if (tile is 'S')
+                {
+                    origin = new(x, y);
+                }
+            }
+        }
+
+        var garden = new Garden(grid, steps);
+
+        var walk = PathFinding.BreadthFirst(garden, new(origin, 0), CancellationToken.None);
+        return walk.Where((p) => p.Steps == steps).Select((p) => p.Location).Distinct().Count();
     }
 
     /// <inheritdoc />
@@ -25,15 +58,42 @@ public sealed class Day21 : Puzzle
     {
         ArgumentNullException.ThrowIfNull(args);
 
-        var values = await ReadResourceAsLinesAsync(cancellationToken);
+        var map = await ReadResourceAsLinesAsync(cancellationToken);
 
-        Solution = Solve(values);
+        Plots64 = Walk(map, steps: 64);
 
         if (Verbose)
         {
-            Logger.WriteLine("{0}", Solution);
+            Logger.WriteLine("The elf could reach {0} plots in exactly 64 steps.", Plots64);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(Plots64);
+    }
+
+    private sealed record Step(Point Location, int Steps);
+
+    private sealed class Garden(SquareGrid grid, int stepGoal) : IGraph<Step>
+    {
+        public IEnumerable<Step> Neighbors(Step id)
+        {
+            int steps = id.Steps + 1;
+
+            if (steps > stepGoal)
+            {
+                yield break;
+            }
+
+            var vectors = Directions.All;
+
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                Point next = id.Location + vectors[i];
+
+                if (grid.InBounds(next) && grid.IsPassable(next))
+                {
+                    yield return new(next, steps);
+                }
+            }
+        }
     }
 }
--- tests/AdventOfCode.Tests/Puzzles/Y2023/Day21Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2023/Day21Tests.cs
@@ -5,23 +5,37 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 
 public sealed class Day21Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2023_Day21_Solve_Returns_Correct_Value()
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 4)]
+    [InlineData(3, 6)]
+    [InlineData(6, 16)]
+    public void Y2023_Day21_Solve_Returns_Correct_Value(int steps, int expected)
     {
         // Arrange
         string[] values =
         [
-            "_",
+            "...........",
+            ".....###.#.",
+            ".###.##..#.",
+            "..#.#...#..",
+            "....#.#....",
+            ".##..S####.",
+            ".##..#...#.",
+            ".......##..",
+            ".##.#.####.",
+            ".##..##.##.",
+            "...........",
         ];
 
         // Act
-        int actual = Day21.Solve(values);
+        int actual = Day21.Walk(values, steps);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(expected);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2023_Day21_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +43,6 @@ public sealed class Day21Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.Plots64.ShouldBe(3782);
     }
 }
