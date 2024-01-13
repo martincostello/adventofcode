11622b3d89363ccc500011dfa786dd483329d755
martincostello <martin@martincostello.com>
2024-01-13 12:52:40 +0000

    Mark unsolved
    Mark day 17 for 2023 as unsolved.

--- src/AdventOfCode/Puzzles/Y2023/Day17.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day17.cs
@@ -6,7 +6,7 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/17</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 17, "Clumsy Crucible", RequiresData = true)]
+[Puzzle(2023, 17, "Clumsy Crucible", RequiresData = true, Unsolved = true)]
 public sealed class Day17 : Puzzle
 {
     /// <summary>
