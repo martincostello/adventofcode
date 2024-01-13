55aa497d26d7ba2e307e0c8a48fc1ffa38fc9c42
martincostello <martin@martincostello.com>
2024-01-13 12:38:44 +0000

    Mark unsolved
    Mark day 12 for 2023 as unsolved.

--- src/AdventOfCode/Puzzles/Y2023/Day12.cs
+++ src/AdventOfCode/Puzzles/Y2023/Day12.cs
@@ -6,7 +6,7 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2023;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2023/day/12</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2023, 12, "Hot Springs", RequiresData = true)]
+[Puzzle(2023, 12, "Hot Springs", RequiresData = true, Unsolved = true)]
 public sealed class Day12 : Puzzle
 {
     private const char Damaged = '#';
