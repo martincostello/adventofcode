a4757c6239c2fc5c044bcf50ed1630bb98be755a
martincostello <martin@martincostello.com>
2024-01-17 10:51:46 +0000

    Use AssemblyAttribute item
    Use an `AssemblyAttribute` MSBuild item instead of `[Guid("...")]`.

--- src/AdventOfCode.Resources/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
-// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
-
-using System.Runtime.InteropServices;
-
-[assembly: Guid("b128af97-316b-4726-b31b-3f7c0c5d22c2")]
