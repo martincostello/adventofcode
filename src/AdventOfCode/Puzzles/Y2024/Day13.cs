e2cc1c7a21a37aab3ac8e9277c73247ddd79c169
martincostello <martin@martincostello.com>
2024-12-13 21:12:45 +0000

    Solve day 13 part 2
    Solve part 2 of the puzzle for day 13.

--- src/AdventOfCode/Puzzles/Y2024/Day13.cs
+++ src/AdventOfCode/Puzzles/Y2024/Day13.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Point = (long X, long Y);
+
 namespace MartinCostello.AdventOfCode.Puzzles.Y2024;
 
 /// <summary>
@@ -12,16 +14,23 @@ public sealed class Day13 : Puzzle
     /// <summary>
     /// Gets the fewest tokens needed to be spent to win all possible prizes.
     /// </summary>
-    public int FewestTokens { get; private set; }
+    public long FewestTokens { get; private set; }
+
+    /// <summary>
+    /// Gets the fewest tokens needed to be spent to win all possible prizes with the correct offset.
+    /// </summary>
+    public long FewestTokensFixed { get; private set; }
 
     /// <summary>
     /// Plays the specified claw machines.
     /// </summary>
     /// <param name="machines">The specification of the claw machines to play.</param>
+    /// <param name="offset">The claw offset to use.</param>
+    /// <param name="limit">The limit of turns to use when finding a solution.</param>
     /// <returns>
     /// The fewest tokens needed to be spent to win all possible prizes.
     /// </returns>
-    public static int Play(IList<string> machines)
+    public static long Play(IList<string> machines, long offset, long limit)
     {
         var clawMachines = new List<ClawMachine>();
 
@@ -31,19 +40,21 @@ public sealed class Day13 : Puzzle
             var b = Parse(machines[i + 1], '+');
             var prize = Parse(machines[i + 2], '=');
 
+            prize = (prize.X + offset, prize.Y + offset);
+
             clawMachines.Add(new(a, b, prize));
         }
 
-        int tokens = 0;
+        long tokens = 0;
 
         foreach (var machine in clawMachines)
         {
-            tokens += machine.Play();
+            tokens += machine.Play(limit);
         }
 
         return tokens;
 
-        static Point Parse(string value, char delimiter)
+        static (long X, long Y) Parse(string value, char delimiter)
         {
             string coordinates = value.Split(':')[1];
             string[] parts = coordinates.Split(',');
@@ -51,10 +62,10 @@ public sealed class Day13 : Puzzle
             var rawX = parts[0].AsSpan();
             var rawY = parts[1].AsSpan();
 
-            int x = Parse<int>(rawX[(rawX.IndexOf(delimiter) + 1)..]);
-            int y = Parse<int>(rawY[(rawY.IndexOf(delimiter) + 1)..]);
+            long x = Parse<long>(rawX[(rawX.IndexOf(delimiter) + 1)..]);
+            long y = Parse<long>(rawY[(rawY.IndexOf(delimiter) + 1)..]);
 
-            return new(x, y);
+            return (x, y);
         }
     }
 
@@ -65,46 +76,48 @@ public sealed class Day13 : Puzzle
 
         var values = await ReadResourceAsLinesAsync(cancellationToken);
 
-        FewestTokens = Play(values);
+        FewestTokens = Play(values, offset: 0, limit: 100);
+        FewestTokensFixed = Play(values, offset: 10_000_000_000_000, limit: long.MaxValue);
 
         if (Verbose)
         {
             Logger.WriteLine("The fewest tokens you would have to spend to win all possible prizes is {0}.", FewestTokens);
+            Logger.WriteLine("The fewest tokens you would have to spend to win all possible prizes with the correct offset is {0}.", FewestTokensFixed);
         }
 
-        return PuzzleResult.Create(FewestTokens);
+        return PuzzleResult.Create(FewestTokens, FewestTokensFixed);
     }
 
     private sealed record ClawMachine(Point A, Point B, Point Prize)
     {
-        public int Play()
+        public long Play(long limit)
         {
             // See https://en.wikipedia.org/wiki/Cramer%27s_rule#Explicit_formulas_for_small_systems
-            int detAB = (A.X * B.Y) - (A.Y * B.X);
-            int detAC = (A.X * Prize.Y) - (A.Y * Prize.X);
+            long detAB = (A.X * B.Y) - (A.Y * B.X);
+            long detAC = (A.X * Prize.Y) - (A.Y * Prize.X);
 
             if (detAC % detAB is not 0)
             {
                 return 0;
             }
 
-            int detCB = (Prize.X * B.Y) - (Prize.Y * B.X);
+            long detCB = (Prize.X * B.Y) - (Prize.Y * B.X);
 
             if (detCB % detAB is not 0)
             {
                 return 0;
             }
 
-            int x = detCB / detAB;
+            long x = detCB / detAB;
 
-            if (x > 100)
+            if (x > limit)
             {
                 return 0;
             }
 
-            int y = detAC / detAB;
+            long y = detAC / detAB;
 
-            if (y > 100)
+            if (y > limit)
             {
                 return 0;
             }
