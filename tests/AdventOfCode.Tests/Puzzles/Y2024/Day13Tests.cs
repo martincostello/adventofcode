d225393b380b14d0c4e4b4226615abf43bfa9125
martincostello <martin@martincostello.com>
2024-12-13 21:02:15 +0000

    Solve day 13 part 1
    Solve part 1 of the puzzle for day 13.

--- tests/AdventOfCode.Tests/Puzzles/Y2024/Day13Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2024/Day13Tests.cs
@@ -5,23 +5,37 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2024;
 
 public sealed class Day13Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2024_Day13_Solve_Returns_Correct_Value()
+    [Fact]
+    public void Y2024_Day13_Play_Returns_Correct_Value()
     {
         // Arrange
         string[] values =
         [
-            "_",
+            "Button A: X+94, Y+34",
+            "Button B: X+22, Y+67",
+            "Prize: X=8400, Y=5400",
+            string.Empty,
+            "Button A: X+26, Y+66",
+            "Button B: X+67, Y+21",
+            "Prize: X=12748, Y=12176",
+            string.Empty,
+            "Button A: X+17, Y+86",
+            "Button B: X+84, Y+37",
+            "Prize: X=7870, Y=6450",
+            string.Empty,
+            "Button A: X+69, Y+23",
+            "Button B: X+27, Y+71",
+            "Prize: X=18641, Y=10279",
         ];
 
         // Act
-        int actual = Day13.Solve(values);
+        int actual = Day13.Play(values);
 
         // Assert
-        actual.ShouldBe(-1);
+        actual.ShouldBe(480);
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Fact]
     public async Task Y2024_Day13_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +43,6 @@ public sealed class Day13Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.FewestTokens.ShouldBe(37680);
     }
 }
