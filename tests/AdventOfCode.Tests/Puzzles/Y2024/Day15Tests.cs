054cd02a32cbed6ffe1801c17bc5d2c3c55911d9
martincostello <martin@martincostello.com>
2024-12-15 09:13:44 +0000

    Solve day 15 part 1
    Solve part 1 of the puzzle for day 15.

--- tests/AdventOfCode.Tests/Puzzles/Y2024/Day15Tests.cs
+++ tests/AdventOfCode.Tests/Puzzles/Y2024/Day15Tests.cs
@@ -5,23 +5,68 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2024;
 
 public sealed class Day15Tests(ITestOutputHelper outputHelper) : PuzzleTest(outputHelper)
 {
-    [Fact(Skip = "Not implemented.")]
-    public void Y2024_Day15_Solve_Returns_Correct_Value()
+    public static TheoryData<string[], int> Examples()
     {
-        // Arrange
-        string[] values =
-        [
-            "_",
-        ];
-
-        // Act
-        int actual = Day15.Solve(values);
-
-        // Assert
-        actual.ShouldBe(-1);
+        return new()
+        {
+            {
+                new[]
+                {
+                    "########",
+                    "#..O.O.#",
+                    "##@.O..#",
+                    "#...O..#",
+                    "#.#.O..#",
+                    "#...O..#",
+                    "#......#",
+                    "########",
+                    string.Empty,
+                    "<^^>>>vv<v>>v<<",
+                },
+                2028
+            },
+            {
+                new[]
+                {
+                    "##########",
+                    "#..O..O.O#",
+                    "#......O.#",
+                    "#.OO..O.O#",
+                    "#..O@..O.#",
+                    "#O#..O...#",
+                    "#O..O..O.#",
+                    "#.OO.O.OO#",
+                    "#....O...#",
+                    "##########",
+                    string.Empty,
+                    "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+                    "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+                    "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+                    "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+                    "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+                    "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+                    ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+                    "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+                    "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+                    "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+                },
+                10092
+            },
+        };
     }
 
-    [Fact(Skip = "Not implemented.")]
+    [Theory]
+    [MemberData(nameof(Examples))]
+    public void Y2024_Day15_Move_Returns_Correct_Value(string[] values, int expected)
+    {
+        // Act
+        int actual = Day15.Move(values);
+
+        // Assert
+        actual.ShouldBe(expected);
+    }
+
+    [Fact]
     public async Task Y2024_Day15_Solve_Returns_Correct_Solution()
     {
         // Act
@@ -29,6 +74,6 @@ public sealed class Day15Tests(ITestOutputHelper outputHelper) : PuzzleTest(outp
 
         // Assert
         puzzle.ShouldNotBeNull();
-        puzzle.Solution.ShouldBe(-1);
+        puzzle.GpsSum.ShouldBe(1465152);
     }
 }
