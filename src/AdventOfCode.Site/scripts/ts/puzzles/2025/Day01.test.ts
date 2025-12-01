dcb8b44f99c48df159be5c96f500155b89b0e70e
martincostello <martin@martincostello.com>
2025-12-01 14:32:33 +0000

    Solve day 1 in TypeScript
    Add TypeScript solution for day 1 of 2025.

--- src/AdventOfCode.Site/scripts/ts/puzzles/2025/Day01.test.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2025/Day01.test.ts
@@ -5,10 +5,17 @@ import { describe, expect, test } from 'vitest';
 import { Day01 } from './index';
 
 describe('2025', () => {
-    describe.skip('Day 01', () => {
-        test.each([[[], 1530215]])('returns correct value for %s', (inputs: string[], expected: number) => {
+    describe('Day 01', () => {
+        test.each([
+            [['L1000'], true, 10],
+            [['L1001'], true, 10],
+            [['R1000'], true, 10],
+            [['R1001'], true, 10],
+            [['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82'], false, 3],
+            [['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82'], true, 6],
+        ])('returns correct value for %s', (inputs: string[], useMethod0x434C49434B: boolean, expected: number) => {
             // Act
-            const actual = Day01.solve(inputs);
+            const actual = Day01.solve(inputs, useMethod0x434C49434B);
 
             // Assert
             expect(actual).toBe(expected);
--- src/AdventOfCode.Site/scripts/ts/puzzles/2025/Day01.ts
+++ src/AdventOfCode.Site/scripts/ts/puzzles/2025/Day01.ts
@@ -4,11 +4,11 @@
 import { Puzzle2025 } from './Puzzle2025';
 
 export class Day01 extends Puzzle2025 {
-    solution1: number;
-    solution2: number;
+    password1: number;
+    password2: number;
 
     override get solved() {
-        return false;
+        return true;
     }
 
     override get name() {
@@ -19,19 +19,46 @@ export class Day01 extends Puzzle2025 {
         return 1;
     }
 
-    static solve(values: string[]): number {
-        return -1;
+    static solve(values: string[], useMethod0x434C49434B: boolean): number {
+        const steps = 100;
+
+        let previous: number;
+        let current = 50;
+        let zeroes = 0;
+
+        for (const rotation of values) {
+            const sign = rotation[0] === 'R' ? 1 : -1;
+            const clicks = parseInt(rotation.substring(1), 10);
+
+            const cycles = Math.floor(clicks / steps);
+            const distance = clicks % steps;
+
+            previous = current;
+            current = Math.abs(current + sign * distance + steps) % steps;
+
+            if (useMethod0x434C49434B) {
+                zeroes += cycles;
+
+                if (previous !== current && (current === 0 || (previous !== 0 && sign * (current - previous) < 0))) {
+                    zeroes++;
+                }
+            } else if (current === 0) {
+                zeroes++;
+            }
+        }
+
+        return zeroes;
     }
 
     override solveCore(_: string[]) {
         const values = this.readResourceAsLines();
 
-        this.solution1 = Day01.solve(values);
-        this.solution2 = Day01.solve(values);
+        this.password1 = Day01.solve(values, false);
+        this.password2 = Day01.solve(values, true);
 
-        console.info(`${this.solution1}`);
-        console.info(`${this.solution2}`);
+        console.info(`The password to open the door is ${this.password1}`);
+        console.info(`The password to open the door using method 0x434C49434B is ${this.password2}`);
 
-        return this.createResult([this.solution1, this.solution2]);
+        return this.createResult([this.password1, this.password2]);
     }
 }
