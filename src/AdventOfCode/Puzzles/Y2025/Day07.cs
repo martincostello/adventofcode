b4ff977539122ab1cde14a1d18aebcb7f5530bf0
Martin Costello <martin@martincostello.com>
2025-12-07 23:22:12 +0000

    Solve day 7 for 2025 (#2718)
    * Solve day 7 part 1
    
    Solve part 1 of the puzzle for day 7 of 2025.
    
    * Prepare for part 2
    
    Prepare for part 2 of day 7 of 2025.
    
    * Solve day 7 part 2
    
    Solve part 2 of the puzzle for day 7 of 2025.

--- src/AdventOfCode/Puzzles/Y2025/Day07.cs
+++ src/AdventOfCode/Puzzles/Y2025/Day07.cs
@@ -6,41 +6,86 @@ namespace MartinCostello.AdventOfCode.Puzzles.Y2025;
 /// <summary>
 /// A class representing the puzzle for <c>https://adventofcode.com/2025/day/7</c>. This class cannot be inherited.
 /// </summary>
-[Puzzle(2025, 07, "", RequiresData = true, IsHidden = true, Unsolved = true)]
-public sealed class Day07 : Puzzle
+[Puzzle(2025, 07, "Laboratories", RequiresData = true)]
+public sealed class Day07 : Puzzle<int, long>
 {
     /// <summary>
-    /// Gets the solution.
+    /// Finds the number of times the beam splits and the number of timelines it exists in for the
+    /// specified tachyon manifold by simulating the path of the beam from the origin in the diagram.
     /// </summary>
-    public int Solution { get; private set; }
-
-    /// <summary>
-    /// Solves the puzzle.
-    /// </summary>
-    /// <param name="values">The values to solve the puzzle from.</param>
+    /// <param name="diagram">The diagram of the tachyon manifold.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to use.</param>
     /// <returns>
-    /// The solution.
+    /// The number of times the beam splits and the number of timelines it exists on.
     /// </returns>
-    public static int Solve(IReadOnlyList<string> values)
+    public static (int Splits, long Timelines) Simulate(IReadOnlyList<string> diagram, CancellationToken cancellationToken)
     {
-        ArgumentNullException.ThrowIfNull(values);
-        return Unsolved;
+        int width = diagram[0].Length;
+        int height = diagram.Count;
+
+        var grid = new SquareGrid(width, height);
+
+        var origin = grid.VisitCells(diagram, Point.Empty, (grid, point, cell, origin) =>
+        {
+            if (cell is '^')
+            {
+                grid.Locations.Add(point);
+            }
+            else if (cell is 'S')
+            {
+                origin = point;
+            }
+
+            return origin;
+        });
+
+        var splitters = new Dictionary<Point, long>(grid.Locations.Count);
+
+        long timelines = Trace(grid, origin, splitters, cancellationToken);
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return (splitters.Count, timelines);
+
+        static long Trace(SquareGrid grid, Point origin, Dictionary<Point, long> splitters, CancellationToken cancellationToken)
+        {
+            while (grid.InBounds(origin) && !cancellationToken.IsCancellationRequested)
+            {
+                origin -= Directions.Up;
+
+                if (splitters.TryGetValue(origin, out long timelines))
+                {
+                    return timelines;
+                }
+
+                if (grid.Locations.Contains(origin))
+                {
+                    timelines = Trace(grid, origin + new Size(-1, 1), splitters, cancellationToken);
+                    timelines += Trace(grid, origin + new Size(1, 1), splitters, cancellationToken);
+
+                    splitters[origin] = timelines;
+
+                    return timelines;
+                }
+            }
+
+            return 1;
+        }
     }
 
     /// <inheritdoc />
     protected override async Task<PuzzleResult> SolveCoreAsync(string[] args, CancellationToken cancellationToken)
     {
-        ArgumentNullException.ThrowIfNull(args);
+        var diagram = await ReadResourceAsLinesAsync(cancellationToken);
 
-        var values = await ReadResourceAsLinesAsync(cancellationToken);
-
-        Solution = Solve(values);
+        (Solution1, Solution2) = Simulate(diagram, cancellationToken);
 
         if (Verbose)
         {
-            Logger.WriteLine("The solution is {0}.", Solution);
+            Logger.WriteLine("The beam splits {0} times.", Solution1);
+            Logger.WriteLine("A single tachyon particle would exist in {0} different timelines.", Solution2);
         }
 
-        return PuzzleResult.Create(Solution);
+        return PuzzleResult.Create(Solution1, Solution2);
     }
 }
