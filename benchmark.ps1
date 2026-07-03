68574480cc09126ab81efbef986816eeb2885e77
martincostello <martin@martincostello.com>
2026-07-03 15:49:27 +0100

    Bump BenchmarkDotNet to 0.16.0-preview.1
    Update to BenchmarkDotNet 0.16.0-preview.1.

--- benchmark.ps1
+++ benchmark.ps1
@@ -74,7 +74,9 @@ $benchmarks = (Join-Path $solutionPath "tests" "AdventOfCode.Benchmarks" "Advent
 
 Write-Information "Running benchmarks..."
 
-$additionalArgs = @()
+$additionalArgs = @(
+    "--consumeTasksSynchronously" # For backwards compatibility with BenchmarkDotNet versions before 0.16.0
+)
 
 if (![string]::IsNullOrEmpty($Filter)) {
     $additionalArgs += "--filter"
